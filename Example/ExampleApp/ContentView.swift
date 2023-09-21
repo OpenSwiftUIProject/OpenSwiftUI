d4fe24051000d8504a90eede77dc7d77e7c1156e
Kyle <kyle201817146@gmail.com>
2023-09-22 01:48:28 +0800

    Add OpenSwiftUI Example App project

---
+++ Example/ExampleApp/ContentView.swift
@@ -0,0 +1,14 @@
+//
+//  ContentView.swift
+//  ExampleApp
+//
+//  Created by Kyle on 2023/9/22.
+//
+
+import OpenSwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        fatalError()
+    }
+}
---
+++ Example/ExampleApp/ExampleApp.swift
@@ -0,0 +1,18 @@
+//
+//  ExampleAppApp.swift
+//  ExampleApp
+//
+//  Created by Kyle on 2023/9/22.
+//
+
+import OpenSwiftUI
+
+@main
+struct ExampleApp: App {
+    var body: some Scene {
+        fatalError()
+//        WindowGroup {
+//            ContentView()
+//        }
+    }
+}
