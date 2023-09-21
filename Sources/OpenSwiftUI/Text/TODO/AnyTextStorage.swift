86887f1aa835426e4c2c9773f20c7ca5bacf332e
Kyle <kyle201817146@gmail.com>
2023-09-22 02:17:01 +0800

    Add basic Text support

---
+++ Sources/OpenSwiftUI/Text/TODO/AnyTextStorage.swift
@@ -0,0 +1,16 @@
+//
+//  AnyTextStorage.swift
+//  OpenSwiftUI
+//
+//  Created by Kyle on 2023/9/22.
+//  Lastest Version: iOS 15.5
+//  Status: Empty
+
+import Foundation
+
+@_hasMissingDesignatedInitializers
+@usableFromInline
+class AnyTextStorage {
+    @objc
+    deinit {}
+}
---
+++ Sources/OpenSwiftUI/View/TODO/UnaryView.swift
@@ -0,0 +1,11 @@
+//
+//  UnaryView.swift
+//  OpenSwiftUI
+//
+//  Created by Kyle on 2023/9/22.
+//  Lastest Version: iOS 15.5
+//  Status: Empty
+
+import Foundation
+
+protocol UnaryView: View {}
