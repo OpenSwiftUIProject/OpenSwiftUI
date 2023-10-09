fd2c55b412c9879191856dc07e4b5c3459bef00b
Kyle <kyle201817146@gmail.com>
2023-10-09 15:47:38 +0800

    Update VectorArithmetic.swift to iOS 17.0

--- Sources/OpenSwiftUI/Interactivity/Animation/VectorArithmetic.swift
+++ Sources/OpenSwiftUI/Interactivity/Animation/VectorArithmetic.swift
@@ -3,12 +3,38 @@
 //  OpenSwiftUI
 //
 //  Created by Kyle on 2023/10/9.
-//  Lastest Version: iOS 15.5
+//  Lastest Version: iOS 17.0
 //  Status: Complete
 
 public protocol VectorArithmetic: AdditiveArithmetic {
     mutating func scale(by rhs: Double)
     var magnitudeSquared: Double { get }
+}
+
+extension VectorArithmetic {
+    @_alwaysEmitIntoClient
+    public func scaled(by rhs: Double) -> Self {
+        var result = self
+        result.scale(by: rhs)
+        return result
+    }
+
+    @_alwaysEmitIntoClient
+    public mutating func interpolate(towards other: Self, amount: Double) {
+        // lhs + (rhs - lhs) * t
+        var result = other
+        result -= self
+        result.scale(by: amount)
+        result += self
+        self = result
+    }
+
+    @_alwaysEmitIntoClient
+    public func interpolated(towards other: Self, amount: Double) -> Self {
+        var result = self
+        result.interpolate(towards: other, amount: amount)
+        return result
+    }
 }
 
 extension Float: VectorArithmetic {
