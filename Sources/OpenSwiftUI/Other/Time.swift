6be404674eed2d02e36bf399a7bab93fe0b89230
Kyle <kyle201817146@gmail.com>
2023-10-17 11:49:25 +0800

    Add Time implementation

--- Sources/OpenSwiftUI/Other/Time.swift
+++ Sources/OpenSwiftUI/Other/Time.swift
@@ -4,10 +4,16 @@
 //
 //  Created by Kyle on 2023/10/16.
 //  Lastest Version: iOS 15.5
-//  Status: WIP
+//  Status: Complete
 
 import Foundation
 
-struct Time {
+struct Time: Comparable, Hashable {
     var seconds : Double
+
+    static func < (lhs: Time, rhs: Time) -> Bool {
+        lhs.seconds < rhs.seconds
+    }
+
+    static var infinity: Time { Time(seconds: .infinity) }
 }
