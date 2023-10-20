472bd8d159938e56b84fcdb651a7c0e60c9d364c
Kyle <kyle201817146@gmail.com>
2023-10-20 17:10:33 +0800

    Fix OF_OPTIONS build issue on Linux

--- Sources/_OpenGraph/include/OGAttributeTypeFlags.hpp
+++ Sources/_OpenGraph/include/OGAttributeTypeFlags.hpp
@@ -10,13 +10,10 @@
 
 #include <OpenFoundation/OpenFoundation.h>
 
-// FIXME: OF_OPTIONS is not working on Linux platform
-#if !TARGET_OS_LINUX
 typedef OF_OPTIONS(uint32_t, OGAttributeTypeFlags) {
     OGAttributeTypeFlags_0 = 0,
     OGAttributeTypeFlags_8 = 1 << 3,
 };
-#endif
 
 #endif /* OGAttribute_hpp */
 
