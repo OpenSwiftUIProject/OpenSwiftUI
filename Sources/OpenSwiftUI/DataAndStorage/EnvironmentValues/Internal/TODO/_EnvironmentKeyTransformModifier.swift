0279edc64c843663f99422e38e52e97ca6aeee9a
Kyle <kyle201817146@gmail.com>
2023-12-06 19:41:44 +0800

    Update _EnvironmentKeyTransformModifier

--- Sources/OpenSwiftUI/DataAndStorage/EnvironmentValues/Internal/TODO/_EnvironmentKeyTransformModifier.swift
+++ Sources/OpenSwiftUI/DataAndStorage/EnvironmentValues/Internal/TODO/_EnvironmentKeyTransformModifier.swift
@@ -1,19 +1,26 @@
-protocol _GraphInputsModifier {}
+//
+//  _EnvironmentKeyTransformModifier.swift
+//  OpenSwiftUI
+//
+//  Created by Kyle on 2023/12/5.
+//  Lastest Version: iOS 15.5
+//  Status: WIP
+//  ID: 1DBD4F024EFF0E73A70DB6DD05D5B548
 
 @frozen
-public struct _EnvironmentKeyTransformModifier<Value> : ViewModifier, _GraphInputsModifier {
-    public var keyPath: Swift.WritableKeyPath<EnvironmentValues, Value>
-    public var transform: (inout Value) -> Swift.Void
+public struct _EnvironmentKeyTransformModifier<Value>: PrimitiveViewModifier, _GraphInputsModifier {
+    public var keyPath: WritableKeyPath<EnvironmentValues, Value>
+    public var transform: (inout Value) -> Void
     
     @inlinable
-    public init(keyPath: Swift.WritableKeyPath<EnvironmentValues, Value>, transform: @escaping (inout Value) -> Swift.Void) {
+    public init(keyPath: WritableKeyPath<EnvironmentValues, Value>, transform: @escaping (inout Value) -> Void) {
         self.keyPath = keyPath
         self.transform = transform
     }
+
     public static func _makeInputs(modifier: _GraphValue<_EnvironmentKeyTransformModifier<Value>>, inputs: inout _GraphInputs) {
 
     }
-    public typealias Body = Never
 }
 
 extension View {
@@ -27,4 +34,8 @@ extension View {
             transform: transform
         ))
     }
+}
+
+private struct ChildEnvironment {
+    
 }
