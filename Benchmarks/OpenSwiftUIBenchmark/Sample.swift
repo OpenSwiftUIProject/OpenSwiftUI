f559d02aa06cf96379b557d6cfbafc457edef4ef
Kyle <kyle201817146@gmail.com>
2024-01-05 00:00:36 +0800

    Add initial benchmark support

---
+++ Benchmarks/OpenSwiftUIBenchmark/Sample.swift
@@ -0,0 +1,14 @@
+//
+//  Sample.swift
+//  
+//
+//  Created by Kyle on 2024/1/4.
+//
+
+import Benchmark
+
+let benchmarks = {
+    Benchmark("Minimal benchmark") { _ in
+      // measure something here
+    }
+}
