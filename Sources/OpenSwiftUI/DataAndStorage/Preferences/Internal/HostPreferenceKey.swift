6734af4110e800e969635d0f3f4bc40a711b96be
Kyle <kyle201817146@gmail.com>
2024-01-05 15:26:10 +0800

    Add PreferenceKey related protocol

---
+++ Sources/OpenSwiftUI/DataAndStorage/Preferences/Internal/HostPreferenceKey.swift
@@ -0,0 +1,9 @@
+//
+//  HostPreferenceKey.swift
+//  OpenSwiftUI
+//
+//  Created by Kyle on 2023/1/5.
+//  Lastest Version: iOS 15.5
+//  Status: Complete
+
+protocol HostPreferenceKey: PreferenceKey {}
