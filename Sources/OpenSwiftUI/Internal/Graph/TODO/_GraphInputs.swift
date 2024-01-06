d5fb3cbd4a03c62a99249df3b0ff816b14190037
Kyle <kyle201817146@gmail.com>
2024-01-06 15:42:14 +0800

    Update _GraphInputs

--- Sources/OpenSwiftUI/Internal/Graph/TODO/_GraphInputs.swift
+++ Sources/OpenSwiftUI/Internal/Graph/TODO/_GraphInputs.swift
@@ -1,13 +1,13 @@
 internal import OpenGraphShims
 
 public struct _GraphInputs {
-//    var customInputs: PropertyList
+    var customInputs: PropertyList
     var time: Attribute<Time>
-//    var cachedEnvironment: MutableBox<CachedEnvironment>
-    var phase: Attribute<Phase>
-//    var transaction: Attribute<Transaction>
+    var cachedEnvironment: MutableBox<CachedEnvironment>
+    var phase: Attribute<_GraphInputs.Phase>
+    var transaction: Attribute<Transaction>
     var changedDebugProperties: _ViewDebug.Properties
-    var options: Options
+    var options: _GraphInputs.Options
     // FIXME: Compile crash on Linux
     #if !os(Linux)
     var mergedInputs: Set<OGAttribute>
