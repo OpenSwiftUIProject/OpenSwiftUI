f217348e5419b15b07056c2d4ed08e5d2eaf027f
Kyle <kyle201817146@gmail.com>
2024-01-28 23:35:49 +0800

    Add BoxVTable

--- Sources/OpenSwiftUI/DataAndStorage/ModelData/DynamicProperty/DynamicPropertyBuffer.swift
+++ Sources/OpenSwiftUI/DataAndStorage/ModelData/DynamicProperty/DynamicPropertyBuffer.swift
@@ -84,19 +84,67 @@ extension _DynamicPropertyBuffer {
 // MARK: - BoxVTableBase
 
 private class BoxVTableBase {
-    static func moveInitialize(ptr _: UnsafeMutableRawPointer, from _: UnsafeMutableRawPointer) {
+    class func moveInitialize(
+        ptr _: UnsafeMutableRawPointer,
+        from _: UnsafeMutableRawPointer
+    ) {
         fatalError()
     }
     
-    static func deinitialize(ptr _: UnsafeMutableRawPointer) {}
+    class func deinitialize(ptr _: UnsafeMutableRawPointer) {}
 
-    static func reset(ptr _: UnsafeMutableRawPointer) {}
+    class func reset(ptr _: UnsafeMutableRawPointer) {}
 
-    static func update(ptr _: UnsafeMutableRawPointer, property _: UnsafeMutableRawPointer, phase _: _GraphInputs.Phase) -> Bool {
+    class func update(
+        ptr _: UnsafeMutableRawPointer,
+        property _: UnsafeMutableRawPointer,
+        phase _: _GraphInputs.Phase
+    ) -> Bool {
         false
     }
     
-    static func getState<Value>(ptr _: UnsafeMutableRawPointer, type _: Value.Type) -> Binding<Value>? {
+    class func getState<Value>(
+        ptr _: UnsafeMutableRawPointer,
+        type _: Value.Type
+    ) -> Binding<Value>? {
         nil
+    }
+}
+
+private class BoxVTable<Box: DynamicPropertyBox>: BoxVTableBase {
+    override class func moveInitialize(ptr destination: UnsafeMutableRawPointer, from: UnsafeMutableRawPointer) {
+        let fromBoxPointer = from.assumingMemoryBound(to: Box.self)
+        let destinationBoxPointer = destination.assumingMemoryBound(to: Box.self)
+        destinationBoxPointer.initialize(to: fromBoxPointer.move())
+    }
+    
+    override class func deinitialize(ptr: UnsafeMutableRawPointer) {
+        let boxPointer = ptr.assumingMemoryBound(to: Box.self)
+        boxPointer.pointee.destroy()
+        boxPointer.deinitialize(count: 1)
+    }
+    
+    override class func reset(ptr: UnsafeMutableRawPointer) {
+        let boxPointer = ptr.assumingMemoryBound(to: Box.self)
+        boxPointer.pointee.reset()
+    }
+    
+    override class func update(
+        ptr: UnsafeMutableRawPointer,
+        property: UnsafeMutableRawPointer,
+        phase: _GraphInputs.Phase
+    ) -> Bool {
+        let boxPointer = ptr.assumingMemoryBound(to: Box.self)
+        let propertyPointer = property.assumingMemoryBound(to: Box.Property.self)
+        let isUpdated = boxPointer.pointee.update(property: &propertyPointer.pointee, phase: phase)
+        if isUpdated {
+            // TODO: OSSignpost
+        }
+        return isUpdated
+    }
+    
+    override class func getState<Value>(ptr: UnsafeMutableRawPointer, type: Value.Type) -> Binding<Value>? {
+        let boxPointer = ptr.assumingMemoryBound(to: Box.self)
+        return boxPointer.pointee.getState(type: type)
     }
 }
