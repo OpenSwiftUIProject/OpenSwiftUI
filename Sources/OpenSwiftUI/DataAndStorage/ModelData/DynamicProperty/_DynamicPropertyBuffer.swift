f7ca5420fd8085edcdf1fe6c47add9d843a40ddd
Kyle <kyle201817146@gmail.com>
2024-01-28 21:23:54 +0800

    Update _DynamicPropertyBuffer

--- Sources/OpenSwiftUI/DataAndStorage/ModelData/DynamicProperty/_DynamicPropertyBuffer.swift
+++ Sources/OpenSwiftUI/DataAndStorage/ModelData/DynamicProperty/_DynamicPropertyBuffer.swift
@@ -4,21 +4,60 @@
 //
 //  Created by Kyle on 2023/9/24.
 //  Lastest Version: iOS 15.5
-//  Status: Complete
+//  Status: WIP
 //  ID: 68550FF604D39F05971FE35A26EE75B0
+
+private let nullPtr: UnsafeMutableRawPointer = Unmanaged.passUnretained(unsafeBitCast(0, to: AnyObject.self)).toOpaque()
 
 public struct _DynamicPropertyBuffer {
     var buf: UnsafeMutableRawPointer
     var size: Int32
     var _count: Int32
     
-    init(fields _: DynamicPropertyCache.Fields, container _: _GraphValue<some Any>, inputs _: inout _GraphInputs, baseOffset _: Int) {
-        fatalError("")
+    init() {
+        buf = nullPtr
+        size = 0
+        _count = 0
+    }
+    
+    init<Value>(
+        fields: DynamicPropertyCache.Fields,
+        container: _GraphValue<Value>,
+        inputs: inout _GraphInputs,
+        baseOffset: Int
+    ) {
+        self.init()
+        addFields(fields, container: container, inputs: &inputs, baseOffset: baseOffset)
     }
 
-    func append(_: some DynamicPropertyBox, fieldOffset _: Int) {}
 
-    func addFields(_: DynamicPropertyCache.Fields, container _: _GraphValue<some Any>, inputs _: inout _GraphInputs, baseOffset _: Int) {}
+    mutating func addFields<Value>(
+        _ fields: DynamicPropertyCache.Fields,
+        container: _GraphValue<Value>,
+        inputs: inout _GraphInputs,
+        baseOffset: Int
+    ) {
+        switch fields.layout {
+        case let .product(fieldArray):
+            for field in fieldArray {
+                field.type._makeProperty(
+                    in: &self,
+                    container: container,
+                    fieldOffset: field.offset + baseOffset,
+                    inputs: &inputs
+                )
+            }
+        case let .sum(type, taggedFields):
+            guard !taggedFields.isEmpty else {
+                return
+            }
+            // TODO
+        }
+    }
+    
+    func append(_: some DynamicPropertyBox, fieldOffset _: Int) {
+        // TODO
+    }
 }
 
 extension _DynamicPropertyBuffer {
