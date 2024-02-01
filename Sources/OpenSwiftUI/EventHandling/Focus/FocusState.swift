4bb14a1fd08cf112cbb4d40f2a270d002233ff5e
Kyle <kyle201817146@gmail.com>
2024-02-02 00:34:19 +0800

    Add FocusState implementation

--- Sources/OpenSwiftUI/EventHandling/Focus/FocusState.swift
+++ Sources/OpenSwiftUI/EventHandling/Focus/FocusState.swift
@@ -8,7 +8,7 @@
 //  ID: 274D264A38B51DC68ACC48A91353B7D0
 
 @frozen
-//@propertyWrapper
+@propertyWrapper
 public struct FocusState<Value>: DynamicProperty where Value: Hashable {
     @frozen
     @propertyWrapper
@@ -40,6 +40,64 @@ public struct FocusState<Value>: DynamicProperty where Value: Hashable {
                 #endif
             }
         }
+
         private enum PrivateType {}
+    }
+
+    var value: Value
+    var location: AnyLocation<Value>?
+    var resetValue: Value
+    public var wrappedValue: Value {
+        get {
+            getValue(forReading: true)
+        }
+        nonmutating set {
+            guard let location else {
+                return
+            }
+            location.set(newValue, transaction: Transaction())
+        }
+    }
+
+    public var projectedValue: FocusState<Value>.Binding {
+        let value = getValue(forReading: false)
+        let binding: OpenSwiftUI.Binding<Value>
+        if let location {
+            binding = OpenSwiftUI.Binding(value: value, location: location)
+        } else {
+            Log.runtimeIssues("Accessing FocusState's value outside of the body of a View. This will result in a constant Binding of the initial value and will not update.")
+            binding = .constant(value)
+        }
+        return Binding(binding: binding)
+    }
+    
+    public static func _makeProperty<V>(in buffer: inout _DynamicPropertyBuffer, container: _GraphValue<V>, fieldOffset: Int, inputs: inout _GraphInputs) {
+        // TODO
+    }
+    
+    public init() where Value == Bool {
+        value = false
+        location = nil
+        resetValue = false
+    }
+    
+    public init<T>() where Value == T?, T: Hashable {
+        value = nil
+        location = nil
+        resetValue = nil
+    }
+    
+    private func getValue(forReading: Bool) -> Value {
+        guard let location else {
+            return value
+        }
+        if GraphHost.isUpdating {
+            if forReading {
+                location.wasRead = true
+            }
+            return value
+        } else {
+            return location.get()
+        }
     }
 }
