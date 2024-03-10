c24926bfd084eec7a2aa97882185f7fa00394082
Kyle <kyle201817146@gmail.com>
2024-03-11 01:38:34 +0800

    Add _UIGraphicsView implementation

---
+++ Sources/OpenSwiftUI/Integration/UIKit/_UIGraphicsView.swift
@@ -0,0 +1,21 @@
+//
+//  _UIGraphicsView.swift
+//  OpenSwiftUI
+//
+//  Audited for RELEASE_2021
+//  Status: Complete
+
+#if os(iOS)
+internal import COpenSwiftUI
+import UIKit
+
+class _UIGraphicsView: UIView {
+    override func _shouldAnimateProperty(withKey key: String) -> Bool {
+        if layer.hasBeenCommitted() {
+            super._shouldAnimateProperty(withKey: key)
+        } else {
+            false
+        }
+    }
+}
+#endif
