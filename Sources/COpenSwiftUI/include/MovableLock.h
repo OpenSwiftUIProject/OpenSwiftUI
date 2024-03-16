35aac35079c72c36fb922cdbc0c7a8269d4b852c
Kyle <kyle201817146@gmail.com>
2024-03-16 23:10:20 +0800

    Add MovableLock

---
+++ Sources/COpenSwiftUI/include/MovableLock.h
@@ -0,0 +1,50 @@
+//
+//  MovableLock.h
+//  OpenSwiftUI
+//
+//  Audited for RELEASE_2021
+//  Status: Complete
+
+#ifndef MovableLock_h
+#define MovableLock_h
+
+#include "OpenSwiftUIBase.h"
+#import <pthread.h>
+
+OPENSWIFTUI_ASSUME_NONNULL_BEGIN
+
+typedef struct MovableLock_s {
+    pthread_mutex_t mutex;
+    pthread_cond_t cond1;
+    pthread_cond_t cond2;
+    pthread_cond_t cond3;
+    pthread_t main;
+    pthread_t owner;
+    uint32_t level;
+    uint32_t unknown;
+    void (* _Nullable function)(const void *context);
+    const void * _Nullable context;
+    bool unknown4;
+    bool unknown5;
+} MovableLock_t;
+
+typedef MovableLock_t *MovableLock __attribute((swift_newtype(struct)));
+
+OPENSWIFTUI_EXPORT
+OPENSWIFTUI_REFINED_FOR_SWIFT
+MovableLock _MovableLockCreate(void) OPENSWIFTUI_SWIFT_NAME(MovableLock.create());
+
+OPENSWIFTUI_EXPORT
+OPENSWIFTUI_REFINED_FOR_SWIFT
+void _MovableLockDestory(MovableLock lock) OPENSWIFTUI_SWIFT_NAME(MovableLock.destory(self:));
+bool _MovableLockIsOwner(MovableLock lock) OPENSWIFTUI_SWIFT_NAME(MovableLock.isOwner(self:));
+bool _MovableLockIsOuterMostOwner(MovableLock lock) OPENSWIFTUI_SWIFT_NAME(MovableLock.isOuterMostOwner(self:));
+void _MovableLockLock(MovableLock lock) OPENSWIFTUI_SWIFT_NAME(MovableLock.lock(self:));
+void _MovableLockUnlock(MovableLock lock) OPENSWIFTUI_SWIFT_NAME(MovableLock.unlock(self:));
+void _MovableLockSyncMain(MovableLock lock, const void *context, void (*function)(const void *context)) OPENSWIFTUI_SWIFT_NAME(MovableLock.syncMain(self:_:function:));
+void _MovableLockWait(MovableLock lock) OPENSWIFTUI_SWIFT_NAME(MovableLock.wait(self:));
+void _MovableLockBroadcast(MovableLock lock) OPENSWIFTUI_SWIFT_NAME(MovableLock.broadcast(self:));
+
+OPENSWIFTUI_ASSUME_NONNULL_END
+
+#endif /* MovableLock_h */
---
+++ Tests/OpenSwiftUITests/COpenSwiftUI/MovableLockTests.swift
@@ -0,0 +1,33 @@
+//
+//  MovableLockTests.swift
+//
+//
+//  Created by Kyle on 2024/3/16.
+//
+
+import COpenSwiftUI
+import Testing
+
+final class MovableLockTests {
+    let lock: MovableLock
+
+    init() {
+        lock = MovableLock.create()
+    }
+
+    deinit {
+        lock.destory()
+    }
+    
+    @Test
+    func owner() {
+        #expect(lock.isOwner() == false)
+        #expect(lock.isOuterMostOwner() == false)
+        lock.lock()
+        #expect(lock.isOwner() == true)
+        #expect(lock.isOuterMostOwner() == true)
+        lock.unlock()
+        #expect(lock.isOwner() == false)
+        #expect(lock.isOuterMostOwner() == false)
+    }
+}
