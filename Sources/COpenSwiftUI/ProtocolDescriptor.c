5167605b87ba99159ce9c17e25b33e3967ef0f12
Kyle <kyle201817146@gmail.com>
2024-04-09 00:33:26 +0800

    Refactor AnyView init fromValue implementation

--- Sources/COpenSwiftUI/ProtocolDescriptor.c
+++ Sources/COpenSwiftUI/ProtocolDescriptor.c
@@ -23,17 +23,3 @@ const void *$s11OpenSwiftUI12ViewModifierMp;
 const void *_OpenSwiftUI_viewModifierProtocolDescriptor(void) {
     return &$s11OpenSwiftUI12ViewModifierMp;
 }
-
-void _OpenSwiftUI_callVisitViewType1(void *visitor_value,
-                                     const void *view_type,
-                                     const void *view_pwt) {
-    _OpenSwiftUI_callVisitViewType2(visitor_value, view_type, view_type, view_pwt);
-}
-
-OPENSWIFTUI_EXPORT
-void _OpenSwiftUI_callVisitViewType2(void *visitor_value,
-                                     const void *view_type,
-                                     const void *view_type2,
-                                     const void *view_pwt) {
-    _OpenSwiftUI_callVisitViewType(visitor_value, view_type, view_type2, view_pwt);
-}
