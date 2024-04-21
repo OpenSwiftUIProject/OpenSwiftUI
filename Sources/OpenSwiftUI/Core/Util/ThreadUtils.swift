d74f2fd4ad000b2999a9d168e61621bccef0b76a
Kyle <kyle201817146@gmail.com>
2024-04-21 18:42:49 +0800

    FIx WASI build issue

--- Sources/OpenSwiftUI/Core/Util/ThreadUtils.swift
+++ Sources/OpenSwiftUI/Core/Util/ThreadUtils.swift
@@ -9,9 +9,14 @@ import Foundation
 
 @inline(__always)
 func performOnMainThread(_ block: @escaping () -> Void) {
+    #if os(WASI)
+    // See #76: Thread and RunLoopMode.common is not available on WASI currently
+    block()
+    #else
     if Thread.isMainThread {
         block()
     } else {
         RunLoop.main.perform(inModes: [.common], block: block)
     }
+    #endif
 }
