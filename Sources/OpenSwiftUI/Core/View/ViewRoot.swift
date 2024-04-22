8313af3bb3026713b1567f3e93f5936121389d95
Kyle <kyle201817146@gmail.com>
2024-04-23 00:26:24 +0800

    Implement ConditionContent._makeView

---
+++ Sources/OpenSwiftUI/Core/View/ViewRoot.swift
@@ -0,0 +1,14 @@
+//
+//  ViewRoot.swift
+//  OpenSwiftUI
+//
+//  Audited for RELEASE_2021
+//  Status: WIP
+//  ID: 00F12C0E37A19C593ECA0DBD3BE26541
+
+extension View {
+    static func makeImplicitRoot(view: _GraphValue<Self>, inputs: _ViewInputs) -> _ViewOutputs {
+        // TODO
+        return .init()
+    }
+}
