3c5bc95c36f5bab19e9ea2541a408a93b8946847
Kyle <kyle201817146@gmail.com>
2024-05-05 01:02:30 +0800

    Update EmptyModifier implementation

--- Sources/OpenSwiftUI/Core/Modifier/ViewModifier/ViewModifier.swift
+++ Sources/OpenSwiftUI/Core/Modifier/ViewModifier/ViewModifier.swift
@@ -5,6 +5,49 @@
 //  Audited for RELEASE_2021
 //  Status: Complete
 
+/// A modifier that you apply to a view or another view modifier, producing a
+/// different version of the original value.
+///
+/// Adopt the ``ViewModifier`` protocol when you want to create a reusable
+/// modifier that you can apply to any view. The example below combines several
+/// modifiers to create a new modifier that you can use to create blue caption
+/// text surrounded by a rounded rectangle:
+///
+///     struct BorderedCaption: ViewModifier {
+///         func body(content: Content) -> some View {
+///             content
+///                 .font(.caption2)
+///                 .padding(10)
+///                 .overlay(
+///                     RoundedRectangle(cornerRadius: 15)
+///                         .stroke(lineWidth: 1)
+///                 )
+///                 .foregroundColor(Color.blue)
+///         }
+///     }
+///
+/// You can apply ``View/modifier(_:)`` directly to a view, but a more common
+/// and idiomatic approach uses ``View/modifier(_:)`` to define an extension to
+/// ``View`` itself that incorporates the view modifier:
+///
+///     extension View {
+///         func borderedCaption() -> some View {
+///             modifier(BorderedCaption())
+///         }
+///     }
+///
+/// You can then apply the bordered caption to any view, similar to this:
+///
+///     Image(systemName: "bus")
+///         .resizable()
+///         .frame(width:50, height:50)
+///     Text("Downtown Bus")
+///         .borderedCaption()
+///
+/// ![A screenshot showing the image of a bus with a caption reading
+/// Downtown Bus. A view extension, using custom a modifier, renders the
+///  caption in blue text surrounded by a rounded
+///  rectangle.](OpenSwiftUI-View-ViewModifier.png)
 public protocol ViewModifier {
     associatedtype Body: View
     
@@ -27,6 +70,10 @@ public protocol ViewModifier {
     
     typealias Content = _ViewModifier_Content<Self>
     
+    /// Gets the current body of the caller.
+    ///
+    /// `content` is a proxy for the view that will have the modifier
+    /// represented by `Self` applied to it.
     @ViewBuilder
     @MainActor(unsafe)
     func body(content: Content) -> Body
