fc7d5e5b76515c7f36344ef73aee9d20254e2ee0
Kyle <kyle201817146@gmail.com>
2024-09-01 23:26:57 +0800

    Add some doc for Model

--- Sources/OpenSwiftUI/Core/Data/Location/AnyLocation.swift
+++ Sources/OpenSwiftUI/Core/Data/Location/AnyLocation.swift
@@ -5,11 +5,18 @@
 //  Audited for RELEASE_2021
 //  Status: Complete
 
+/// The base type of all type-erased locations.
 @usableFromInline
 class AnyLocationBase {}
 
+/// The base type of all type-erased locations with value-type Value.
+/// It is annotated as `@unchecked Sendable` so that user types such as
+/// `State`, and `SceneStorage` can be cleanly `Sendable`. However, it is
+/// also the user types' responsibility to ensure that `get`, and `set` does
+/// not access the graph concurrently (`get` should not be called while graph
+/// is updating, for example).
 @usableFromInline
-class AnyLocation<Value>: AnyLocationBase {
+class AnyLocation<Value>: AnyLocationBase, @unchecked Sendable {
     var wasRead: Bool {
         get { fatalError() }
         set { fatalError() }
