c3c166e14c9e5282929ff3fbf44ef55a89179fa7
Kyle <kyle201817146@gmail.com>
2024-09-02 00:55:37 +0800

    Update MutableBox implementation

--- Sources/OpenSwiftUI/Core/Data/Boxes/MutableBox.swift
+++ Sources/OpenSwiftUI/Core/Data/Boxes/MutableBox.swift
@@ -2,33 +2,38 @@
 //  MutableBox.swift
 //  OpenSwiftUI
 //
-//  Audited for RELEASE_2021
+//  Audited for RELEASE_2024
 //  Status: Complete
 
 @propertyWrapper
-final class MutableBox<A> {
-    private var value: A
+final package class MutableBox<T> {
+    final package var value: T
+    
+    @inlinable
+    package init(_ value: T) {
+        self.value = value
+    }
 
-    var wrappedValue: A {
+    @inlinable
+    convenience package init(wrappedValue value: T) {
+        self.value = value
+    }
+    
+    @inlinable
+    final package var wrappedValue: T {
         get { value }
         set { value = newValue }
     }
     
-    init(_ value: A) {
-        self.value = value
-    }
-
-    init(wrappedValue value: A) {
-        self.value = value
-    }
-
-    var projectedValue: MutableBox<A> {
+    @inlinable
+    final package var projectedValue: MutableBox<T> {
         self
     }
 }
 
-extension MutableBox: Equatable where A: Equatable {
-    static func == (lhs: MutableBox<A>, rhs: MutableBox<A>) -> Bool {
+extension MutableBox: Equatable where T: Equatable {
+    @inlinable
+    package static func == (lhs: MutableBox<T>, rhs: MutableBox<T>) -> Bool {
         lhs.value == rhs.value
     }
 }
