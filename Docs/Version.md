aa6ddb3878bc79ca44994712cdd940da6ab98a6f
Kyle <kyle201817146@gmail.com>
2024-09-08 16:08:01 +0800

    Add ShapeStyle and AnyShapeStyle

--- Docs/Version.md
+++ Docs/Version.md
@@ -3,3 +3,5 @@
 RELEASE_2021 - iOS 15.5 - macOS 12.7.1
 
 RELEASE_2023 - iOS 17.4 - macOS 14.4.1
+
+<!--RELEASE_2024 - iOS 18.0 Beta 1 - macOS 15.0 Beta 1-->
---
+++ Sources/OpenSwiftUI/View/Shape/ShapeStyle/AnyShapeStyle.swift
@@ -0,0 +1,77 @@
+//
+//  AnyShapeStyle.swift
+//  OpenSwiftUI
+//
+//  Audited for RELEASE_2021
+//  Status: Complete
+//  ID: ABC85937500395B09974756E9F651929
+
+import Foundation
+internal import OpenGraphShims
+
+/// A type-erased ShapeStyle value.
+@frozen
+public struct AnyShapeStyle: ShapeStyle {
+    @usableFromInline
+    @frozen
+    package struct Storage: Equatable {
+        package var box: AnyShapeStyleBox
+        
+        @usableFromInline
+        package static func == (lhs: Storage, rhs: Storage) -> Bool {
+            if lhs.box === rhs.box {
+                return true
+            } else {
+                return lhs.box.isEqual(to: rhs.box)
+            }
+        }
+    }
+
+    package var storage: Storage
+    
+    /// Create an instance from `style`.
+    public init<S>(_ style: S) where S: ShapeStyle {
+        storage = .init(box: ShapeStyleBox(style))
+    }
+    
+    public func _apply(to shape: inout _ShapeStyle_Shape) {
+        storage.box.apply(to: &shape)
+    }
+    
+    public static func _apply(to type: inout _ShapeStyle_ShapeType) {
+        type.result = .none
+    }
+    
+    #if OPENSWIFTUI_SUPPORT_2023_API
+    public typealias Resolved = Never
+    #endif
+}
+
+/// Abstract base class for type-erased ShapeStyle storage.
+@usableFromInline
+package class AnyShapeStyleBox {
+    package func apply(to shape: inout _ShapeStyle_Shape) {}
+    package func isEqual(to other: AnyShapeStyleBox) -> Bool { false }
+    
+    @objc
+    @usableFromInline
+    deinit {}
+}
+
+// ID: ABC85937500395B09974756E9F651929
+private final class ShapeStyleBox<S>: AnyShapeStyleBox where S: ShapeStyle {
+    let base: S
+    
+    init(_ base: S) {
+        self.base = base
+    }
+    
+    override func apply(to shape: inout _ShapeStyle_Shape) {
+        base._apply(to: &shape)
+    }
+    
+    override func isEqual(to other: AnyShapeStyleBox) -> Bool {
+        let otherBox = other as? Self
+        return otherBox.map { compareValues(base, $0.base) } ?? false
+    }
+}
