e5f871f27e66d67a777bf3fa52e15015e0d54cdf
Kyle <kyle201817146@gmail.com>
2024-09-25 01:41:06 +0800

    Add UpdateTests

---
+++ Tests/OpenSwiftUICoreTests/Data/UpdateTests.swift
@@ -0,0 +1,39 @@
+//
+//  UpdateTests.swift
+//  OpenSwiftUICoreTests
+
+import OpenSwiftUICore
+import Testing
+
+struct UpdateTests {
+    @Test
+    @MainActor
+    func example() async {
+        await confirmation(expectedCount: 4) { confirmation in
+            Update.locked {
+                confirmation()
+                #expect(!Update.isActive)
+                #expect(!Update.threadIsUpdating)
+                #expect(Update.isOwner)
+                Update.dispatchImmediately {
+                    confirmation()
+                    #expect(Update.isActive)
+                    #expect(!Update.threadIsUpdating)
+                }
+                var result = 0
+                Update.enqueueAction {
+                    confirmation()
+                    result += 2
+                    #expect(result == 2)
+                }
+                Update.enqueueAction {
+                    confirmation()
+                    result += 3
+                    #expect(result == 5)
+                }
+                Update.dispatchActions()
+                #expect(result == 5)
+            }
+        }
+    }
+}
