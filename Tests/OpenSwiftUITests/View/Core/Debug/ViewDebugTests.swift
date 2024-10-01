b6848e753128b1e879422ac708aa1f3f5c42ec61
Kyle <kyle201817146@gmail.com>
2024-10-01 18:15:53 +0800

    Fix ViewDebug test case build issue

--- Tests/OpenSwiftUITests/View/Core/Debug/ViewDebugTests.swift
+++ Tests/OpenSwiftUITests/View/Core/Debug/ViewDebugTests.swift
@@ -12,14 +12,9 @@ import Foundation
 struct ViewDebugTests {
     @Test(.disabled("Skip the test until we finish the implementation of _ViewDebug"))
     func type() throws {
-        let data = try #require(_ViewDebug.serializedData([
-            .init(
-                data: [
-                    .type: CGSize.self,
-                ],
-                childData: []
-            ),
-        ]))
+        var rawData = _ViewDebug.Data()
+        rawData.data = [.type: CGSize.self]
+        let data = try #require(_ViewDebug.serializedData([rawData]))
         let content = String(decoding: data, as: UTF8.self)
         #expect(content == #"""
         [{"properties":[{"id":0,"attribute":{"type":"__C.CGSize","flags":0,"readableType":""}}],"children":[]}]
@@ -28,14 +23,9 @@ struct ViewDebugTests {
 
     @Test(.disabled("Skip the test until we finish the implementation of _ViewDebug"))
     func size() throws {
-        let data = try #require(_ViewDebug.serializedData([
-            .init(
-                data: [
-                    .size: CGSize(width: 20, height: 20),
-                ],
-                childData: []
-            ),
-        ]))
+        var rawData = _ViewDebug.Data()
+        rawData.data = [.size: CGSize(width: 20, height: 20)]
+        let data = try #require(_ViewDebug.serializedData([rawData]))
         let content = String(decoding: data, as: UTF8.self)
         #expect(content == #"""
         [{"properties":[],"children":[]}]
