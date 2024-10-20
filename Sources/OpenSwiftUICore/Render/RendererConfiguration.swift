599aaa31392fdf671a22c7b7c90424d0dd0b4a71
Kyle <kyle201817146@gmail.com>
2024-10-20 18:00:32 +0800

    Add RendererConfiguration

---
+++ Sources/OpenSwiftUICore/Render/RendererConfiguration.swift
@@ -0,0 +1,44 @@
+//
+//  RendererConfiguration.swift
+//  OpenSwiftUICore
+//
+//  Audited for RELEASE_2024
+//  Status: Complete
+
+public struct _RendererConfiguration {
+    public enum Renderer {
+        case `default`
+        indirect case rasterized(_ options: _RendererConfiguration.RasterizationOptions = .init())
+    }
+    
+    public var renderer: _RendererConfiguration.Renderer
+    public var minFrameInterval: Double
+    public init(renderer: _RendererConfiguration.Renderer = .default) {
+        self.renderer = renderer
+        self.minFrameInterval = .zero
+    }
+    
+    public static func rasterized(_ options: _RendererConfiguration.RasterizationOptions = .init()) -> _RendererConfiguration {
+        _RendererConfiguration(renderer: .rasterized(options))
+    }
+    
+    public struct RasterizationOptions {
+        public var colorMode: ColorRenderingMode = .nonLinear
+        public var rbColorMode: Int32? = nil
+        public var rendersAsynchronously: Bool = false
+        public var isOpaque: Bool = true
+        public var drawsPlatformViews: Bool = true
+        public var prefersDisplayCompositing: Bool = false
+        public var maxDrawableCount: Int = 3
+        public init() {}
+    }
+}
+
+@available(*, unavailable)
+extension _RendererConfiguration: Sendable {}
+
+@available(*, unavailable)
+extension _RendererConfiguration.Renderer: Sendable {}
+
+@available(*, unavailable)
+extension _RendererConfiguration.RasterizationOptions: Sendable {}
