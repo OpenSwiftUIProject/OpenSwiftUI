83cc73c3d2aaf73670679d9af19dc1e2a350075f
Kyle <kyle201817146@gmail.com>
2024-10-20 21:57:12 +0800

    Add GraphicsRenderer

--- Sources/OpenSwiftUICore/Render/DisplayList.GraphicsRenderer.swift
+++ Sources/OpenSwiftUICore/Render/DisplayList.GraphicsRenderer.swift
@@ -3,10 +3,70 @@
 //  OpenSwiftUICore
 //
 //  Audited for RELEASE_2024
-//  Status: WIP
+//  Status: Blocked by RenderBox and GraphicsContext
+//  ID: EFAEDE41CB8C85EF3A6A18DC05438A3C
+
+import Foundation
 
 extension DisplayList {
     final package class GraphicsRenderer {
+        package enum PlatformViewMode {
+            case ignored
+            case unsupported
+            case rendered(update: Bool)
+        }
         
+        private struct Cache {
+            var callbacks: [CallbackKey: Void /* RBDisplayListContents */]
+            var animators: [AnimatorKey: Void /* _DisplayList_AnyEffectAnimator */ ]
+            
+            struct CallbackKey: Hashable {
+                var index: DisplayList.Index.ID
+                var seed: DisplayList.Seed
+                var scale: CGFloat
+            }
+            
+            struct AnimatorKey: Hashable {
+                var index: DisplayList.Index.ID
+            }
+        }
+        
+        private var oldCache: DisplayList.GraphicsRenderer.Cache
+        private var newCache: DisplayList.GraphicsRenderer.Cache
+        var index: DisplayList.Index
+        var time: Time
+        var nextTime: Time
+        var stableIDs: _DisplayList_StableIdentityMap?
+        var inTransitionGroup: Bool
+        var stateHashes: [StrongHash]
+        package var platformViewMode: DisplayList.GraphicsRenderer.PlatformViewMode
+        
+        package init(platformViewMode: DisplayList.GraphicsRenderer.PlatformViewMode) {
+            fatalError("TODO")
+        }
+        
+        package func render(at time: Time, do body: () -> Void) {
+            fatalError("TODO")
+        }
+        
+        package func renderDisplayList(_ list: DisplayList, at time: Time, in ctx: inout GraphicsContext) {
+            fatalError("TODO")
+        }
+        
+        package func render(list: DisplayList, in ctx: inout GraphicsContext) {
+            fatalError("TODO")
+        }
+        
+        package func render(item: DisplayList.Item, in ctx: inout GraphicsContext) {
+            fatalError("TODO")
+        }
+        
+        package func drawImplicitLayer(in ctx: inout GraphicsContext, content: (inout GraphicsContext) -> Void) {
+            fatalError("TODO")
+        }
+        
+        package func renderPlatformView(_ view: AnyObject?, in ctx: GraphicsContext, size: CGSize, viewType: any Any.Type) {
+            fatalError("TODO")
+        }
     }
 }
