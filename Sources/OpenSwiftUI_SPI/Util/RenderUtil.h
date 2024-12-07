c6caf4ff501197e52310979e5e5bd89875153976
Kyle <kyle201817146@gmail.com>
2024-12-07 21:00:02 +0800

    Add RenderUtil.c

---
+++ Sources/OpenSwiftUI_SPI/Util/RenderUtil.h
@@ -0,0 +1,13 @@
+//
+//  RenderUtil.h
+//  OpenSwiftUI_SPI
+
+#ifndef RenderUtil_h
+#define RenderUtil_h
+
+#include "OpenSwiftUIBase.h"
+
+uint32_t _HighFrameRateReasonMake(uint32_t value);
+bool _VariableFrameDurationIsSupported();
+
+#endif /* RenderUtil_h */
