9406c6a0ad24723c7d1c951afbb4634c861ba8fc
Kyle <kyle201817146@gmail.com>
2024-12-08 01:19:11 +0800

    Update ViewOrigin

--- Sources/OpenSwiftUICore/Layout/Transform/ViewOrigin.swift
+++ Sources/OpenSwiftUICore/Layout/Transform/ViewOrigin.swift
@@ -1,8 +1,8 @@
 //
 //  ViewOrigin.swift
-//  OpenSwiftUI
+//  OpenSwiftUICore
 //
-//  Audited for iOS 15.5
+//  Audited for iOS 18.0
 //  Status: Complete
 
 package import Foundation
@@ -10,13 +10,57 @@ package import Foundation
 package struct ViewOrigin: Equatable {
     package var value: CGPoint
     
+    @inlinable
+    package init(invalid: Void) {
+        self.init(CGPoint(x: Double.nan, y: Double.nan))
+    }
+    
+    @inlinable
+    package init(_ value: CGPoint) {
+        self.value = value
+    }
+    
+    @inlinable
+    package init() {
+        self.init(.zero)
+    }
+    
+    @inlinable
+    package var x: CGFloat {
+        get { value.x }
+        set { value.x = newValue }
+    }
+    
+    @inlinable
+    package var y: CGFloat {
+        get { value.y }
+        set { value.y = newValue }
+    }
+    
     @inline(__always)
-    package static var zero: ViewOrigin { ViewOrigin(value: .zero) }
+    static let zero = ViewOrigin()
+}
+
+extension ViewOrigin {
+    package subscript(d: Axis) -> CGFloat {
+        get {
+            switch d {
+                case .horizontal: x
+                case .vertical: y
+            }
+        }
+        set {
+            switch d {
+                case .horizontal: x = newValue
+                case .vertical: y = newValue
+            }
+        }
+    }
 }
 
 extension ViewOrigin: Animatable {
-    package var animatableData: AnimatablePair<CGFloat, CGFloat> {
-        get { .init(value.x, value.y) }
-        set { value = .init(x: newValue.first, y: newValue.second) }
+    package var animatableData: CGPoint.AnimatableData {
+        get { value.animatableData }
+        set { value.animatableData = newValue }
     }
 }
