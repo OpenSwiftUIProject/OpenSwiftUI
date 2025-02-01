cea745daf18d260d6a8d284aa9407881336a3097
Kyle <kyle201817146@gmail.com>
2025-02-02 00:53:56 +0800

    Add HostingViewTransparentBackgroundReason

---
+++ Sources/OpenSwiftUI/Integration/Hosting/Platform/HostingViewTransparentBackgroundReason.swift
@@ -0,0 +1,39 @@
+//
+//  HostingViewTransparentBackgroundReason.swift
+//  OpenSwiftUI
+//
+//  Audited for iOS 18.0
+//  Status: Complete
+
+struct HostingViewTransparentBackgroundReason: OptionSet, CustomStringConvertible {
+    var rawValue: UInt32
+    
+    static var catalystSidebar = HostingViewTransparentBackgroundReason(rawValue: 1 << 0)
+    static var catalystPresentation = HostingViewTransparentBackgroundReason(rawValue: 1 << 1)
+    static var legacyPresentationSPI = HostingViewTransparentBackgroundReason(rawValue: 1 << 2)
+    static var containerBackground = HostingViewTransparentBackgroundReason(rawValue: 1 << 3)
+    static var listItemBackground = HostingViewTransparentBackgroundReason(rawValue: 1 << 4)
+    
+    var description: String {
+        var description = ""
+        if contains(.catalystSidebar) {
+            description.append("catalystSidebar, ")
+        }
+        if contains(.catalystPresentation) {
+            description.append("catalystPresentation, ")
+        }
+        if contains(.legacyPresentationSPI) {
+            description.append("legacyPresentationSPI, ")
+        }
+        if contains(.containerBackground) {
+            description.append("containerBackground, ")
+        }
+        if contains(.listItemBackground) {
+            description.append("listItemBackground, ")
+        }
+        if !description.isEmpty {
+            description.removeLast(2)
+        }
+        return "[\(description)]"
+    }
+}
