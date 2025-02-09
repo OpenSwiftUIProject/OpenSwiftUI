82a5f1e5a50286ea1e84d97e82854139a970c9c4
Kyle <kyle201817146@gmail.com>
2025-02-10 02:14:57 +0800

    Fix Linux platform dladdr issue

---
+++ Sources/OpenSwiftUI_SPI/Shims/CSymbols/OpenSwiftUI_CSymbols.h
@@ -0,0 +1,16 @@
+//
+//  OpenSwiftUI_CSymbols.h
+//  OpenSwiftUI_SPI
+
+#ifndef OpenSwiftUI_CSymbols_h
+#define OpenSwiftUI_CSymbols_h
+
+#include "OpenSwiftUIBase.h"
+
+OPENSWIFTUI_ASSUME_NONNULL_BEGIN
+
+const char * _Nullable getSymbolPathName(const void *address);
+
+OPENSWIFTUI_ASSUME_NONNULL_END
+
+#endif /* OpenSwiftUI_CSymbols_h */
