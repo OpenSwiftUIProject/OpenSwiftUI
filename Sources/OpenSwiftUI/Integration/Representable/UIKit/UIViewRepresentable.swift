43afa4f36e2caeb0e224d6a06714f35c71443ab2
Kyle <kyle201817146@gmail.com>
2025-02-10 22:39:56 +0800

    [WIP] Update UIViewRepresentable

--- Sources/OpenSwiftUI/Integration/Representable/UIKit/UIViewRepresentable.swift
+++ Sources/OpenSwiftUI/Integration/Representable/UIKit/UIViewRepresentable.swift
@@ -8,6 +8,8 @@
 #if os(iOS)
 
 public import UIKit
+public import OpenSwiftUICore
+import OpenGraphShims
 
 /// A wrapper for a UIKit view that you use to integrate that view into your
 /// OpenSwiftUI view hierarchy.
@@ -18,12 +20,12 @@ public import UIKit
 /// use its methods to create, update, and tear down your view. The creation and
 /// update processes parallel the behavior of OpenSwiftUI views, and you use them to
 /// configure your view with your app's current state information. Use the
-/// teardown process to remove your view cleanly from your OpenSwiftUI. For example,
+/// teardown process to remove your view cleanly from your Open For example,
 /// you might use the teardown process to notify other objects that the view is
 /// disappearing.
 ///
-/// To add your view into your SwiftUI interface, create your
-/// ``UIViewRepresentable`` instance and add it to your SwiftUI interface. The
+/// To add your view into your OpenSwiftUI interface, create your
+/// ``UIViewRepresentable`` instance and add it to your OpenSwiftUI interface. The
 /// system calls the methods of your representable instance at appropriate times
 /// to create and update the view. The following example shows the inclusion of
 /// a custom `MyRepresentedCustomView` structure in the view hierarchy.
@@ -38,21 +40,28 @@ public import UIKit
 ///     }
 ///
 /// The system doesn't automatically communicate changes occurring within your
-/// view to other parts of your SwiftUI interface. When you want your view to
-/// coordinate with other SwiftUI views, you must provide a
-/// ``NSViewControllerRepresentable/Coordinator`` instance to facilitate those
+/// view to other parts of your OpenSwiftUI interface. When you want your view to
+/// coordinate with other OpenSwiftUI views, you must provide a
+/// ``UIViewRepresentable/Coordinator`` instance to facilitate those
 /// interactions. For example, you use a coordinator to forward target-action
-/// and delegate messages from your view to any SwiftUI views.
+/// and delegate messages from your view to any OpenSwiftUI views.
+///
+/// - Warning: OpenSwiftUI fully controls the layout of the UIKit view's
+/// [center](https://developer.apple.com/documentation/UIKit/UIView/1622627-center),
+/// [bounds](https://developer.apple.com/documentation/UIKit/UIView/1622580-bounds),
+/// [frame](https://developer.apple.com/documentation/UIKit/UIView/1622621-frame), and
+/// [transform](https://developer.apple.com/documentation/UIKit/UIView/1622459-transform)
+/// properties. Don't directly set these layout-related properties on the view
+/// managed by a `UIViewRepresentable` instance from your own
+/// code because that conflicts with OpenSwiftUI and results in undefined behavior.
 @available(macOS, unavailable)
 @available(watchOS, unavailable)
-public protocol UIViewRepresentable: View where Self.Body == Never {
-
+@MainActor
+@preconcurrency
+public protocol UIViewRepresentable: View where Body == Never {
     /// The type of view to present.
     associatedtype UIViewType: UIView
     
-    /// A type to coordinate with the view.
-    associatedtype Coordinator = Void
-
     /// Creates the view object and configures its initial state.
     ///
     /// You must implement this method and use it to create your view object.
@@ -66,11 +75,10 @@ public protocol UIViewRepresentable: View where Self.Body == Never {
     ///   the current state of the system.
     ///
     /// - Returns: Your UIKit view configured with the provided information.
-    @MainActor
-    func makeUIView(context: Self.Context) -> Self.UIViewType
+    func makeUIView(context: Context) -> UIViewType
 
     /// Updates the state of the specified view with new information from
-    /// OpenSwiftUI.
+    /// Open
     ///
     /// When the state of your app changes, OpenSwiftUI updates the portions of your
     /// interface affected by those changes. OpenSwiftUI calls this method for any
@@ -82,11 +90,10 @@ public protocol UIViewRepresentable: View where Self.Body == Never {
     ///   - uiView: Your custom view object.
     ///   - context: A context structure containing information about the current
     ///     state of the system.
-    @MainActor
-    func updateUIView(_ uiView: Self.UIViewType, context: Self.Context)
+    func updateUIView(_ uiView: UIViewType, context: Context)
     
-    @MainActor
-    func _resetUIView(_ uiView: Self.UIViewType, coordinator: Self.Coordinator, destory: () -> Void)
+    @_spi(Private)
+    func _resetUIView(_ uiView: UIViewType, coordinator: Coordinator, destroy: () -> Void)
 
     /// Cleans up the presented UIKit view (and coordinator) in anticipation of
     /// their removal.
@@ -98,11 +105,14 @@ public protocol UIViewRepresentable: View where Self.Body == Never {
     /// - Parameters:
     ///   - uiView: Your custom view object.
     ///   - coordinator: The custom coordinator instance you use to communicate
-    ///     changes back to OpenSwiftUI. If you do not use a custom coordinator, the
+    ///     changes back to Open If you do not use a custom coordinator, the
     ///     system provides a default instance.
     @MainActor
-    static func dismantleUIView(_ uiView: Self.UIViewType, coordinator: Self.Coordinator)
-
+    static func dismantleUIView(_ uiView: UIViewType, coordinator: Coordinator)
+    
+    /// A type to coordinate with the view.
+    associatedtype Coordinator = Void
+    
     /// Creates the custom instance that you use to communicate changes from
     /// your view to other parts of your OpenSwiftUI interface.
     ///
@@ -118,20 +128,27 @@ public protocol UIViewRepresentable: View where Self.Body == Never {
     /// ``UIViewRepresentable/makeUIView(context:)`` method. The system provides
     /// your coordinator either directly or as part of a context structure when
     /// calling the other methods of your representable instance.
-    @MainActor
-    func makeCoordinator() -> Self.Coordinator
+    func makeCoordinator() -> Coordinator
     
-    // func _identifiedViewTree(in uiView: UIViewType) -> _IdentifiedViewTree
+    func _identifiedViewTree(in uiView: UIViewType) -> _IdentifiedViewTree
+    
+    func sizeThatFits(_ proposal: ProposedViewSize, uiView: UIViewType, context: Context) -> CGSize?
+
     func _overrideSizeThatFits(_ size: inout CGSize, in: _ProposedSize, uiView: UIViewType)
-    // func _overrideLayoutTraits(_ traits: inout _LayoutTraits, for uiView: UIViewType)
+
+    func _overrideLayoutTraits(_ layoutTraits: inout _LayoutTraits, for uiView: UIViewType)
 
     static func _modifyBridgedViewInputs(_ inputs: inout _ViewInputs)
-    
+
+    static func _layoutOptions(_ provider: UIViewType) -> LayoutOptions
+
     typealias Context = UIViewRepresentableContext<Self>
+
+    typealias LayoutOptions = _PlatformViewRepresentableLayoutOptions
 }
 
 @available(macOS, unavailable)
-extension UIViewRepresentable where Self.Coordinator == () {
+extension UIViewRepresentable where Coordinator == () {
 
     /// Creates the custom instance that you use to communicate changes from
     /// your view to other parts of your SwiftUI interface.
@@ -148,50 +165,47 @@ extension UIViewRepresentable where Self.Coordinator == () {
     /// ``UIViewRepresentable/makeUIView(context:)`` method. The system provides
     /// your coordinator either directly or as part of a context structure when
     /// calling the other methods of your representable instance.
-    public func makeCoordinator() -> Self.Coordinator {
+    public func makeCoordinator() -> Coordinator {
         return
     }
 }
 
 @available(macOS, unavailable)
 extension UIViewRepresentable {
-    /// Cleans up the presented UIKit view (and coordinator) in anticipation of
-    /// their removal.
-    ///
-    /// Use this method to perform additional clean-up work related to your
-    /// custom view. For example, you might use this method to remove observers
-    /// or update other parts of your SwiftUI interface.
-    ///
-    /// - Parameters:
-    ///   - uiView: Your custom view object.
-    ///   - coordinator: The custom coordinator instance you use to communicate
-    ///     changes back to SwiftUI. If you do not use a custom coordinator, the
-    ///     system provides a default instance.
-    public static func dismantleUIView(_ uiView: Self.UIViewType, coordinator: Self.Coordinator) {}
-
-    /// Declares the content and behavior of this view.
-    public var body: Never { bodyError() }
-}
-
-extension UIViewRepresentable {
-    func _resetUIView(_ uiView: Self.UIViewType, coordinator: Self.Coordinator, destory: () -> Void) {
-        destory()
+    public func _resetUIView(_ uiView: UIViewType, coordinator: Coordinator, destroy: () -> Void) {
+        destroy()
     }
-    
-    // func _identifiedViewTree(in uiView: UIViewType) -> _IdentifiedViewTree
-    func _overrideSizeThatFits(_ size: inout CGSize, in: _ProposedSize, uiView: UIViewType) {}
-    // func _overrideLayoutTraits(_ traits: inout _LayoutTraits, for uiView: UIViewType)
 
-    static func _modifyBridgedViewInputs(_ inputs: inout _ViewInputs) {}
-    
-    public static func _makeView(view: _GraphValue<Self>, inputs: _ViewInputs) -> _ViewOutputs {
+    public static func dismantleUIView(_ uiView: UIViewType, coordinator: Coordinator) {}
+
+    nonisolated public static func _makeView(view: _GraphValue<Self>, inputs: _ViewInputs) -> _ViewOutputs {
+        precondition(isLinkedOnOrAfter(.v4) ? Metadata(Self.self).isValueType : true, "")
+
         preconditionFailure("TODO")
     }
-    
-    public static func _makeViewList(view: _GraphValue<Self>, inputs: _ViewListInputs) -> _ViewListOutputs {
+
+    nonisolated public static func _makeViewList(view: _GraphValue<Self>, inputs: _ViewListInputs) -> _ViewListOutputs {
         preconditionFailure("TODO")
     }
+
+    public func _identifiedViewTree(in uiView: UIViewType) -> _IdentifiedViewTree { .empty }
+
+    public func sizeThatFits(_ proposal: ProposedViewSize, uiView: UIViewType, context: Context) -> CGSize? { nil }
+
+    public func _overrideSizeThatFits(_ size: inout CGSize, in proposedSize: _ProposedSize, uiView: UIViewType) {}
+
+    public func _overrideLayoutTraits(_ layoutTraits: inout _LayoutTraits, for uiView: UIViewType) {}
+
+    public static func _modifyBridgedViewInputs(_ inputs: inout _ViewInputs) {}
+
+    public static func _layoutOptions(_ provider: UIViewType) -> LayoutOptions { .init(rawValue: 1) }
+
+    public var body: Never {
+        bodyError()
+    }
 }
+
+// FIXME
 
 /// Contextual information about the state of the system that you use to create
 /// and update your UIKit view.
@@ -228,11 +242,10 @@ public struct UIViewRepresentableContext<Representable> where Representable: UIV
         self.environment = environment
         self.preferenceBridge = preferenceBridge
     }
-}
 
-@available(macOS, unavailable)
-@available(watchOS, unavailable)
-extension UIViewRepresentableContext : Sendable {
+//    public func animate(changes: () -> Void, completion: (() -> Void)? = nil) {
+//
+//    }
 }
 
 #endif
