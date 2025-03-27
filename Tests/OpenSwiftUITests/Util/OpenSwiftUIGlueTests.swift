8317062813a3741446f5c4bd56a2bfb293a28655
Kyle <kyle201817146@gmail.com>
2025-03-27 15:57:05 +0800

    Add OpenSwiftUIGlueTest

---
+++ Tests/OpenSwiftUITests/Util/OpenSwiftUIGlueTests.swift
@@ -0,0 +1,31 @@
+//
+//  OpenSwiftUIGlueTests.swift
+//  OpenSwiftUITests
+
+#if canImport(Darwin)
+
+import Testing
+@_spi(ForOpenSwiftUIOnly)
+import OpenSwiftUI
+@_spi(ForOpenSwiftUIOnly)
+import OpenSwiftUICore
+
+// MARK: - OpenSwiftUIGlue
+
+struct OpenSwiftUIGlueTests {
+    @Test
+    func sharedInstance() {
+        #expect(CoreGlue.shared is OpenSwiftUIGlue)
+    }
+}
+
+// MARK: - OpenSwiftUIGlue2
+
+struct OpenSwiftUIGlue2Tests {
+    @Test
+    func sharedInstance() {
+        #expect(CoreGlue2.shared is OpenSwiftUIGlue2)
+    }
+}
+
+#endif
