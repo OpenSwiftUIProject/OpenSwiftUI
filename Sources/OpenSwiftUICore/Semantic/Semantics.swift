94097b7ee0f636cb314c61a70321ad5efd7520c8
Kyle <kyle201817146@gmail.com>
2025-04-17 10:45:52 +0800

    Doc: add availability versions documentation and version map

--- Sources/OpenSwiftUICore/Semantic/Semantics.swift
+++ Sources/OpenSwiftUICore/Semantic/Semantics.swift
@@ -138,33 +138,106 @@ extension Semantics {
     }
 }
 
-// 2019.9.1 OS release (iOS 13)
+// NOTE: See ./Docs/AvailabilityVersions/README.md for more information
+
+/// Fall 2019 (2019.9.1) SDK version set
+///
+/// macOS 10.15, iOS 13.0, watchOS 6.0, tvOS 13.0, bridgeOS 4.0, driverKit 19.0
 let openSwiftUI_v1_os_versions = dyld_build_version_t(version: 0x07E3_0901)
+
+/// Autumn 2019 (2019.9.2) SDK version set
+///
+/// macOS 10.15, iOS 13.1, watchOS 6.0, tvOS 13.0, bridgeOS 4.0, driverKit 19.0
 let openSwiftUI_autumn_2019_os_versions = dyld_build_version_t(version: 0x07E3_0902)
+
+/// Late Fall 2019 (2019.16.21) SDK version set
+///
+/// macOS 10.15.1, iOS 13.2, watchOS 6.1, tvOS 13.2, bridgeOS 4.1, driverKit 19.0
 let openSwiftUI_late_fall_2019_os_versions = dyld_build_version_t(version: 0x07E3_1015)
+
+/// Winter 2019 (2019.18.1) SDK version set
+///
+/// macOS 10.15.1, iOS 13.3, watchOS 6.1, tvOS 13.3, bridgeOS 4.1, driverKit 19.0
 let openSwiftUI_v1_3_1_os_versions = dyld_build_version_t(version: 0x07E3_1201)
+
+/// Spring 2020 (2020.3.1) SDK version set
+///
+/// macOS 10.15.4, iOS 13.4, watchOS 6.2, tvOS 13.4, bridgeOS 4.1, driverKit 19.0
 let openSwiftUI_v1_4_os_versions = dyld_build_version_t(version: 0x07E4_0301)
+
+/// Late Spring 2020 (2020.4.21) SDK version set
+///
+/// macOS 10.15.4, iOS 13.5, watchOS 6.2, tvOS 13.4, bridgeOS 4.1, driverKit 19.0
 let openSwiftUI_late_spring_2020_os_versions = dyld_build_version_t(version: 0x07E4_0415)
+
+/// Summer 2020 (2020.6.1) SDK version set
+///
+/// macOS 10.15.4, iOS 13.6, watchOS 6.2, tvOS 13.4, bridgeOS 4.1, driverKit 19.0
 let openSwiftUI_summer_2020_os_versions = dyld_build_version_t(version: 0x07E4_0601)
-// 2020.9.1 OS release (iOS 14)
+
+/// Fall 2020 (2020.9.1) SDK version set
+///
+/// macOS 10.16, iOS 14.0, watchOS 7.0, tvOS 14.0, bridgeOS 5.0, driverKit 20.0
 let openSwiftUI_v2_os_versions = dyld_build_version_t(version: 0x07E4_0901)
+
+/// Late Fall 2020 (2020.16.21) SDK version set
+///
+/// macOS 10.16, iOS 14.2, watchOS 7.1, tvOS 14.1, bridgeOS 5.0, driverKit 20.0
 let openSwiftUI_v2_1_os_versions = dyld_build_version_t(version: 0x07E4_1015)
+
+/// Spring 2021 (2021.3.1) SDK version set
+///
+/// macOS 11.3, iOS 14.5, watchOS 7.4, tvOS 14.5, bridgeOS 5.3, driverKit 20.0
 let openSwiftUI_v2_3_os_versions = dyld_build_version_t(version: 0x07E5_0301)
-// 2021.9.1 OS release (iOS 15)
+
+/// Fall 2021 (2021.9.1) SDK version set
+///
+/// macOS 12.0, iOS 15.0, watchOS 8.0, tvOS 15.0, bridgeOS 6.0, driverKit 21.0
 let openSwiftUI_v3_0_os_versions = dyld_build_version_t(version: 0x07E5_0901)
-// 2021.12.1 OS release
+
+/// Winter 2021 (2021.18.01) SDK version set
+///
+/// macOS 12.1, iOS 15.2, watchOS 8.3, tvOS 15.2, bridgeOS 6.2, driverKit 21.0
 let openSwiftUI_v3_2_os_versions = dyld_build_version_t(version: 0x07E5_1201)
+
+/// Spring 2022 (2022.3.1) SDK version set
+///
+/// macOS 12.3, iOS 15.4, watchOS 8.5, tvOS 15.4, bridgeOS 6.4, driverKit 21.0
 let openSwiftUI_v3_4_os_versions = dyld_build_version_t(version: 0x07E6_0301)
-// 2022.9.1 OS release (iOS 16)
+
+/// Fall 2022 (2022.9.1) SDK version set
+///
+/// macOS 13.0, iOS 16.0, watchOS 9.0, tvOS 16.0, bridgeOS 7.0, driverKit 22.0
 let openSwiftUI_v4_0_os_versions = dyld_build_version_t(version: 0x07E6_0901)
+
+/// 2022_SU_E (2022.35.0) SDK version set
+///
+/// macOS 13.3, iOS 16.4, watchOS 9.4, tvOS 16.4, bridgeOS 7.3, driverKit 22.4
 let openSwiftUI_v4_4_os_versions = dyld_build_version_t(version: 0x07E6_2300)
-// 2023.9.1 OS release (iOS 17)
+
+/// Fall 2023 (2023.9.1) SDK version set
+///
+/// macOS 14.0, iOS 17.0, watchOS 10.0, tvOS 17.0, bridgeOS 8.0, driverKit 23.0
 let openSwiftUI_v5_0_os_versions = dyld_build_version_t(version: 0x07E7_0901)
+
+/// 2023_SU_C (2023.13.1) SDK version set
+///
+/// macOS 14.2, iOS 17.2, watchOS 10.2, tvOS 17.2, bridgeOS 8.2, driverKit 23.2, visionOS 1.0
 let openSwiftUI_v5_2_os_versions = dyld_build_version_t(version: 0x07E7_0d01)
-// 2024 OS release (iOS 18)
+
+/// Fall 2024 (2024.0.0) SDK version set
+///
+/// macOS 15.0, iOS 18.0, watchOS 11.0, tvOS 18.0, bridgeOS 9.0, driverKit 24.0, visionOS 2.0
 let openSwiftUI_v6_0_os_versions = dyld_build_version_t(version: 0x07E8_0000)
+
+/// 2024 (2024.1.0) SDK version set
 let openSwiftUI_v6_1_os_versions = dyld_build_version_t(version: 0x07E8_0100)
+
+/// 2024 (2024.2.0) SDK version set
 let openSwiftUI_v6_2_os_versions = dyld_build_version_t(version: 0x07E8_0200)
+
+/// 2024 (2024.4.0) SDK version set
 let openSwiftUI_v6_4_os_versions = dyld_build_version_t(version: 0x07E8_0400)
-// 2025 OS release (iOS 19)
+
+/// Fall 2025 (2025.0.0) SDK version set
 let openSwiftUI_v7_0_os_versions = dyld_build_version_t(version: 0x07E9_0000)
