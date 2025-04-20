7fe7c289b343a4003515427370e36ce5f45cab08
Kyle <kyle201817146@gmail.com>
2025-04-20 13:17:31 +0800

    Update semanticsIsDeployedOnOrAfter test case

--- Tests/OpenSwiftUICoreTests/Semantic/SemanticsTests.swift
+++ Tests/OpenSwiftUICoreTests/Semantic/SemanticsTests.swift
@@ -37,14 +37,50 @@ struct SemanticsTests {
         #expect(Semantics.v5.description == "2023-9-1")
         #expect(Semantics.v6.description == "2024-0-0")
     }
-    
+
     @Test
-    func isDeployedOnOrAfter() {
-        #expect(OpenSwiftUICore.isDeployedOnOrAfter(.v1) == true)
-        #expect(OpenSwiftUICore.isDeployedOnOrAfter(.v2) == true)
-        #expect(OpenSwiftUICore.isDeployedOnOrAfter(.v3) == true)
-        #expect(OpenSwiftUICore.isDeployedOnOrAfter(.v4) == true)
-        #expect(OpenSwiftUICore.isDeployedOnOrAfter(.v5) == true)
-        #expect(OpenSwiftUICore.isDeployedOnOrAfter(.v6) == false)
+    func semanticsIsDeployedOnOrAfter() {
+        let expectedIsDeployedOnOrAfterV1: Bool
+        let expectedIsDeployedOnOrAfterV2: Bool
+        let expectedIsDeployedOnOrAfterV3: Bool
+        let expectedIsDeployedOnOrAfterV4: Bool
+        let expectedIsDeployedOnOrAfterV5: Bool
+        let expectedIsDeployedOnOrAfterV6: Bool
+        if #available(iOS 13, macOS 10.15, *) {
+            expectedIsDeployedOnOrAfterV1 = true
+        } else {
+            expectedIsDeployedOnOrAfterV1 = false
+        }
+        if #available(iOS 14, macOS 11, *) {
+            expectedIsDeployedOnOrAfterV2 = true
+        } else {
+            expectedIsDeployedOnOrAfterV2 = false
+        }
+        if #available(iOS 15, macOS 12, *) {
+            expectedIsDeployedOnOrAfterV3 = true
+        } else {
+            expectedIsDeployedOnOrAfterV3 = false
+        }
+        if #available(iOS 16, macOS 13, *) {
+            expectedIsDeployedOnOrAfterV4 = true
+        } else {
+            expectedIsDeployedOnOrAfterV4 = false
+        }
+        if #available(iOS 17, macOS 14, *) {
+            expectedIsDeployedOnOrAfterV5 = true
+        } else {
+            expectedIsDeployedOnOrAfterV5 = false
+        }
+        if #available(iOS 18, macOS 15, *) {
+            expectedIsDeployedOnOrAfterV6 = true
+        } else {
+            expectedIsDeployedOnOrAfterV6 = false
+        }
+        #expect(isDeployedOnOrAfter(.v1) == expectedIsDeployedOnOrAfterV1)
+        #expect(isDeployedOnOrAfter(.v2) == expectedIsDeployedOnOrAfterV2)
+        #expect(isDeployedOnOrAfter(.v3) == expectedIsDeployedOnOrAfterV3)
+        #expect(isDeployedOnOrAfter(.v4) == expectedIsDeployedOnOrAfterV4)
+        #expect(isDeployedOnOrAfter(.v5) == expectedIsDeployedOnOrAfterV5)
+        #expect(isDeployedOnOrAfter(.v6) == expectedIsDeployedOnOrAfterV6)
     }
 }
