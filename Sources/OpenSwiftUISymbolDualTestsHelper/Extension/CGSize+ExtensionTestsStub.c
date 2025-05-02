e9db49aa9fcf4db1bff053db0a3ecbb51aa99d74
Kyle <kyle201817146@gmail.com>
2025-05-02 14:32:46 +0800

    Add SymbolLocator dependency and OpenSwiftUISymbolDualTests (#263)

---
+++ Sources/OpenSwiftUISymbolDualTestsHelper/Extension/CGSize+ExtensionTestsStub.c
@@ -0,0 +1,13 @@
+//
+//  CGSize+ExtensionTestsStub.c
+//  OpenSwiftUISymbolDualTestsHelper
+
+#include "OpenSwiftUIBase.h"
+
+#if OPENSWIFTUI_TARGET_OS_DARWIN
+
+#import <SymbolLocator.h>
+
+DEFINE_SL_STUB_SLF(OpenSwiftUITestStub_CGSizeHasZero, SwiftUI, $sSo6CGSizeV7SwiftUIE7hasZeroSbvg);
+
+#endif
