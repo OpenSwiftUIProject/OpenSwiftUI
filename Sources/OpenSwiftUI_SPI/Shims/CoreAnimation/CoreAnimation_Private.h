06a459b73488b298325352cfd6c236da0c029b92
Kyle <kyle201817146@gmail.com>
2025-05-17 17:16:17 +0800

    Update Private API for CoreAnimation (#286)

--- Sources/OpenSwiftUI_SPI/Shims/CoreAnimation/CoreAnimation_Private.h
+++ Sources/OpenSwiftUI_SPI/Shims/CoreAnimation/CoreAnimation_Private.h
@@ -15,9 +15,7 @@ OPENSWIFTUI_ASSUME_NONNULL_BEGIN
 
 @interface CALayer (OpenSwiftUI_SPI)
 @property (nonatomic, assign, readonly) BOOL hasBeenCommitted_openswiftui_safe_wrapper OPENSWIFTUI_SWIFT_NAME(hasBeenCommitted);
-- (void)setAllowsEdgeAntialiasing_openswiftui_safe_wrapper:(BOOL)allows OPENSWIFTUI_SWIFT_NAME(setAllowsEdgeAntialiasing(_:));
-- (void)setAllowsGroupOpacity_openswiftui_safe_wrapper:(BOOL)allows OPENSWIFTUI_SWIFT_NAME(setAllowsGroupOpacity(_:));
-- (void)setAllowsGroupBlending_openswiftui_safe_wrapper:(BOOL)allows OPENSWIFTUI_SWIFT_NAME(setAllowsGroupBlending(_:));
+@property (nonatomic, assign) BOOL allowsGroupBlending_openswiftui_safe_wrapper OPENSWIFTUI_SWIFT_NAME(allowsGroupBlending);
 
 @property (nonatomic, assign) uint64_t openSwiftUI_viewTestProperties;
 @end
