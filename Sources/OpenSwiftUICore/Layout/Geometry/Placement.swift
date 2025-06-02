31b92c8a24e388c3795f43c2f2503625fa11142e
Kyle <kyle201817146@gmail.com>
2025-06-02 17:19:29 +0800

    Update Placement indent

--- Sources/OpenSwiftUICore/Layout/Geometry/Placement.swift
+++ Sources/OpenSwiftUICore/Layout/Geometry/Placement.swift
@@ -2,29 +2,29 @@
 //  Placement.swift
 //  OpenSwiftUICore
 //
-//  Audited for iOS 18.0
 //  Status: Complete
 
 public import Foundation
 
+// MARK: - Placement [6.4.41]
+
 /// The position and proposed size of a child view, as determined by a layout.
 public struct _Placement: Equatable {
-    
     public var proposedSize: CGSize {
         get { proposedSize_.fixingUnspecifiedDimensions() }
         set { proposedSize_ = _ProposedSize(newValue) }
     }
-        
+
     package var proposedSize_: _ProposedSize
-    
+
     /// The relative position in the child's actual size that will be placed at
     /// `anchorPosition`.
     public var anchor: UnitPoint
-    
+
     /// The location that the anchor will be given in the layout’s coordinate
     /// space.
     public var anchorPosition: CGPoint
-    
+
     /// Creates an instance with the given `proposedSize`, anchoring `anchor` at
     /// `anchorPosition`.
     public init(proposedSize: CGSize, anchoring anchor: UnitPoint = .topLeading, at anchorPosition: CGPoint) {
@@ -32,25 +32,25 @@ public struct _Placement: Equatable {
         self.anchor = anchor
         self.anchorPosition = anchorPosition
     }
-    
+
     package init(proposedSize: _ProposedSize, anchoring anchor: UnitPoint, at anchorPosition: CGPoint) {
         self.proposedSize_ = proposedSize
         self.anchor = anchor
         self.anchorPosition = anchorPosition
     }
-    
+
     package init(proposedSize: _ProposedSize, at anchorPosition: CGPoint) {
         self.proposedSize_ = proposedSize
         self.anchor = .topLeading
         self.anchorPosition = anchorPosition
     }
-    
+
     package init(proposedSize: CGSize, aligning anchor: UnitPoint, in area: CGSize) {
         self.proposedSize_ = _ProposedSize(proposedSize)
         self.anchor = anchor
         self.anchorPosition = anchor.in(area)
     }
-    
+
     package init(proposedSize: _ProposedSize, aligning anchor: UnitPoint, in area: CGSize) {
         self.proposedSize_ = proposedSize
         self.anchor = anchor
