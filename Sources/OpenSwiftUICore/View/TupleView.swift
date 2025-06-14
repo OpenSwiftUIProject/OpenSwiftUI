6c5c7ff239db65ef746ae2a630fe2e57d6d005ec
Kyle <kyle201817146@gmail.com>
2025-06-14 19:28:00 +0800

    Update TupleView

--- Sources/OpenSwiftUICore/View/TupleView.swift
+++ Sources/OpenSwiftUICore/View/TupleView.swift
@@ -1,27 +1,131 @@
 //
 //  TupleView.swift
-//  OpenSwiftUI
+//  OpenSwiftUICore
 //
-//  Audited for iOS 15.5
-//  Status: WIP
-//  ID: 79611CB2B7848ECB3D9EC1F26B13F28F
+//  Status: Complete
+//  ID: 79611CB2B7848ECB3D9EC1F26B13F28F (SwiftUI)
+//  ID: DE681AB5F1A334FA14ECABDE70CB1955 (SwiftUICore)
 
+import OpenGraphShims
+
+// MARK: - TupleView [6.4.41]
+
+/// A View created from a swift tuple of View values.
 @available(OpenSwiftUI_v1_0, *)
 @frozen
-public struct TupleView<T>: PrimitiveView {
+public struct TupleView<T>: PrimitiveView, View {
     public var value: T
-    @inlinable public init(_ value: T) { self.value = value }
 
-    //  public static func _makeView(view: _GraphValue<TupleView<T>>, inputs: _ViewInputs) -> _ViewOutputs
-    //  public static func _makeViewList(view: _GraphValue<TupleView<T>>, inputs: _ViewListInputs) -> _ViewListOutputs
-    //  public static func _viewListCount(inputs: _ViewListCountInputs) -> Int?
-    public typealias Body = Never
-}
+    @inlinable
+    public init(_ value: T) { self.value = value }
 
-extension TupleView {
-    private struct CountViews {}
-    private struct MakeList {}
-    private struct MakeUnary {}
+    public static func _makeView(
+        view: _GraphValue<TupleView<T>>,
+        inputs: _ViewInputs
+    ) -> _ViewOutputs {
+        let contentTypes = ViewDescriptor.tupleDescription(TupleType(T.self)).contentTypes
+        if contentTypes.count == 1 {
+            var makeUnary = MakeUnary(view: view, inputs: inputs, outputs: nil)
+            contentTypes[0].1.visitType(visitor: &makeUnary)
+            return makeUnary.outputs!
+        } else {
+            guard !contentTypes.isEmpty else {
+                return _ViewOutputs()
+            }
+            return makeImplicitRoot(view: view, inputs: inputs)
+        }
+    }
+
+    public static func _makeViewList(
+        view: _GraphValue<TupleView<T>>,
+        inputs: _ViewListInputs
+    ) -> _ViewListOutputs {
+        let tupleType = TupleType(T.self)
+        let contentTypes = ViewDescriptor.tupleDescription(tupleType).contentTypes
+        var makeList = MakeList(
+            view: view,
+            inputs: inputs,
+            index: 0,
+            offset: 0,
+            wrapChildren: inputs.options.contains(.tupleViewCreatesUnaryElements),
+            outputs: []
+        )
+        if inputs.options.contains(.tupleViewCreatesUnaryElements),
+           makeList.inputs.options.contains(.tupleViewCreatesUnaryElements) {
+            makeList.inputs.options.subtract([.requiresSections, .tupleViewCreatesUnaryElements])
+        }
+        for (index, conformance) in contentTypes {
+            makeList.index = index
+            makeList.offset = tupleType.elementOffset(at: index)
+            conformance.visitType(visitor: &makeList)
+        }
+        return _ViewListOutputs.concat(makeList.outputs, inputs: inputs)
+    }
+
+    @available(OpenSwiftUI_v2_0, *)
+    public static func _viewListCount(
+        inputs: _ViewListCountInputs
+    ) -> Int? {
+        let contentTypes = ViewDescriptor.tupleDescription(TupleType(T.self)).contentTypes
+        if inputs.options.contains(.tupleViewCreatesUnaryElements) {
+            return contentTypes.count
+        } else {
+            var countViews = CountViews(inputs: inputs, result: 0)
+            for contentType in contentTypes {
+                contentType.1.visitType(visitor: &countViews)
+            }
+            return countViews.result
+        }
+    }
+
+    private struct MakeUnary: ViewTypeVisitor {
+        var view: _GraphValue<TupleView<T>>
+        var inputs: _ViewInputs
+        var outputs: _ViewOutputs?
+
+        mutating func visit<V>(type: V.Type) where V: View {
+            outputs = V.makeDebuggableView(
+                view: view.unsafeBitCast(to: V.self),
+                inputs: inputs
+            )
+        }
+    }
+
+    private struct MakeList: ViewTypeVisitor {
+        var view: _GraphValue<TupleView<T>>
+        var inputs: _ViewListInputs
+        var index: Int
+        var offset: Int
+        let wrapChildren: Bool
+        var outputs: [_ViewListOutputs]
+
+        mutating func visit<V>(type: V.Type) where V : View {
+            inputs.base.pushStableIndex(index)
+            let view = view.value.unsafeOffset(at: offset, as: V.self)
+            let output = if wrapChildren {
+                _ViewListOutputs.unaryViewList(view: _GraphValue(view), inputs: inputs)
+            } else {
+                V.makeDebuggableViewList(view: _GraphValue(view), inputs: inputs)
+            }
+            outputs.append(output)
+            inputs.implicitID = output.nextImplicitID
+        }
+    }
+
+    private struct CountViews: ViewTypeVisitor {
+        var inputs: _ViewListCountInputs
+        var result: Int?
+
+        mutating func visit<V>(type: V.Type) where V : View {
+            guard let oldResult = result,
+                  let count = V._viewListCount(inputs: inputs)
+            else {
+                result = nil
+                return
+            }
+            result = oldResult + count
+        }
+    }
 }
 
 @available(*, unavailable)
