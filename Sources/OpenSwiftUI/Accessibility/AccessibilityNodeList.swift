43c58951abce79f73c1912c41f932f2e84e61d71
Kyle <kyle201817146@gmail.com>
2025-06-21 23:20:16 +0800

    Update Identified API

---
+++ Sources/OpenSwiftUI/Accessibility/AccessibilityNodeList.swift
@@ -0,0 +1,41 @@
+import OpenGraphShims
+
+// FIXME
+struct AccessibilityNodeList {
+    var nodes: [AccessibilityNode]
+    var version: DisplayList.Version
+}
+
+class AccessibilityNode {
+    // TODO
+}
+
+// TODO
+struct AccessibilityNodeProxy {
+    static func makeProxyForIdentifiedView(
+        with list: AccessibilityNodeList?,
+        environment: EnvironmentValues
+    ) -> AccessibilityNodeProxy? {
+        nil
+    }
+}
+
+// MARK: - AccessibilityNodesKey [6.4.41]
+
+struct AccessibilityNodesKey: PreferenceKey {
+    static let defaultValue = AccessibilityNodeList(nodes: [], version: .init())
+
+    static func reduce(value: inout AccessibilityNodeList, nextValue: () -> AccessibilityNodeList) {
+        let next = nextValue()
+        value.version.combine(with: next.version)
+        value.nodes.append(contentsOf: next.nodes)
+    }
+}
+
+extension _ViewOutputs {
+    @inline(__always)
+    var accessibilityNodes: Attribute<AccessibilityNodeList>? {
+        get { self[AccessibilityNodesKey.self] }
+        set { self[AccessibilityNodesKey.self] = newValue }
+    }
+}
--- Sources/OpenSwiftUICore/View/IdentifiedView/IdentifiedViewProxy.swift
+++ Sources/OpenSwiftUICore/View/IdentifiedView/IdentifiedViewProxy.swift
@@ -2,11 +2,14 @@
 //  IdentifiedViewProxy.swift
 //  OpenSwiftUICore
 //
-//  Audited for iOS 18.0
 //  Status: Complete
 
 public import Foundation
+import OpenGraphShims
 
+// MARK: - _IdentifiedViewProxy [6.4.41]
+
+@available(OpenSwiftUI_v1_0, *)
 public struct _IdentifiedViewProxy {
     public var identifier: AnyHashable
     package var size: CGSize
@@ -36,6 +39,8 @@ public struct _IdentifiedViewProxy {
 @available(*, unavailable)
 extension _IdentifiedViewProxy: Sendable {}
 
+// MARK: - IdentifiedViewPlatformInputs [6.4.41]
+
 package struct IdentifiedViewPlatformInputs {
     package init(inputs: _ViewInputs, outputs: _ViewOutputs) {}
 }
@@ -46,6 +51,78 @@ extension _IdentifiedViewProxy {
     }
 }
 
+// MARK: - IdentifierProvider [6.4.41]
+
 package protocol IdentifierProvider {
     func matchesIdentifier<I>(_ identifier: I) -> Bool where I: Hashable
+}
+
+extension _BenchmarkHost {
+    public func viewForIdentifier<I, V>(
+        _ identifier: I,
+        _ type: V.Type
+    ) -> V? where I: Hashable, V: View {
+        guard let render = self as? ViewRendererHost else {
+            return nil
+        }
+        return render.findIdentifier(identifier, root: nil) { attribute in
+            var predicate = ViewValuePredicate<V>(view: nil)
+            _ = attribute.breadthFirstSearch(options: ._2) { anyAttribute in
+                predicate.apply(to: anyAttribute)
+            }
+            return predicate.view
+        }
+    }
+
+    public func stateForIdentifier<I, S, V>(
+        _ id: I,
+        type stateType: S.Type,
+        in viewType: V.Type
+    ) -> Binding<S>? where I: Hashable, V: View {
+        guard let render = self as? ViewRendererHost else {
+            return nil
+        }
+        return render.stateForIdentifier(id, type: stateType, in: viewType)
+    }
+}
+
+extension ViewRendererHost {
+    func stateForIdentifier<I, S, V>(
+        _ id: I,
+        type stateType: S.Type,
+        in viewType: V.Type
+    ) -> Binding<S>? where I: Hashable, V: View {
+        findIdentifier(id, root: nil) { attribute in
+            var predicate = ViewStatePredicate<V, S>()
+            _ = attribute.breadthFirstSearch(options: ._2) { anyAttribute in
+                predicate.apply(to: anyAttribute)
+            }
+            return predicate.state
+        }
+    }
+
+    func findIdentifier<I, V>(
+        _ identifier: I,
+        root: AnyAttribute?,
+        filter: (AnyAttribute) -> V?
+    ) -> V? where I: Hashable {
+        let root = root ?? viewGraph.rootView
+        var v: V? = nil
+        _ = root.breadthFirstSearch(options: ._2) { attribute in
+            func project(type: any Any.Type) -> Bool {
+                // FIXME: type
+                guard let provider = attribute._bodyPointer as? IdentifierProvider else {
+                    return false
+                }
+                guard provider.matchesIdentifier(identifier),
+                      let value = filter(attribute) else {
+                    return false
+                }
+                v = value
+                return true
+            }
+            return project(type: attribute._bodyType)
+        }
+        return v
+    }
 }
