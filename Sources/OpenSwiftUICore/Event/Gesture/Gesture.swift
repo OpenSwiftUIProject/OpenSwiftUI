adbdb444f4389aa606fe6965aae926b5ec274040
Kyle <kyle201817146@gmail.com>
2025-06-29 13:29:46 +0800

    Update _GestureOutputs implementation

--- Sources/OpenSwiftUICore/Event/Gesture/Gesture.swift
+++ Sources/OpenSwiftUICore/Event/Gesture/Gesture.swift
@@ -228,63 +228,86 @@ extension _GestureInputs: Sendable {}
 @available(*, unavailable)
 extension _GestureInputs.InheritedPhase: Sendable {}
 
-// MARK: - GestureOutputs [6.5.4] [WIP]
+// MARK: - GestureOutputs [6.5.4]
 
 /// Output (aka synthesized) attributes for gesture objects.
 @available(OpenSwiftUI_v1_0, *)
 public struct _GestureOutputs<Value> {
     package var phase: Attribute<GesturePhase<Value>>
 
+    private var _debugData: OptionalAttribute<GestureDebug.Data>
+
     package var preferences: PreferencesOutputs
 
     package var debugData: Attribute<GestureDebug.Data>? {
-        get { preconditionFailure("TODO") }
-        set { preconditionFailure("TODO") }
+        get { _debugData.attribute }
+        set { _debugData.attribute = newValue }
     }
 
     package init(phase: Attribute<GesturePhase<Value>>) {
-        preconditionFailure("TODO")
+        self.phase = phase
+        self._debugData = .init()
+        self.preferences = .init()
     }
 
     package func withPhase<T>(_ phase: Attribute<GesturePhase<T>>) -> _GestureOutputs<T> {
-        preconditionFailure("TODO")
+        var outputs = _GestureOutputs<T>(phase: phase)
+        outputs._debugData = _debugData
+        outputs.preferences = preferences
+        return outputs
     }
 
     package func overrideDefaultValues(_ childOutputs: _GestureOutputs<Value>) {
-        preconditionFailure("TODO")
+        phase.overrideDefaultValue(childOutputs.phase, type: GesturePhase<Value>.self)
+        if let debugData, let childDebugData = childOutputs.debugData {
+            debugData.overrideDefaultValue(childDebugData, type: GestureDebug.Data.self)
+        }
+        preferences.attachIndirectOutputs(to: childOutputs.preferences)
     }
 
     package func setIndirectDependency(_ dependency: AnyAttribute?) {
-        preconditionFailure("TODO")
+        phase.identifier.indirectDependency = dependency
+        if let debugData {
+            debugData.identifier.indirectDependency = dependency
+        }
+        preferences.setIndirectDependency(dependency)
     }
 
     package func attachIndirectOutputs(_ childOutputs: _GestureOutputs<Value>) {
-        preconditionFailure("TODO")
+        phase.identifier.source = childOutputs.phase.identifier
+        if let debugData, let childDebugData = childOutputs.debugData {
+            debugData.identifier.source = childDebugData.identifier
+        }
+        preferences.attachIndirectOutputs(to: childOutputs.preferences)
     }
 
     package func detachIndirectOutputs() {
-        preconditionFailure("TODO")
+        phase.identifier.source = .nil
+        if let debugData {
+            debugData.identifier.source = .nil
+        }
+        preferences.detachIndirectOutputs()
     }
 
     package subscript(anyKey key: any PreferenceKey.Type) -> AnyAttribute? {
-        get { preconditionFailure("TODO") }
-        set { preconditionFailure("TODO") }
+        get { preferences[anyKey: key] }
+        set { preferences[anyKey: key] = newValue }
     }
 
     package subscript<K>(key: K.Type) -> Attribute<K.Value>? where K: PreferenceKey {
-        get { preconditionFailure("TODO") }
-        set { preconditionFailure("TODO") }
+        get { preferences[key] }
+        set { preferences[key] = newValue }
     }
 
     package mutating func appendPreference<K>(
         key: K.Type,
         value: Attribute<K.Value>
     ) where K: PreferenceKey {
-        preconditionFailure("TODO")
+        preferences.appendPreference(key: key, value: value)
     }
 
     package func forEachPreference(_ body: (any PreferenceKey.Type, AnyAttribute) -> Void) {
-        preconditionFailure("TODO")
+        preferences.forEachPreference(body)
     }
 }
 
