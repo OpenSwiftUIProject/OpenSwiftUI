c55f0d5a0dc64010018eac333f7b8f7386d164b6
Kyle <kyle201817146@gmail.com>
2025-06-29 16:26:57 +0800

    Add API interface for MapGesture and RepeatGesture

---
+++ Sources/OpenSwiftUICore/Event/Gesture/RepeatGesture.swift
@@ -0,0 +1,59 @@
+//
+//  RepeatGesture.swift
+//  OpenSwiftUICore
+//
+//  Status: Unimplmented
+//  ID: BECD07FC80B4CA0BF429B041392E806A (SwiftUICore)
+
+import OpenGraphShims
+
+// MARK: - RepeatGesture [6.5.4]
+
+extension Gesture {
+    package func repeatCount(
+        _ count: Int,
+        maximumDelay: Double = 0.35
+    ) -> ModifierGesture<RepeatGesture<Value>, Self> {
+        modifier(RepeatGesture(count: count, maximumDelay: maximumDelay))
+    }
+}
+
+package struct RepeatGesture<Value>: GestureModifier {
+    package var count: Int
+    package var maximumDelay: Double
+
+    package init(count: Int, maximumDelay: Double = 0.35) {
+        self.count = count
+        self.maximumDelay = maximumDelay
+    }
+
+    package static func _makeGesture(
+        modifier: _GraphValue<RepeatGesture<Value>>,
+        inputs: _GestureInputs,
+        body: (_GestureInputs) -> _GestureOutputs<Value>
+    ) -> _GestureOutputs<Value> {
+        openSwiftUIUnimplementedFailure()
+    }
+}
+
+private struct RepeatPhase<V>: ResettableGestureRule {
+    @Attribute var modifier: RepeatGesture<V>
+    @Attribute var phase: GesturePhase<V>
+    @Attribute var time: Time
+    @Attribute var resetSeed: UInt32
+    @Attribute var resetDelta: UInt32
+    var useGestureGraph: Bool
+    var deadline: Time?
+    var index: UInt32
+    var lastResetSeed: UInt32
+
+    typealias PhaseValue = V
+    typealias Value = GesturePhase<V>
+
+    mutating func updateValue() {
+        guard resetIfNeeded() else {
+            return
+        }
+        openSwiftUIUnimplementedFailure()
+    }
+}
