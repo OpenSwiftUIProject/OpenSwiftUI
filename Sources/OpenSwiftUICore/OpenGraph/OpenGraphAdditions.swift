40796a88864d1855235338ad3a8c3a40698ea994
Kyle <kyle201817146@gmail.com>
2025-06-29 14:00:01 +0800

    Add OpenGraphAdditions interface (#357)

---
+++ Sources/OpenSwiftUICore/OpenGraph/OpenGraphAdditions.swift
@@ -0,0 +1,286 @@
+//
+//  OpenGraphAdditions.swift
+//  OpenSwiftUICore
+//
+//  Status: WIP
+//  ID: 372497ED4F569296C4450147CA418CD0 (SwiftUICore)
+
+package import OpenGraphShims
+
+// FIXME
+extension OGSearchOptions {
+    package static var _2: OGSearchOptions {
+        .init(rawValue: 2)
+    }
+}
+
+// FIXME
+extension Subgraph {
+    package typealias Flags = OGAttributeFlags
+    package typealias ChildFlags = OGAttributeFlags
+}
+
+extension Graph {
+    package typealias TraceOptions = TraceFlags
+}
+
+
+// MARK: - Defaultable [6.5.4]
+
+package protocol Defaultable {
+    associatedtype Value
+
+    static var defaultValue: Value { get }
+}
+
+// MARK: - AsyncAttribute
+
+package protocol AsyncAttribute: _AttributeBody {}
+
+extension AsyncAttribute {
+    // FIXME
+    package typealias Flags = OGAttributeTypeFlags
+
+    package static var flags: Flags { [] }
+}
+
+// MARK: - DefaultRule [WIP]
+
+package struct DefaultRule<T>: Rule, AsyncAttribute, CustomStringConvertible where T: Defaultable {
+    package init() {
+        preconditionFailure("TODO")
+    }
+
+    package static var initialValue: T.Value? {
+        get { preconditionFailure("TODO") }
+    }
+
+    package var value: T.Value {
+        get { preconditionFailure("TODO") }
+    }
+
+    package var description: String {
+        get { preconditionFailure("TODO") }
+    }
+
+    package typealias Value = T.Value
+}
+
+extension Attribute {
+    package func overrideDefaultValue<T>(
+        _ value: Attribute<Value>?,
+        type: T.Type
+    ) where Value == T.Value, T: Defaultable {
+        preconditionFailure("TODO")
+    }
+
+    package func invalidateValueIfNeeded() -> Bool {
+        preconditionFailure("TODO")
+    }
+
+    package func unsafeBitCast<T>(to _: T.Type) -> Attribute<T> {
+        unsafeOffset(at: 0, as: T.self)
+    }
+}
+
+package protocol RemovableAttribute: _AttributeBody {
+    static func willRemove(attribute: AnyAttribute)
+    static func didReinsert(attribute: AnyAttribute)
+}
+
+package protocol InvalidatableAttribute: _AttributeBody {
+    static func willInvalidate(attribute: AnyAttribute)
+}
+
+extension AnyAttribute.Flags {
+    package static var transactional: Subgraph.Flags {
+        get { preconditionFailure("TODO") }
+    }
+
+    package static var removable: Subgraph.Flags {
+        get { preconditionFailure("TODO") }
+    }
+
+    package static var invalidatable: Subgraph.Flags {
+        get { preconditionFailure("TODO") }
+    }
+
+    package static var scrapeable: Subgraph.Flags {
+        get { preconditionFailure("TODO") }
+    }
+}
+
+extension Subgraph.ChildFlags {
+    package static var secondary: Subgraph.ChildFlags {
+        get { preconditionFailure("TODO") }
+    }
+}
+
+extension Subgraph {
+    package func addSecondaryChild(_ child: Subgraph) {
+        preconditionFailure("TODO")
+    }
+
+    package func willRemove() {
+        forEach(.removable) { attribute in
+            let type = attribute._bodyType
+            if let removableType = type as? RemovableAttribute.Type {
+                removableType.willRemove(attribute: attribute)
+            }
+        }
+    }
+
+    package func didReinsert() {
+        forEach(.removable) { attribute in
+            let type = attribute._bodyType
+            if let removableType = type as? RemovableAttribute.Type {
+                removableType.didReinsert(attribute: attribute)
+            }
+        }
+    }
+
+    package func willInvalidate(isInserted: Bool) {
+        forEach(isInserted ? [.removable, .invalidatable] : [.invalidatable]) { attribute in
+            let type = attribute._bodyType
+            if let invalidatableType = type as? InvalidatableAttribute.Type {
+                invalidatableType.willInvalidate(attribute: attribute)
+            } else if isInserted, let removableType = type as? RemovableAttribute.Type {
+                removableType.willRemove(attribute: attribute)
+            }
+        }
+    }
+}
+
+extension Attribute {
+    package func syncMainIfReferences<T>(do body: (Value) -> T) -> T {
+        let (value, flags) = valueAndFlags(options: [._2])
+        if flags.contains(.requiresMainThread) {
+            var result: T?
+            Update.syncMain {
+                result = body(value)
+            }
+            return result!
+        } else {
+            return body(value)
+        }
+    }
+
+    package func allowsAsyncUpdate() -> Bool {
+        preconditionFailure("TODO")
+    }
+}
+
+extension WeakAttribute {
+    package var uncheckedIdentifier: Attribute<Value> {
+        get { preconditionFailure("TODO") }
+    }
+
+    package func allowsAsyncUpdate() -> Bool {
+        preconditionFailure("TODO")
+    }
+}
+
+// MARK: - TreeElementFlags [6.5.4]
+
+package struct TreeElementFlags: OptionSet {
+    package let rawValue: UInt32
+
+    package init(rawValue: UInt32) {
+        self.rawValue = rawValue
+    }
+
+    package static let viewList: TreeElementFlags = .init(rawValue: 1 << 0)
+}
+
+// MARK: - TreeValueFlags [6.5.4]
+
+package struct TreeValueFlags: OptionSet {
+    package let rawValue: UInt32
+
+    package init(rawValue: UInt32) {
+        preconditionFailure("TODO")
+    }
+
+    // FIXME?
+    package static let stateSignal: TreeValueFlags = .init(rawValue: 1)
+    package static let environmentObjectSignal: TreeValueFlags = .init(rawValue: 2)
+    package static let observedObjectSignal: TreeValueFlags = .init(rawValue: 3)
+    package static let appStorageSignal: TreeValueFlags = .init(rawValue: 4)
+    package static let sceneStorageSignal: TreeValueFlags = .init(rawValue: 5)
+}
+
+// MARK: - Metadata Additions [6.5.4]
+
+extension Metadata {
+    package var isValueType: Bool {
+        switch kind {
+            case .struct, .enum, .optional, .tuple: true
+            default: false
+        }
+    }
+
+    // TODO: Optimize this implementation
+    package func genericType(at index: Int) -> any Any.Type {
+        UnsafeRawPointer(rawValue)
+            .advanced(by: index &* 8)
+            .advanced(by: 16)
+            .assumingMemoryBound(to: Any.Type.self)
+            .pointee
+    }
+
+    @inline(__always)
+    package func projectEnum(
+        at ptr: UnsafeRawPointer,
+        tag: Int,
+        _ body: (UnsafeRawPointer) -> Void
+    ) {
+        projectEnumData(UnsafeMutableRawPointer(mutating: ptr))
+        body(ptr)
+        injectEnumTag(tag: UInt32(tag), UnsafeMutableRawPointer(mutating: ptr))
+    }
+}
+
+@inline(__always)
+package func compareEnumTags<T>(_ v1: T, _ v2: T) -> Bool {
+    func tag(of value: T) -> Int {
+        withUnsafePointer(to: value) {
+            Int(Metadata(T.self).enumTag($0))
+        }
+    }
+    let tag1 = tag(of: v1)
+    let tag2 = tag(of: v2)
+    return tag1 == tag2
+}
+
+// MARK: - Attribute + toOptional [6.5.4]
+
+extension Attribute {
+    package var toOptional: Attribute<Value?> {
+        Attribute<Value?>(ToOptional(data: self))
+    }
+}
+
+private struct ToOptional<T>: Rule, AsyncAttribute {
+    @Attribute var data: T
+
+    typealias Value = T?
+
+    var value: T? { data }
+}
+
+// MARK: - Graph Additions
+
+extension Graph {
+    @inline(__always)
+    package static func cancelCurrentUpdateIfDeadlinePassed() -> Bool {
+        preconditionFailure("TODO")
+    }
+
+    package static func startTracing(options: Graph.TraceOptions? = nil) {
+        Graph.startTracing(nil, options: options ?? ProcessEnvironment.tracingOptions)
+    }
+
+    package static func stopTracing() {
+        Graph.stopTracing(nil)
+    }
+}
