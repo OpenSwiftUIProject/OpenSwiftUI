8f324b88b9c3a3e5ad4c030e6457dac0fa4d7185
Kyle <kyle201817146@gmail.com>
2025-07-12 11:02:19 +0800

    Add OffsetEffectUITests and IgnoredByLayoutEffectUITests

---
+++ Example/OpenSwiftUIUITests/Render/GeometryEffect/IgnoredByLayoutEffectUITests.swift
@@ -0,0 +1,40 @@
+//
+//  IgnoredByLayoutEffectUITests.swift
+//  OpenSwiftUIUITests
+
+import Testing
+import SnapshotTesting
+
+@MainActor
+@Suite(.snapshots(record: .never, diffTool: diffTool))
+struct IgnoredByLayoutEffectUITests {
+    @Test(.disabled("Animation is not implmemented yet"))
+    func offsetIgnoredByLayout() {
+        struct ContentView: View {
+            var body: some View {
+                WobbleColorView()
+            }
+        }
+
+        struct WobbleEffect: GeometryEffect {
+            var amount: CGFloat = 10
+            var shakesPerUnit = 3
+            var animatableData: CGFloat
+
+            nonisolated func effectValue(size: CGSize) -> ProjectionTransform {
+                let translation = amount * sin(animatableData * .pi * CGFloat(shakesPerUnit))
+                return ProjectionTransform(CGAffineTransform(translationX: translation, y: 0))
+            }
+        }
+
+        struct WobbleColorView: View {
+            @State private var wobble = false
+
+            var body: some View {
+                Color.red.frame(width: 200, height: 200)
+                    .modifier(_OffsetEffect(offset: CGSize(width: 0, height: 100)).ignoredByLayout())
+            }
+        }
+        openSwiftUIAssertSnapshot(of: ContentView())
+    }
+}
