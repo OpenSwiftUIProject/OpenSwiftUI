11dbb216803b01d4f074ab05cbf6ff197797f7d9
Kyle <kyle201817146@gmail.com>
2025-07-12 11:38:26 +0800

    Add Spacer support (#380)
    * Add Spacer implementation
    
    * Add Spacer UI Tests

---
+++ Sources/OpenSwiftUI/Integration/PlatformItemList.swift
@@ -0,0 +1,68 @@
+//
+//  PlatformItemList.swift
+//  OpenSwiftUI
+//
+//  Status: Empty
+//  ID: CE84B1BFBEAEAB6361605407E54625A3 (SwiftUI)
+
+// FIXME
+struct PlatformItemList {
+    var itesms: [Item]
+
+    // FIXME
+    struct Item {}
+
+    fileprivate struct Key: PreferenceKey {
+        static let defaultValue: PlatformItemList = .init(itesms: [])
+
+        static func reduce(value: inout PlatformItemList, nextValue: () -> PlatformItemList) {
+            value.itesms.append(contentsOf: nextValue().itesms)
+        }
+    }
+}
+
+extension PreferencesInputs {
+    @inline(__always)
+    var requiresPlatformItemList: Bool {
+        get {
+            contains(PlatformItemList.Key.self)
+        }
+        set {
+            if newValue {
+                add(PlatformItemList.Key.self)
+            } else {
+                remove(PlatformItemList.Key.self)
+            }
+        }
+    }
+}
+
+// MARK: - PlatformItemListSpacerRepresentable [6.5.4] [WIP]
+
+struct PlatformItemListSpacerRepresentable: PlatformSpacerRepresentable {
+    static func shouldMakeRepresentation(inputs: _ViewInputs) -> Bool {
+        guard inputs.preferences.requiresPlatformItemList else {
+            return false
+        }
+        return inputs[PlatformItemListFlagsInput.self].contains(.init(rawValue: 1 << 3)) // FIXME
+    }
+    
+    static func makeRepresentation(inputs: _ViewInputs, outputs: inout _ViewOutputs) {
+        // outputs.preferences.makePreferenceWriter
+        _openSwiftUIUnimplementedFailure()
+    }
+}
+
+// MARK: PlatformItemListFlagsInput [WIP]
+
+struct PlatformItemListFlagsInput: ViewInput {
+    static var defaultValue: PlatformItemListFlagsSet = []
+}
+
+struct PlatformItemListFlagsSet: OptionSet {
+    var rawValue: UInt32
+
+    init(rawValue: UInt32) {
+        self.rawValue = rawValue
+    }
+}
