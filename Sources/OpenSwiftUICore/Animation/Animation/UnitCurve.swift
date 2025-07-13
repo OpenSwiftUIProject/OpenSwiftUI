6d6a43a13df5df2f9bcb901265f104cdb9bfbcf7
Kyle <kyle201817146@gmail.com>
2025-07-13 23:34:40 +0800

    Add UnitCurve implementation

---
+++ Sources/OpenSwiftUICore/Animation/Animation/UnitCurve.swift
@@ -0,0 +1,334 @@
+//
+//  UnitCurve.swift
+//  OpenSwiftUICore
+//
+//  Audited for 6.5.4
+//  Status: Blocked by CubicSolver implementation
+//  ID: 54864F491103B6AE5CAC10D2D922245F (SwiftUICore)
+
+package import Foundation
+
+// MARK: - UnitCurve
+
+/// A  function defined by a two-dimensional curve that maps an input
+/// progress in the range [0,1] to an output progress that is also in the
+/// range [0,1]. By changing the shape of the curve, the effective speed
+/// of an animation or other interpolation can be changed.
+///
+/// The horizontal (x) axis defines the input progress: a single input
+/// progress value in the range [0,1] must be provided when evaluating a
+/// curve.
+///
+/// The vertical (y) axis maps to the output progress: when a curve is
+/// evaluated, the y component of the point that intersects the input progress
+/// is returned.
+@available(OpenSwiftUI_v5_0, *)
+public struct UnitCurve {
+    package var function: UnitCurve.Function
+
+    /// Creates a new curve using bezier control points.
+    ///
+    /// The x components of the control points are clamped to the range [0,1] when
+    /// the curve is evaluated.
+    ///
+    /// - Parameters:
+    ///   - startControlPoint: The cubic Bézier control point associated with
+    ///     the curve's start point at (0, 0). The tangent vector from the
+    ///     start point to its control point defines the initial velocity of
+    ///     the timing function.
+    ///   - endControlPoint: The cubic Bézier control point associated with the
+    ///     curve's end point at (1, 1). The tangent vector from the end point
+    ///     to its control point defines the final velocity of the timing
+    ///     function.
+    public static func bezier(
+        startControlPoint: UnitPoint,
+        endControlPoint: UnitPoint
+    ) -> UnitCurve {
+        .init(
+            function: .bezier(
+                startControlPoint: startControlPoint,
+                endControlPoint: endControlPoint
+            )
+        )
+    }
+
+    /// Returns the output value (y component) of the curve at the given time.
+    ///
+    /// - Parameters:
+    ///   - time: The input progress (x component). The provided value is
+    ///     clamped to the range [0,1].
+    ///
+    /// - Returns: The output value (y component) of the curve at the given
+    ///   progress.
+    public func value(at progress: Double) -> Double {
+        _openSwiftUIUnimplementedFailure()
+    }
+
+    /// Returns the rate of change (first derivative) of the output value of
+    /// the curve at the given time.
+    ///
+    /// - Parameters:
+    ///   - progress: The input progress (x component). The provided value is
+    ///     clamped to the range [0,1].
+    ///
+    /// - Returns: The velocity of the output value (y component) of the curve
+    ///   at the given time.
+    public func velocity(at progress: Double) -> Double {
+        _openSwiftUIUnimplementedFailure()
+    }
+
+    /// Returns a copy of the curve with its x and y components swapped.
+    ///
+    /// The inverse can be used to solve a curve in reverse: given a
+    /// known output (y) value, the corresponding input (x) value can be found
+    /// by using `inverse`:
+    ///
+    ///     let curve = UnitCurve.easeInOut
+    ///
+    ///     /// The input time for which an easeInOut curve returns 0.6.
+    ///     let inputTime = curve.inverse.evaluate(at: 0.6)
+    ///
+    public var inverse: UnitCurve {
+        switch function {
+        case .linear:
+            .init(function: .linear)
+        case .circularEaseIn:
+            .init(function: .circularEaseOut)
+        case .circularEaseOut:
+            .init(function: .circularEaseIn)
+        case .circularEaseInOut:
+            .init(function: .circularEaseInOut)
+        case let .bezier(startControlPoint, endControlPoint):
+            .bezier(
+                startControlPoint: UnitPoint(x: startControlPoint.y, y: startControlPoint.x),
+                endControlPoint: UnitPoint(x: endControlPoint.y, y: endControlPoint.x)
+            )
+        }
+    }
+}
+
+extension UnitCurve {
+    package enum Function: Hashable {
+        case linear
+        case circularEaseIn
+        case circularEaseOut
+        case circularEaseInOut
+        case bezier(startControlPoint: UnitPoint, endControlPoint: UnitPoint)
+    }
+}
+
+@available(OpenSwiftUI_v5_0, *)
+extension UnitCurve: Sendable {}
+
+@available(OpenSwiftUI_v5_0, *)
+extension UnitCurve: Hashable {}
+
+@available(OpenSwiftUI_v5_0, *)
+extension UnitCurve {
+
+    /// A bezier curve that starts out slowly, speeds up over the middle, then
+    /// slows down again as it approaches the end.
+    ///
+    /// The start and end control points are located at (x: 0.42, y: 0) and
+    /// (x: 0.58, y: 1).
+    @available(*, deprecated, message: "Use easeInOut instead")
+    public static let easeInEaseOut: UnitCurve = .easeInOut
+
+    /// A bezier curve that starts out slowly, speeds up over the middle, then
+    /// slows down again as it approaches the end.
+    ///
+    /// The start and end control points are located at (x: 0.42, y: 0) and
+    /// (x: 0.58, y: 1).
+    public static let easeInOut: UnitCurve = .init(
+        function: .bezier(
+            startControlPoint: UnitPoint(x: 0.42, y: 0),
+            endControlPoint: UnitPoint(x: 0.58, y: 1)
+        )
+    )
+
+    /// A bezier curve that starts out slowly, then speeds up as it finishes.
+    ///
+    /// The start and end control points are located at (x: 0.42, y: 0) and
+    /// (x: 1, y: 1).
+    public static let easeIn: UnitCurve = .init(
+        function: .bezier(
+            startControlPoint: UnitPoint(x: 0.42, y: 0),
+            endControlPoint: UnitPoint(x: 1, y: 1)
+        )
+    )
+
+    /// A bezier curve that starts out quickly, then slows down as it
+    /// approaches the end.
+    ///
+    /// The start and end control points are located at (x: 0, y: 0) and
+    /// (x: 0.58, y: 1).
+    public static let easeOut: UnitCurve = .init(
+        function: .bezier(
+            startControlPoint: UnitPoint(x: 0, y: 0),
+            endControlPoint: UnitPoint(x: 0.58, y: 1)
+        )
+    )
+
+    /// A curve that starts out slowly, then speeds up as it finishes.
+    ///
+    /// The shape of the curve is equal to the fourth (bottom right) quadrant
+    /// of a unit circle.
+    public static let circularEaseIn: UnitCurve = .init(function: .circularEaseIn)
+
+    /// A circular curve that starts out quickly, then slows down as it
+    /// approaches the end.
+    ///
+    /// The shape of the curve is equal to the second (top left) quadrant of
+    /// a unit circle.
+    public static let circularEaseOut: UnitCurve = .init(function: .circularEaseOut)
+
+    /// A circular curve that starts out slowly, speeds up over the middle,
+    /// then slows down again as it approaches the end.
+    ///
+    /// The shape of the curve is defined by a piecewise combination of
+    /// `circularEaseIn` and `circularEaseOut`.
+    public static let circularEaseInOut: UnitCurve = .init(function: .circularEaseInOut)
+
+    /// A linear curve.
+    ///
+    /// As the linear curve is a straight line from (0, 0) to (1, 1),
+    /// the output progress is always equal to the input progress, and
+    /// the velocity is always equal to 1.0.
+    public static let linear: UnitCurve = .init(function: .linear)
+}
+
+// MARK: - UnitCurve.CubicSolver [WIP]
+
+extension UnitCurve {
+    package struct CubicSolver: Sendable, Hashable {
+        private var ax: Double
+        private var bx: Double
+        private var cx: Double
+        private var ay: Double
+        private var by: Double
+        private var cy: Double
+
+        package init(startControlPoint: UnitPoint, endControlPoint: UnitPoint) {
+            cx = startControlPoint.x * 3.0
+            bx = (endControlPoint.x - startControlPoint.x) * 3.0 - cx
+            ax = 1.0 - cx - bx
+            cy = startControlPoint.y * 3.0
+            by = (endControlPoint.y - startControlPoint.y) * 3.0 - cy
+            ay = 1.0 - cy - by
+        }
+
+        package var startControlPoint: UnitPoint {
+            UnitPoint(x: cx / 3.0, y: cy / 3.0)
+        }
+
+        package var endControlPoint: UnitPoint {
+            UnitPoint(x: (bx + cx) / 3.0 + cx / 3.0, y: (by + cy) / 3.0 + cy / 3.0)
+        }
+
+        package func value(at time: Double) -> Double {
+            _openSwiftUIUnimplementedFailure()
+        }
+
+        package func velocity(at time: Double) -> Double {
+            _openSwiftUIUnimplementedFailure()
+        }
+
+        private func solveX(_: Double, epsilon: Double) -> Double {
+            _openSwiftUIUnimplementedFailure()
+        }
+    }
+}
+
+// MARK: - CubicSolver + ProtobufMessage
+
+extension UnitCurve.CubicSolver: ProtobufMessage {
+    package func encode(to encoder: inout ProtobufEncoder) {
+        encoder.doubleField(1, startControlPoint.x)
+        encoder.doubleField(2, startControlPoint.y)
+        encoder.doubleField(3, endControlPoint.x)
+        encoder.doubleField(4, endControlPoint.y)
+    }
+
+    package init(from decoder: inout ProtobufDecoder) throws {
+        var startControlPoint: UnitPoint = .zero
+        var endControlPoint: UnitPoint = .zero
+        while let field = try decoder.nextField() {
+            switch field.tag {
+            case 1: startControlPoint.x = try decoder.doubleField(field)
+            case 2: startControlPoint.y = try decoder.doubleField(field)
+            case 3: endControlPoint.x = try decoder.doubleField(field)
+            case 4: endControlPoint.y = try decoder.doubleField(field)
+            default: try decoder.skipField(field)
+            }
+        }
+        self.init(startControlPoint: startControlPoint, endControlPoint: endControlPoint)
+    }
+}
+
+// MARK: - UnitCurve + BezierTimingFunction
+
+extension UnitCurve {
+    package init<T>(_ curve: BezierTimingFunction<T>) where T: BinaryFloatingPoint {
+        self.init(function: .bezier(
+            startControlPoint: UnitPoint(x: .init(curve.p1x), y: .init(curve.p1y)),
+            endControlPoint: UnitPoint(x: .init(curve.p2x), y: .init(curve.p2y))
+        ))
+    }
+}
+
+// MARK: - UnitCurveAnimation
+
+package struct UnitCurveAnimation: InternalCustomAnimation {
+    package var duration: TimeInterval
+    package var curve: UnitCurve
+
+    package init(curve: UnitCurve, duration: TimeInterval) {
+        self.duration = duration
+        self.curve = curve
+    }
+
+    @_specialize(exported: false, kind: partial, where V == Double)
+    @_specialize(exported: false, kind: partial, where V == AnimatablePair<AnimatablePair<CGFloat, CGFloat>, AnimatablePair<CGFloat, CGFloat>>)
+    package func animate<V>(
+        value: V,
+        time: TimeInterval,
+        context: inout AnimationContext<V>
+    ) -> V? where V: VectorArithmetic {
+        guard duration >= time, duration > 0 else {
+            return nil
+        }
+        let factor = curve.value(at: (time / duration).clamp(min: 0.0, max: 1.0))
+        return value.scaled(by: factor)
+    }
+
+    package func velocity<V>(
+        value: V,
+        time: TimeInterval,
+        context: AnimationContext<V>
+    ) -> V? where V: VectorArithmetic {
+        guard duration >= time, duration > 0 else {
+            return nil
+        }
+        let factor = curve.velocity(at: (time / duration).clamp(min: 0.0, max: 1.0))
+        return value.scaled(by: 1 / duration).scaled(by: factor)
+    }
+
+    package var function: Animation.Function {
+        switch curve.function {
+        case .linear:
+            .linear(duration: duration)
+        case .circularEaseIn:
+            .circularEaseIn(duration: duration)
+        case .circularEaseOut:
+            .circularEaseOut(duration: duration)
+        case .circularEaseInOut:
+            .circularEaseInOut(duration: duration)
+        case let .bezier(startControlPoint, endControlPoint):
+            .bezier(
+                duration: duration,
+                cp1: .init(x: startControlPoint.x, y: startControlPoint.y),
+                cp2: .init(x: endControlPoint.x, y: endControlPoint.y)
+            )
+        }
+    }
+}
