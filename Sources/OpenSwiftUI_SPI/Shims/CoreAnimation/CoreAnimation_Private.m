58bb64c936fc067d7456075cecb65fbee3067945
Kyle <kyle201817146@gmail.com>
2025-07-19 01:32:48 +0800

    Add safe wrapper for CADisplayLink

--- Sources/OpenSwiftUI_SPI/Shims/CoreAnimation/CoreAnimation_Private.m
+++ Sources/OpenSwiftUI_SPI/Shims/CoreAnimation/CoreAnimation_Private.m
@@ -44,6 +44,17 @@
 @end
 
 @implementation CADisplayLink (OpenSwiftUI_SPI)
+
++ (instancetype)displayLinkWithDisplay_openswiftui_safe_wrapper:(CADisplay *)display target:(id)target selector:(SEL)targetSelector {
+    OPENSWIFTUI_SAFE_WRAPPER_IMP(CADisplayLink *, @"displayLinkWithDisplay:target:selector:", nil, CADisplay *, id, SEL);
+    return func(self, selector, display, target, targetSelector);
+}
+
+- (CADisplay *)display_openswiftui_safe_wrapper {
+    OPENSWIFTUI_SAFE_WRAPPER_IMP(CADisplay *, @"display", nil);
+    return func(self, selector);
+}
+
 - (void)setHighFrameRateReasons_openswiftui_safe_wrapper:(const uint32_t *)reasons count:(NSInteger)count {
     OPENSWIFTUI_SAFE_WRAPPER_IMP(void, @"setHighFrameRateReasons:count:", , const uint32_t *, NSInteger);
     return func(self, selector, reasons, count);
