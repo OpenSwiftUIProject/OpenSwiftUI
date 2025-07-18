a6e6a95dd4dc39c7e31a634913131bca9c6d40bb
Kyle <kyle201817146@gmail.com>
2025-07-19 00:04:41 +0800

    Add ColorAnimationExample

---
+++ Example/SharedExample/Animation/ColorAnimationExample.swift
@@ -0,0 +1,28 @@
+//
+//  ColorAnimationExample.swift
+//  SharedExample
+//
+//  Created by Kyle on 2025/7/20.
+//
+#if OPENSWIFTUI
+import OpenSwiftUI
+#else
+import SwiftUI
+#endif
+import Foundation
+
+struct ColorAnimationExample: View {
+    @State private var showRed = false
+    var body: some View {
+        VStack {
+            Color(platformColor: showRed ? .red : .blue)
+                .frame(width: showRed ? 200 : 400, height: showRed ? 200 : 400)
+        }
+        .animation(.easeInOut(duration: 2), value: showRed)
+        .onAppear {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+                showRed.toggle()
+            }
+        }
+    }
+}
