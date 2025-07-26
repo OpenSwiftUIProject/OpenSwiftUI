2d6044b9039c2ea348a33e4a079c70b2f111c56c
Kyle <kyle201817146@gmail.com>
2025-07-26 22:45:53 +0800

    Fix compatibility alignment issue (#429)

--- Sources/OpenSwiftUITestsSupport/Integration/PlatformHostingControllerHelper.swift
+++ Sources/OpenSwiftUITestsSupport/Integration/PlatformHostingControllerHelper.swift
@@ -32,10 +32,10 @@ extension PlatformHostingController {
 
 // FIXME: A workaround to bypass the Issue #87
 package func workaroundIssue87(_ vc: PlatformViewController) {
-    #if OPENSWIFTUI
+//    #if OPENSWIFTUI
     // TODO: Use swift-test exist test feature to detect the crash instead or sliently workaroun it
     CrashWorkaround.shared.objects.append(vc)
-    #endif
+//    #endif
 }
 
 private final class CrashWorkaround {
