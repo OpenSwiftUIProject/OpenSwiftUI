7c335a14cc5c482c6a9658da2dcbfbb83e5a1aa8
Kyle <kyle201817146@gmail.com>
2025-07-27 23:42:26 +0800

    Update PlatformViewRepresentableLayoutOptions

--- Sources/OpenSwiftUI/Integration/Representable/Platform/PlatformViewRepresentableLayoutOptions.swift
+++ Sources/OpenSwiftUI/Integration/Representable/Platform/PlatformViewRepresentableLayoutOptions.swift
@@ -2,15 +2,17 @@
 //  PlatformViewRepresentableLayoutOptions.swift
 //  OpenSwiftUI
 //
-//  Audited for iOS 18.0
+//  Audited for 6.5.4
 //  Status: Complete
 
 /// Options for defining how a platform representable interacts with the
 /// bridging implementation.
+@available(OpenSwiftUI_v4_1, *)
 public struct _PlatformViewRepresentableLayoutOptions: OptionSet {
     public let rawValue: Int
 
     @_spi(Private)
+    @available(OpenSwiftUI_v5_0, *)
     public static let propagatesSafeArea: _PlatformViewRepresentableLayoutOptions = .init(rawValue: 1 << 2)
 
     public init(rawValue: Int) {
