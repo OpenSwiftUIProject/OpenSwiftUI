d099f2be38267cef0d67d19e6d646bca14d98ef9
Kyle <kyle201817146@gmail.com>
2025-08-16 22:57:09 +0800

    Update flaky AnimationCOmpletionCompatibilityTests

--- Tests/OpenSwiftUICompatibilityTests/Animation/Animation/AnimationCompletionCompatibilityTests.swift
+++ Tests/OpenSwiftUICompatibilityTests/Animation/Animation/AnimationCompletionCompatibilityTests.swift
@@ -25,56 +25,29 @@ struct AnimationCompletionCompatibilityTests {
             let confirmation: Confirmation
             var continuation: UnsafeContinuation<Void, Never>
 
+            @State private var showRed = false
+            @State private var scale = 1.0
+
             var body: some View {
-                VStack {
-                    LogicalCompletionView(confirmation: confirmation, continuation: continuation)
-                    RemovedCompletionView(confirmation: confirmation, continuation: continuation)
-                }
-            }
-
-            struct LogicalCompletionView: View {
-                @State private var showRed = false
-
-                let confirmation: Confirmation
-                var continuation: UnsafeContinuation<Void, Never>
-
-                var body: some View {
-                    VStack {
-                        Color(platformColor: showRed ? .red : .blue)
-                            .onAppear {
-                                let animation = Animation.linear(duration: 3)
-                                    .logicallyComplete(after: 1)
-                                withAnimation(animation, completionCriteria: .logicallyComplete) {
-                                    showRed.toggle()
-                                } completion: {
-                                    Helper.values.append(1)
-                                    confirmation()
-                                }
-                            }
+                Color(platformColor: showRed ? .red : .blue)
+                    .frame(width: 100 * scale, height: 100 * scale)
+                    .onAppear {
+                        let animation = Animation.linear(duration: 5)
+                            .logicallyComplete(after: 1)
+                        withAnimation(animation, completionCriteria: .logicallyComplete) {
+                            showRed.toggle()
+                        } completion: {
+                            Helper.values.append(1)
+                            confirmation()
+                        }
+                        withAnimation(animation, completionCriteria: .removed) {
+                            scale = 2.0
+                        } completion: {
+                            Helper.values.append(2)
+                            confirmation()
+                            continuation.resume()
+                        }
                     }
-                }
-            }
-            struct RemovedCompletionView: View {
-                @State private var showRed = false
-
-                let confirmation: Confirmation
-                var continuation: UnsafeContinuation<Void, Never>
-
-                var body: some View {
-                    VStack {
-                        Color(platformColor: showRed ? .red : .blue)
-                            .onAppear {
-                                let animation = Animation.linear(duration: 2)
-                                withAnimation(animation, completionCriteria: .removed) {
-                                    showRed.toggle()
-                                } completion: {
-                                    Helper.values.append(2)
-                                    confirmation()
-                                    continuation.resume()
-                                }
-                            }
-                    }
-                }
             }
         }
         
