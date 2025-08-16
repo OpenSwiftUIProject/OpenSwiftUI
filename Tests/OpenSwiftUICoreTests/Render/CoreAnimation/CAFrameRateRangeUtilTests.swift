301fef207a3167dadee4c1cbc0f0ea7218d2c134
Kyle <kyle201817146@gmail.com>
2025-08-16 17:18:58 +0800

    Add CAFrameRateRangeUtilTests (#451)

---
+++ Tests/OpenSwiftUICoreTests/Render/CoreAnimation/CAFrameRateRangeUtilTests.swift
@@ -0,0 +1,24 @@
+//
+//  CAFrameRateRangeUtilTests.swift
+//  OpenSwiftUICoreTests
+
+#if canImport(QuartzCore)
+import Testing
+@_spi(ForOpenSwiftUIOnly)
+import OpenSwiftUICore
+import QuartzCore
+
+struct CAFrameRateRangeUtilTests {
+    @Test(arguments: [
+        (.zero, .default),
+        (0.05, .init(minimum: 20, maximum: 60, preferred: 20)), // 20
+        (0.025, .init(minimum: 40, maximum: 60, preferred: 40)), // 40
+        (0.02, .default), // 50
+        (0.0125, .init(minimum: 80, maximum: 80, preferred: 80)), // 80
+    ] as [(Double, CAFrameRateRange)])
+    func initWithInterval(interval: Double, expected: CAFrameRateRange) {
+        let range = CAFrameRateRange(interval: interval)
+        #expect(range == expected)
+    }
+}
+#endif
---
+++ Tests/OpenSwiftUISymbolDualTests/Render/CoreAnimation/CAFrameRateRangeUtilDualTests.swift
@@ -0,0 +1,29 @@
+//
+//  CAFrameRateRangeUtilDualTests.swift
+//  OpenSwiftUICoreTests
+
+#if canImport(SwiftUI, _underlyingVersion: 6.5.4)
+import QuartzCore
+import Testing
+
+// MARK: - FlexFrameLayoutTests
+
+extension CAFrameRateRange {
+    @_silgen_name("SwiftUITestStub_CAFrameRateRangeInitInterval")
+    init(swiftUI_interval: Double)
+}
+
+struct CAFrameRateRnageUtilDualTests {
+    @Test(arguments: [
+        (.zero, .default),
+        (0.05, .init(minimum: 20, maximum: 60, preferred: 20)), // 20
+        (0.025, .init(minimum: 40, maximum: 60, preferred: 40)), // 40
+        (0.02, .default), // 50
+        (0.0125, .init(minimum: 80, maximum: 80, preferred: 80)), // 80
+    ] as [(Double, CAFrameRateRange)])
+    func initWithInterval(interval: Double, expected: CAFrameRateRange) {
+        let range = CAFrameRateRange(swiftUI_interval: interval)
+        #expect(range == expected)
+    }
+}
+#endif
