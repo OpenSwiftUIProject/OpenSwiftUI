8ed136b706268381df736edd356466ab42740597
Kyle <kyle201817146@gmail.com>
2025-08-18 00:36:35 +0800

    Update ColorAnimationExample

--- Example/SharedExample/Animation/ColorAnimationExample.swift
+++ Example/SharedExample/Animation/ColorAnimationExample.swift
@@ -15,9 +15,9 @@ struct ColorAnimationExample: View {
             Color(platformColor: showRed ? .red : .blue)
                 .frame(width: showRed ? 200 : 400, height: showRed ? 200 : 400)
         }
-        .animation(.easeInOut(duration: 2), value: showRed)
+        .animation(.easeInOut(duration: 5), value: showRed)
         .onAppear {
-            DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
                 showRed.toggle()
             }
         }
