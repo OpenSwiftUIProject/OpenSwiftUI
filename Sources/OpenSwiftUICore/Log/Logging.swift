9c1d50424fae33d1f173b34083071f60d65df5b7
Kyle <kyle201817146@gmail.com>
2025-09-15 00:44:13 +0800

    Fix Logging API issue

--- Sources/OpenSwiftUICore/Log/Logging.swift
+++ Sources/OpenSwiftUICore/Log/Logging.swift
@@ -23,12 +23,32 @@ extension Logger {
 
 extension Logger.Level {
     #if DEBUG
-    package static let `default`: Logger.Level = .debug
+    public static let `default`: Logger.Level = .debug
     #else
-    package static let `default`: Logger.Level = .info
+    public static let `default`: Logger.Level = .info
     #endif
 }
 
+extension Logger {
+    @inlinable
+    public func log(
+        _ message: @autoclosure () -> Logger.Message,
+        metadata: @autoclosure () -> Logger.Metadata? = nil,
+        file: String = #fileID,
+        function: String = #function,
+        line: UInt = #line
+    ) {
+        self.log(
+            level: .default,
+            message(),
+            metadata: metadata(),
+            source: nil,
+            file: file,
+            function: function,
+            line: line
+        )
+    }
+}
 #else
 public import os.log
 
