c4862f464f56f75916b3fd14a7df0c2281b4afba
Kyle <kyle201817146@gmail.com>
2025-10-02 17:50:46 +0800

    Add Environment+Observable macro model support

--- Sources/OpenSwiftUI/Modifier/SceneModifier/Environment+Scene.swift
+++ Sources/OpenSwiftUI/Modifier/SceneModifier/Environment+Scene.swift
@@ -3,8 +3,10 @@ public import OpenCombine
 #else
 public import Combine
 #endif
+public import OpenObservation
 
 extension Scene {
+
     /// Supplies an `ObservableObject` to a view subhierarchy.
     ///
     /// The object can be read by any child by using `EnvironmentObject`:
@@ -34,6 +36,59 @@ extension Scene {
     ///   the scene's subhierarchy.
     @available(OpenSwiftUI_v5_0, *)
     nonisolated public func environmentObject<T>(_ object: T) -> some Scene where T: ObservableObject {
+        _openSwiftUIUnimplementedFailure()
+    }
+}
+
+extension Scene {
+
+    /// Places an observable object in the scene's environment.
+    ///
+    /// Use this modifier to place an object that you declare with the
+    /// [Observable()](https://swiftpackageindex.com/openswiftuiproject/openobservation/main/documentation/openobservation/observable())
+    /// macro into a scene's environment. For example, you can add an instance
+    /// of a custom observable `Profile` class to the environment of a
+    /// ``WindowGroup`` scene:
+    ///
+    ///     @Observable class Profile { ... }
+    ///
+    ///     @main
+    ///     struct MyApp: App {
+    ///         var body: some View {
+    ///             WindowGroup {
+    ///                 ContentView()
+    ///             }
+    ///             .environment(Profile.currentProfile)
+    ///         }
+    ///     }
+    ///
+    /// You then read the object inside `ContentView` or one of its descendants
+    /// using the ``Environment`` property wrapper:
+    ///
+    ///     struct ContentView: View {
+    ///         @Environment(Profile.self) private var currentProfile: Profile
+    ///
+    ///         var body: some View { ... }
+    ///     }
+    ///
+    /// This modifier affects the given scene, as well as the scene's descendant
+    /// views. It has no effect outside the view hierarchy on which you call it.
+    /// The environment of a given view hierarchy holds only one observable
+    /// object of a given type.
+    ///
+    /// - Note: This modifier takes an object that conforms to the
+    ///   [Observable](https://swiftpackageindex.com/openswiftuiproject/openobservation/main/documentation/openobservation/observable)
+    ///   protocol. To add environment objects that conform to the
+    ///   [ObservableObject](https://swiftpackageindex.com/openswiftuiproject/opencombine/main/documentation/opencombine/observableobject)
+    ///   protocol, use ``View/environmentObject(_:)`` instead.
+    ///
+    /// - Parameter object: The object to set for this object's type in the
+    ///   environment, or `nil` to clear an object of this type from the
+    ///   environment.
+    ///
+    /// - Returns: A scene that has the specified object in its environment.
+    @available(OpenSwiftUI_v5_0, *)
+    nonisolated public func environment<T>(_ object: T?) -> some View where T: AnyObject, T: Observable {
         _openSwiftUIUnimplementedFailure()
     }
 }
--- Sources/OpenSwiftUICore/Data/Combine/Environment+Objects.swift
+++ Sources/OpenSwiftUICore/Data/Combine/Environment+Objects.swift
@@ -1,9 +1,10 @@
 //
-//  EnvironmentObjectKey.swift
+//  Environment+Objects.swift
 //  OpenSwiftUICore
 //
 //  Audited for 6.5.4
 //  Status: Complete
+//  ID: D6C90B7A81ED24386CD276102C65B68D (SwiftUICore)
 
 #if OPENSWIFTUI_OPENCOMBINE
 import OpenCombine
@@ -24,9 +25,21 @@ struct EnvironmentObjectKey<ObjectType>: EnvironmentKey, Hashable where ObjectTy
 // MARK: - EnvironmentValues + EnvironmentObjectKey
 
 extension EnvironmentValues {
-    subscript<ObjectType>(key: EnvironmentObjectKey<ObjectType>) -> ObjectType? where ObjectType: AnyObject {
+    subscript<ObjectType>(_: EnvironmentObjectKey<ObjectType>) -> ObjectType? where ObjectType: AnyObject {
         get { self[objectType: ObjectType.self] }
         set { self[objectType: ObjectType.self] = newValue }
+    }
+
+    subscript<ObjectType>(forceUnwrapping key: EnvironmentObjectKey<ObjectType>) -> ObjectType where ObjectType: AnyObject {
+        get {
+            guard let object = self[key] else {
+                preconditionFailure("No Observable object of type \(ObjectType.self) found. A View.environmentObject(_:) for \(ObjectType.self) may be missing as an ancestor of this view.")
+            }
+            return object
+        }
+        set {
+            self[key] = newValue
+        }
     }
 
     subscript<ObjectType>(objectType _: ObjectType.Type) -> ObjectType? where ObjectType: AnyObject {
