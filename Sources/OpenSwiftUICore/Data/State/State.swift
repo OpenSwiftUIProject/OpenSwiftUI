7750bb26f206925cabedde403ed7210452f92756
Kyle <kyle201817146@gmail.com>
2025-10-03 21:58:56 +0800

    Update State documentation

--- Sources/OpenSwiftUICore/Data/State/State.swift
+++ Sources/OpenSwiftUICore/Data/State/State.swift
@@ -1,10 +1,11 @@
 //
 //  State.swift
-//  OpenSwiftUI
+//  OpenSwiftUICore
 //
-//  Audited for 3.5.2
+//  Audited for 6.5.4
 //  Status: Complete
-//  ID: 08168374F4710A99DCB15B5E8768D632
+//  ID: 08168374F4710A99DCB15B5E8768D632 (SwiftUI)
+//  ID: F6975D1F800AFE6093C23B3DBD777BCF (SwiftUICore)
 
 import OpenAttributeGraphShims
 
@@ -40,17 +41,12 @@ import OpenAttributeGraphShims
 /// need access, either directly for read-only access, or as a binding for
 /// read-write access. You can safely mutate state properties from any thread.
 ///
-/// > Note: If you need to store a reference type, like an instance of a class,
-///   use a ``StateObject`` instead.
-///
 /// ### Share state with subviews
 ///
 /// If you pass a state property to a subview, OpenSwiftUI updates the subview
 /// any time the value changes in the container view, but the subview can't
 /// modify the value. To enable the subview to modify the state's stored value,
-/// pass a ``Binding`` instead. You can get a binding to a state value by
-/// accessing the state's ``projectedValue``, which you get by prefixing the
-/// property name with a dollar sign (`$`).
+/// pass a ``Binding`` instead.
 ///
 /// For example, you can remove the `isPlaying` state from the play button in
 /// the above example, and instead make the button take a binding:
@@ -66,7 +62,9 @@ import OpenAttributeGraphShims
 ///     }
 ///
 /// Then you can define a player view that declares the state and creates a
-/// binding to the state using the dollar sign prefix:
+/// binding to the state. Get the binding to the state value by accessing the
+/// state's ``projectedValue``, which you get by prefixing the property name
+/// with a dollar sign (`$`):
 ///
 ///     struct PlayerView: View {
 ///         @State private var isPlaying: Bool = false // Create the state here now.
@@ -80,12 +78,179 @@ import OpenAttributeGraphShims
 ///         }
 ///     }
 ///
-/// Like you do for a ``StateObject``, declare ``State`` as private to prevent
+/// Like you do for a ``StateObject``, declare `State` as private to prevent
 /// setting it in a memberwise initializer, which can conflict with the storage
 /// management that OpenSwiftUI provides. Unlike a state object, always
 /// initialize state by providing a default value in the state's
 /// declaration, as in the above examples. Use state only for storage that's
 /// local to a view and its subviews.
+///
+/// ### Store observable objects
+///
+/// You can also store observable objects that you create with the
+/// [Observable()](https://swiftpackageindex.com/openswiftuiproject/openobservation/main/documentation/openobservation/observable())
+/// macro in `State`; for example:
+///
+///     @Observable
+///     class Library {
+///         var name = "My library of books"
+///         // ...
+///     }
+///
+///     struct ContentView: View {
+///         @State private var library = Library()
+///
+///         var body: some View {
+///             LibraryView(library: library)
+///         }
+///     }
+///
+/// A `State` property always instantiates its default value when OpenSwiftUI
+/// instantiates the view. For this reason, avoid side effects and
+/// performance-intensive work when initializing the default value. For
+/// example, if a view updates frequently, allocating a new default object each
+/// time the view initializes can become expensive. Instead, you can defer the
+/// creation of the object using the ``View/task(priority:_:)`` modifier, which
+/// is called only once when the view first appears:
+///
+///     struct ContentView: View {
+///         @State private var library: Library?
+///
+///         var body: some View {
+///             LibraryView(library: library)
+///                 .task {
+///                     library = Library()
+///                 }
+///         }
+///     }
+///
+/// Delaying the creation of the observable state object ensures that
+/// unnecessary allocations of the object doesn't happen each time OpenSwiftUI
+/// initializes the view. Using the ``View/task(priority:_:)`` modifier is also
+/// an effective way to defer any other kind of work required to create the
+/// initial state of the view, such as network calls or file access.
+///
+/// > Note: It's possible to store an object that conforms to the
+/// [ObservableObject](https://swiftpackageindex.com/openswiftuiproject/opencombine/main/documentation/opencombine/observableobject)
+/// protocol in a `State` property. However the view will only update when
+/// the reference to the object changes, such as when setting the property with
+/// a reference to another object. The view will not update if any of the
+/// object's published properties change. To track changes to both the reference
+/// and the object's published properties, use ``StateObject`` instead of
+/// ``State`` when storing the object.
+///
+/// ### Share observable state objects with subviews
+///
+/// To share an [Observable](https://swiftpackageindex.com/openswiftuiproject/openobservation/main/documentation/openobservation/observable)
+/// object stored in `State` with a subview, pass the object reference to
+/// the subview. OpenSwiftUI updates the subview anytime an observable property of
+/// the object changes, but only when the subview's ``View/body`` reads the
+/// property. For example, in the following code `BookView` updates each time
+/// `title` changes but not when `isAvailable` changes:
+///
+///     @Observable
+///     class Book {
+///         var title = "A sample book"
+///         var isAvailable = true
+///     }
+///
+///     struct ContentView: View {
+///         @State private var book = Book()
+///
+///         var body: some View {
+///             BookView(book: book)
+///         }
+///     }
+///
+///     struct BookView: View {
+///         var book: Book
+///
+///         var body: some View {
+///             Text(book.title)
+///         }
+///     }
+///
+/// `State` properties provide bindings to their value. When storing an object,
+/// you can get a ``Binding`` to that object, specifically the reference to the
+/// object. This is useful when you need to change the reference stored in
+/// state in some other subview, such as setting the reference to `nil`:
+///
+///     struct ContentView: View {
+///         @State private var book: Book?
+///
+///         var body: some View {
+///             DeleteBookView(book: $book)
+///                 .task {
+///                     book = Book()
+///                 }
+///         }
+///     }
+///
+///     struct DeleteBookView: View {
+///         @Binding var book: Book?
+///
+///         var body: some View {
+///             Button("Delete book") {
+///                 book = nil
+///             }
+///         }
+///     }
+///
+/// However, passing a ``Binding`` to an object stored in `State` isn't
+/// necessary when you need to change properties of that object. For example,
+/// you can set the properties of the object to new values in a subview by
+/// passing the object reference instead of a binding to the reference:
+///
+///     struct ContentView: View {
+///         @State private var book = Book()
+///
+///         var body: some View {
+///             BookCheckoutView(book: book)
+///         }
+///     }
+///
+///     struct BookCheckoutView: View {
+///         var book: Book
+///
+///         var body: some View {
+///             Button(book.isAvailable ? "Check out book" : "Return book") {
+///                 book.isAvailable.toggle()
+///             }
+///         }
+///     }
+///
+/// If you need a binding to a specific property of the object, pass either the
+/// binding to the object and extract bindings to specific properties where
+/// needed, or pass the object reference and use the ``Bindable`` property
+/// wrapper to create bindings to specific properties. For example, in the
+/// following code `BookEditorView` wraps `book` with `@Bindable`. Then the
+/// view uses the `$` syntax to pass to a ``TextField`` a binding to `title`:
+///
+///     struct ContentView: View {
+///         @State private var book = Book()
+///
+///         var body: some View {
+///             BookView(book: book)
+///         }
+///     }
+///
+///     struct BookView: View {
+///         let book: Book
+///
+///         var body: some View {
+///             BookEditorView(book: book)
+///         }
+///     }
+///
+///     struct BookEditorView: View {
+///         @Bindable var book: Book
+///
+///         var body: some View {
+///             TextField("Title", text: $book.title)
+///         }
+///     }
+///
+@available(OpenSwiftUI_v1_0, *)
 @frozen
 @propertyWrapper
 public struct State<Value> {
