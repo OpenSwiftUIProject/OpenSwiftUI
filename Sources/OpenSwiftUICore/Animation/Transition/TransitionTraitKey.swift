508438769d4e494093f51426c83d5131b8f8d9eb
Kyle <kyle201817146@gmail.com>
2025-10-08 21:12:45 +0800

    Update TransitionTraitKey (#563)

--- Sources/OpenSwiftUICore/Animation/Transition/TransitionTraitKey.swift
+++ Sources/OpenSwiftUICore/Animation/Transition/TransitionTraitKey.swift
@@ -2,9 +2,12 @@
 //  TransitionTraitKey.swift
 //  OpenSwiftUICore
 //
-//  Audited for 6.0.87
-//  Status: WIP
+//  Audited for 6.5.4
+//  Status: Complete
 
+// MARK: - ViewTraitCollection + canTransition
+
+@available(OpenSwiftUI_v1_0, *)
 @usableFromInline
 struct CanTransitionTraitKey: _ViewTraitKey {
     @inlinable
@@ -18,5 +21,32 @@ extension ViewTraitCollection {
     package var canTransition: Bool {
         get { self[CanTransitionTraitKey.self] }
         set { self[CanTransitionTraitKey.self] = newValue }
+    }
+}
+
+// MARK: - ViewTraitCollection + transition
+
+struct TransitionTraitKey: _ViewTraitKey {
+    static var defaultValue: AnyTransition { .opacity }
+}
+
+extension ViewTraitCollection {
+    package var transition: AnyTransition {
+        self[TransitionTraitKey.self]
+    }
+
+    package func optionalTransition(ignoringIdentity: Bool) -> AnyTransition? {
+        guard canTransition else {
+            return nil
+        }
+        let transition = transition
+        if ignoringIdentity, transition.isIdentity {
+            return nil
+        }
+        return transition
+    }
+
+    package func optionalTransition() -> AnyTransition? {
+        optionalTransition(ignoringIdentity: true)
     }
 }
