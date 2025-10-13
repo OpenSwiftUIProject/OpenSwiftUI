581a7e0dfaf4dd4404d5ca82c050202403a030a6
Kyle <kyle201817146@gmail.com>
2025-10-14 03:22:50 +0800

    Add View.transition API support (#571)

--- Sources/OpenSwiftUICore/Animation/Transition/TransitionTraitKey.swift
+++ Sources/OpenSwiftUICore/Animation/Transition/TransitionTraitKey.swift
@@ -5,7 +5,72 @@
 //  Audited for 6.5.4
 //  Status: Complete
 
-// MARK: - ViewTraitCollection + canTransition
+// MARK: - View + transition
+
+@available(OpenSwiftUI_v1_0, *)
+extension View {
+
+    /// Associates a transition with the view.
+    ///
+    /// When this view appears or disappears, the transition will be applied to
+    /// it, allowing for animating it in and out.
+    ///
+    /// The following code will conditionally show MyView, and when it appears
+    /// or disappears, will use a slide transition to show it.
+    ///
+    ///     if isActive {
+    ///         MyView()
+    ///             .transition(.slide)
+    ///     }
+    ///     Button("Toggle") {
+    ///         withAnimation {
+    ///             isActive.toggle()
+    ///         }
+    ///     }
+    @inlinable
+    @_disfavoredOverload
+    nonisolated public func transition(_ t: AnyTransition) -> some View {
+        return _trait(TransitionTraitKey.self, t)
+    }
+
+    /// Associates a transition with the view.
+    ///
+    /// When this view appears or disappears, the transition will be applied to
+    /// it, allowing for animating it in and out.
+    ///
+    /// The following code will conditionally show MyView, and when it appears
+    /// or disappears, will use a custom RotatingFadeTransition transition to
+    /// show it.
+    ///
+    ///     if isActive {
+    ///         MyView()
+    ///             .transition(RotatingFadeTransition())
+    ///     }
+    ///     Button("Toggle") {
+    ///         withAnimation {
+    ///             isActive.toggle()
+    ///         }
+    ///     }
+    @available(OpenSwiftUI_v5_0, *)
+    @_alwaysEmitIntoClient
+    nonisolated public func transition<T>(_ transition: T) -> some View where T: Transition {
+        self.transition(AnyTransition(transition))
+    }
+}
+
+// MARK: - TransitionTraitKey
+
+@available(OpenSwiftUI_v1_0, *)
+@usableFromInline
+struct TransitionTraitKey: _ViewTraitKey {
+    @inlinable
+    static var defaultValue: AnyTransition { .opacity }
+}
+
+@available(*, unavailable)
+extension TransitionTraitKey: Sendable {}
+
+// MARK: - CanTransitionTraitKey
 
 @available(OpenSwiftUI_v1_0, *)
 @usableFromInline
@@ -17,6 +82,8 @@ struct CanTransitionTraitKey: _ViewTraitKey {
 @available(*, unavailable)
 extension CanTransitionTraitKey: Sendable {}
 
+// MARK: - ViewTraitCollection + canTransition
+
 extension ViewTraitCollection {
     package var canTransition: Bool {
         get { self[CanTransitionTraitKey.self] }
@@ -25,10 +92,6 @@ extension ViewTraitCollection {
 }
 
 // MARK: - ViewTraitCollection + transition
-
-struct TransitionTraitKey: _ViewTraitKey {
-    static var defaultValue: AnyTransition { .opacity }
-}
 
 extension ViewTraitCollection {
     package var transition: AnyTransition {
