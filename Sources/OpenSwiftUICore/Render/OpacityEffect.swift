0a45acb2849f352131f850e34e411267906658cc
Kyle <kyle201817146@gmail.com>
2025-10-14 01:25:44 +0800

    [Bugfix] Add placeholder implementation for opacity transition
    Fix needsTransitions issue of checking isIdentity

--- Sources/OpenSwiftUICore/Render/OpacityEffect.swift
+++ Sources/OpenSwiftUICore/Render/OpacityEffect.swift
@@ -4,5 +4,18 @@
 
 extension AnyTransition {
     // FIXME
-    public static let opacity: AnyTransition = .identity
+    public static let opacity: AnyTransition = .init(OpacityTransition())
+}
+
+extension View {
+    func opacity(_ value: Double) -> some View {
+        // FIXME
+        modifier(EmptyModifier())
+    }
+}
+
+struct OpacityTransition: Transition {
+    func body(content: Content, phase: TransitionPhase) -> some View {
+        content.opacity(1)
+    }
 }
