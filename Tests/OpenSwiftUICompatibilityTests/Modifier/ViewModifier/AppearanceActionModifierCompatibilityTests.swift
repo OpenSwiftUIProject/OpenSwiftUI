9c2cc8174a46623c9b57c2abb5f5e539aaec9271
Kyle <kyle201817146@gmail.com>
2025-10-14 01:44:46 +0800

    Add View.transition API support

--- Tests/OpenSwiftUICompatibilityTests/Modifier/ViewModifier/AppearanceActionModifierCompatibilityTests.swift
+++ Tests/OpenSwiftUICompatibilityTests/Modifier/ViewModifier/AppearanceActionModifierCompatibilityTests.swift
@@ -73,4 +73,49 @@ struct AppearanceActionModifierCompatibilityTests {
         }
         await #expect(Helper.result == "AFATDTDT")
     }
+
+    @Test("idTest with identity transition")
+    func idTestWithIdentityTransition() async throws {
+        enum Helper {
+            @MainActor
+            static var result = ""
+        }
+
+        struct ContentView: View {
+            @State private var toggle = false
+            var continuation: UnsafeContinuation<Void, Never>
+
+            var body: some View {
+                Color.red
+                    .onAppear {
+                        if Helper.result.isEmpty {
+                            DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+                                toggle.toggle()
+                            }
+                        } else {
+                            DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+                                continuation.resume()
+                            }
+                        }
+                        Helper.result += "A"
+                        Helper.result += toggle ? "T" : "F"
+                    }
+                    .onDisappear {
+                        Helper.result += "D"
+                        Helper.result += toggle ? "T" : "F"
+                    }
+                    .transition(.identity)
+                    .id(toggle)
+            }
+        }
+
+        try await triggerLayoutWithWindow { continuation in
+            PlatformHostingController(
+                rootView: ContentView(
+                    continuation: continuation
+                )
+            )
+        }
+        await #expect(Helper.result == "AFDTATDT")
+    }
 }
