946efe056a70ab733d6ba4cc0ea5031e43df6af4
Kyle <kyle201817146@gmail.com>
2025-10-16 02:31:54 +0800

    Add IDView displayList expectation

--- Tests/OpenSwiftUICoreTests/View/IDViewTests.swift
+++ Tests/OpenSwiftUICoreTests/View/IDViewTests.swift
@@ -2,9 +2,9 @@
 //  IDViewTests.swift
 //  OpenSwiftUICoreTests
 
-@testable import OpenSwiftUICore
-import Testing
 import Foundation
+import OpenSwiftUICore
+import Testing
 
 @MainActor
 struct IDViewTests {
@@ -14,5 +14,49 @@ struct IDViewTests {
         _ = empty.id("1")
         _ = empty.id(2)
         _ = empty.id(UUID())
+    }
+
+    @Test
+    func idViewDisplayList() {
+        struct ContentView: View {
+            var body: some View {
+                VStack {
+                    Color.red
+                    Color.blue
+                }.id(1)
+            }
+        }
+        let graph = ViewGraph(
+            rootViewType: ContentView.self,
+            requestedOutputs: [.displayList]
+        )
+        graph.instantiateOutputs()
+        graph.setRootView(ContentView())
+        graph.setProposedSize(CGSize(width: 100, height: 100))
+        let (displayList, _) = graph.displayList()
+
+        let expectRegex = try! Regex(#"""
+        \(display-list
+          \(item #:identity \d+ #:version \d+
+            \(frame \([^)]+\)\)\)\)
+        """#)
+        #expect(displayList.description.contains(expectRegex))
+        withKnownIssue("Blocked by DisplayList.print and canonicalize") {
+            let expectRegex = try! Regex(#"""
+            \(display-list
+              \(item #:identity \d+ #:version \d+
+                \(frame \([^)]+\)\)
+                \(effect
+                  \(item #:identity \d+ #:version \d+
+                    \(frame \([^)]+\)\)
+                    \(content-seed \d+\)
+                    \(color #[0-9A-F]{8}\)\)
+                  \(item #:identity \d+ #:version \d+
+                    \(frame \([^)]+\)\)
+                    \(content-seed \d+\)
+                    \(color #[0-9A-F]{8}\)\)\)\)\)
+            """#)
+            #expect(displayList.description.contains(expectRegex))
+        }
     }
 }
