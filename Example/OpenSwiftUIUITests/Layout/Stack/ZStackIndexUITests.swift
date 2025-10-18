5ce4276a933b80d31cd86253c29a5510f8c6e2de
Kyle <kyle201817146@gmail.com>
2025-10-18 18:08:18 +0800

    Update ZIndex test case

---
+++ Example/OpenSwiftUIUITests/Layout/Stack/ZStackIndexUITests.swift
@@ -0,0 +1,40 @@
+//
+//  ZIndexCompatibilityTests.swift
+//  OpenSwiftUICompatibilityTests
+
+import Foundation
+import Testing
+import SnapshotTesting
+
+@MainActor
+@Suite(.snapshots(record: .never, diffTool: diffTool))
+struct ZStackIndexUITests {
+    @Test
+    func zIndexExample() {
+        struct ContentView: View {
+            var body: some View {
+                VStack {
+                    // TODO: Path & Shape is not implemented yet.
+                    Color.yellow
+//                    Rectangle()
+//                        .fill(Color.yellow)
+                        .frame(width: 100, height: 100, alignment: .center)
+                        .zIndex(1) // Top layer.
+                    Color.red
+//                    Rectangle()
+//                        .fill(Color.red)
+                        .frame(width: 100, height: 100, alignment: .center)
+                        .rotationEffect(.degrees(45))
+                        // Here a zIndex of 0 is the default making
+                        // this the bottom layer.
+                }
+            }
+        }
+        withKnownIssue {
+            openSwiftUIAssertSnapshot(
+                of: ContentView(),
+                size: CGSize(width: 200, height: 200)
+            )
+        }
+    }
+}
