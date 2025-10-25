2371add6b4b1d4c1c004a413735576bac35a4a8b
Kyle <kyle201817146@gmail.com>
2025-10-25 14:25:37 +0800

    Add ForEachExample

---
+++ Example/SharedExample/View/DynamicContentView/ForEachExample.swift
@@ -0,0 +1,75 @@
+//
+//  ForEachExample.swift
+//  SharedExample
+
+#if OPENSWIFTUI
+import OpenObservation
+import OpenSwiftUI
+#else
+import Observation
+import SwiftUI
+#endif
+
+struct ForEachExample: View {
+    var body: some View {
+        HStack {
+            ForEachOffsetExample()
+            ForEachKeyPathExample()
+        }
+    }
+}
+
+struct ForEachOffsetExample: View {
+    var body: some View {
+        VStack(spacing: 0) {
+            ForEach(0 ..< 6) { index in
+                Color.red.opacity(Double(index) / 6.0 )
+            }
+        }
+    }
+}
+
+struct ForEachKeyPathExample: View {
+    let opacities = [0, 0.2, 0.4, 0.6, 0.8, 1.0]
+
+    var body: some View {
+        VStack(spacing: 0) {
+            ForEach(opacities, id: \.self) { opacity in
+                Color.red.opacity(opacity)
+            }
+        }
+    }
+}
+
+struct ForEachNonConstantRangeView: View {
+    let range = 0..<5
+
+    var body: some View {
+        // Expect to emit warning: "Non-constant range: not an integer range" in compile time using Xcode toolchain
+        ForEach(range) { _ in
+            EmptyView()
+        }
+    }
+}
+
+@Observable
+private class ForEachLazyContainerNonConstantCountViewModel {
+    var data: [Int] = [1, 2]
+}
+
+// TODO: LazyContainer (eg. List / LazyHVStack)
+struct ForEachLazyContainerNonConstantCountView: View {
+    private typealias Model = ForEachLazyContainerNonConstantCountViewModel
+
+    @State private var model = Model()
+
+    var body: some View {
+//        List {
+            ForEach(model.data, id: \.self) { i in
+                if i % 2 == 0 {
+                    Text(i.description)
+                }
+            }
+//        }
+    }
+}
