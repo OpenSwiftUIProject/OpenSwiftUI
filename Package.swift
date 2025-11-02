OpenSwiftUIProject/OpenSwiftUI

commit 48a1db04b3d0fab5d751c370e42b31f389a66338

Add EnvManager support (#622)

Package.resolved
@@ -1,5 +1,5 @@
 {
-  "originHash" : "2c64b45620932621276e50344e6c5fe6c9d3f5504944717f976fdeef05f66bc5",
+  "originHash" : "5e56f5e0102d16fcc8dac2f347eeb346b445dfb32ffd8468e54aaa3bc8afc236",
   "pins" : [
     {
       "identity" : "darwinprivateframeworks",
@@ -16,7 +16,7 @@
       "location" : "https://github.com/OpenSwiftUIProject/OpenAttributeGraph",
       "state" : {
         "branch" : "main",
-        "revision" : "6f398f0238e498ce60f98b90e99efdc27b0f0c78"
+        "revision" : "7c516532e7a11785e383fbee88af834d89cdb981"
       }
     },
     {
@@ -25,7 +25,7 @@
       "location" : "https://github.com/OpenSwiftUIProject/OpenCoreGraphics",
       "state" : {
         "branch" : "main",
-        "revision" : "706d964f419ce293fe1c7afa9b784de0d91f7e9a"
+        "revision" : "cd89c292c4ed4c25d9468a12d9490cc18304ff37"
       }
     },
     {
@@ -43,7 +43,7 @@
       "location" : "https://github.com/OpenSwiftUIProject/OpenRenderBox",
       "state" : {
         "branch" : "main",
-        "revision" : "a288f586e9ecac85b259d433a795d4104cb929a5"
+        "revision" : "1e9139ba6f5521e7fc879e410dfc4a67136fea15"
       }
     },
     {

commit e94329eb39813cfb9de3f74b4beee379f23a50b9

[Bugfix] Remove werror
Fix CI failure introduced by #622

Package.resolved
@@ -1,5 +1,5 @@
 {
-  "originHash" : "5e56f5e0102d16fcc8dac2f347eeb346b445dfb32ffd8468e54aaa3bc8afc236",
+  "originHash" : "f05af269b37353aa640dcb283620805e2542b2dd5009baa06e300d22a9d742b9",
   "pins" : [
     {
       "identity" : "darwinprivateframeworks",

Package.swift
@@ -268,9 +268,6 @@ if releaseVersion >= 2021 {
         sharedSwiftSettings.append(.define("OPENSWIFTUI_SUPPORT_\(year)_API"))
     }
 }
-if warningsAsErrorsCondition {
-    sharedSwiftSettings.append(.unsafeFlags(["-warnings-as-errors"]))
-}
 if libraryEvolutionCondition {
     // NOTE: -enable-library-evolution will cause module verify failure for `swift build`.
     // Either set OPENSWIFTUI_LIBRARY_EVOLUTION=0 or add `-Xswiftc -no-verify-emitted-module-interface` after `swift build`