8dd7b507021b1c3ce3c81e0a00e444fd6a6715f8
Kyle <kyle201817146@gmail.com>
2026-01-18 00:55:41 +0800

    Add ViewGeometryDualTest case

---
+++ Sources/OpenSwiftUISymbolDualTestsSupport/Layout/View/ViewGeometryTestsStub.c
@@ -0,0 +1,14 @@
+//
+//  ViewGeometryTestsStub.c
+//  OpenSwiftUISymbolDualTestsSupport
+
+#include "OpenSwiftUIBase.h"
+
+#if OPENSWIFTUI_TARGET_OS_DARWIN
+
+#import <SymbolLocator.h>
+
+DEFINE_SL_STUB_SLF(OpenSwiftUITestStub_ViewGeometryIsInvalid, SwiftUI, $s7SwiftUI12ViewGeometryV9isInvalidSbvg);
+
+#endif
+
