a72598211ae8a7a988fbff8904d15c762046c432
Kyle <kyle201817146@gmail.com>
2026-01-25 16:21:46 +0800

    Add SceneList and WindowGroup support

---
+++ Sources/OpenSwiftUI/App/Scene/SceneList.swift
@@ -0,0 +1,111 @@
+//
+//  SceneList.swift
+//  OpenSwiftUI
+//
+//  Audited for 6.5.4
+//  Status: WIP
+
+import Foundation
+@_spi(ForOpenSwiftUIOnly)
+import OpenSwiftUICore
+
+// MARK: - SceneList
+
+struct SceneList {
+    var items: [SceneList.Item]
+
+//    func allDocumentGroups() -> [IdentifiedDocumentGroupConfiguration] {
+//        preconditionFailure("TODO")
+//    }
+
+    func windowGroup(presenting: Any.Type) -> SceneList.Item? {
+        preconditionFailure("TODO")
+    }
+
+    func windowGroup<A>(id: String?, presenting: A.Type) -> SceneList.Item? where A: Decodable, A: Encodable, A: Hashable {
+        _openSwiftUIUnimplementedFailure()
+    }
+}
+
+// MARK: - SceneList.Item
+
+extension SceneList {
+    struct Item: Identifiable {
+        var value: SceneList.Item.Value
+        var id: SceneID
+        var version: DisplayList.Version
+        var environment: EnvironmentValues
+        var options: SceneList.Item.Options
+        var accessibilityProperties: AccessibilityProperties?
+        var activationConditions: Set<String>?
+        // var resizability: WindowResizability.Role
+        var defaultPosition: UnitPoint?
+        var defaultSize: CGSize?
+        // var restorationBehavior: SceneRestorationBehavior.Role
+        // var windowManagerRole: WindowManagerRole
+        // var connectionOptionPayloadStorage: ConnectionOptionPayloadStorage
+
+        // MARK: - SceneList.Item.Summary
+
+        struct Summary: Identifiable {
+            var id: SceneID
+            var kind: SceneList.Item.Kind
+            var value: SceneList.Item.Value
+            var isPrimaryItem: Bool
+            var options: SceneList.Item.Options
+        }
+
+        // MARK: - SceneList.Item.Value
+
+        enum Value {
+            case windowGroup(WindowSceneConfiguration<WindowGroupConfigurationAttributes>)
+//            case stage(WindowSceneConfiguration<StageConfigurationAttributes>)
+//            case documentGroup(IdentifiedDocumentGroupConfiguration)
+            case settings(AnyView)
+//            case menuBarExtra(MenuBarExtraConfiguration)
+//            case customScene(UISceneAdaptorConfiguration)
+//            case singleWindow(SingleWindowConfiguration)
+//            case documentIntroduction(DocumentIntroductionConfiguration)
+//            case alertDialog(DialogConfiguration)
+        }
+
+        // MARK: - SceneList.Item.Kind
+
+        enum Kind {
+            case windowGroup
+            case singleWindow
+            case custom
+            case documentGroup
+            case documentIntroduction
+            case settings
+            case stage
+            case carPlay
+            case clarityUI
+            case externalDisplay
+            case alertDialog
+        }
+
+        // MARK: - SceneList.Item.Options
+
+        struct Options: OptionSet {
+            let rawValue: UInt8
+
+            init(rawValue: UInt8) {
+                self.rawValue = rawValue
+            }
+        }
+    }
+}
+
+
+// MARK: - SceneList.Key
+
+extension SceneList {
+    struct Key: PreferenceKey {
+        static let defaultValue: SceneList = .init(items: [])
+
+        static func reduce(value: inout SceneList, nextValue: () -> SceneList) {
+            value.items.append(contentsOf: nextValue().items)
+        }
+    }
+}
