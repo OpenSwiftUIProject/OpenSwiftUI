a26da04fee9ade0ea23032f4ece3c0e58ad0bd20
Kyle <kyle201817146@gmail.com>
2026-02-08 22:31:15 +0800

    Update ImplicitShapeStyle

--- Sources/OpenSwiftUICore/Shape/ShapeStyle/ImplicitShapeStyle.swift
+++ Sources/OpenSwiftUICore/Shape/ShapeStyle/ImplicitShapeStyle.swift
@@ -2,10 +2,11 @@
 //  ImplicitShapeStyle.swift
 //  OpenSwiftUICore
 //
-//  Audited for 6.0.87
+//  Audited for 6.5.4
 //  Status: Complete
 
 /// A shape style representing the implicit base of style modifiers.
+@available(OpenSwiftUI_v1_0, *)
 @frozen
 public struct _ImplicitShapeStyle: ShapeStyle, PrimitiveShapeStyle {
     @inlinable
