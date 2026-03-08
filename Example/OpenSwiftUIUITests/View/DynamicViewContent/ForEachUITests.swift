fe4d8d4cd81b82063b124efaa65e112c8eb0cd99
Kyle <kyle201817146@gmail.com>
2026-03-08 15:56:01 +0800

    Fix duplicate ContentView

--- Example/OpenSwiftUIUITests/View/DynamicViewContent/ForEachUITests.swift
+++ Example/OpenSwiftUIUITests/View/DynamicViewContent/ForEachUITests.swift
@@ -4,38 +4,19 @@
 
 import Testing
 import SnapshotTesting
+@testable import TestingHost
 
 @MainActor
 @Suite(.snapshots(record: .never, diffTool: diffTool))
 struct ForEachUITests {
     @Test
     func offset() {
-        struct ContentView: View {
-            var body: some View {
-                VStack(spacing: 0) {
-                    ForEach(0 ..< 6) { index in
-                        Color.red.opacity(Double(index) / 6.0 )
-                    }
-                }
-            }
-        }
-        openSwiftUIAssertSnapshot(of: ContentView())
+        openSwiftUIAssertSnapshot(of: ForEachOffsetExample())
     }
 
     @Test
     func keyPath() {
-        struct ContentView: View {
-            let opacities = [0, 0.2, 0.4, 0.6, 0.8, 1.0]
-
-            var body: some View {
-                VStack(spacing: 0) {
-                    ForEach(opacities, id: \.self) { opacity in
-                        Color.red.opacity(opacity)
-                    }
-                }
-            }
-        }
-        openSwiftUIAssertSnapshot(of: ContentView())
+        openSwiftUIAssertSnapshot(of: ForEachKeyPathExample())
     }
 
     @Test
