b4f57c8c05113d9d431af8a0294311277fb2eaa8
Kyle <kyle201817146@gmail.com>
2026-03-23 02:09:13 +0800

    [NFC] Merge EmptyViewRendererHost into ViewRendererHost file

--- Sources/OpenSwiftUICore/View/Graph/ViewRendererHost.swift
+++ Sources/OpenSwiftUICore/View/Graph/ViewRendererHost.swift
@@ -572,3 +572,40 @@ extension ViewRendererHost {
         viewGraph.graph.archiveJSON(name: name)
     }
 }
+
+// MARK: - EmptyViewRendererHost [6.5.4]
+
+final package class EmptyViewRendererHost: ViewRendererHost {
+    package let viewGraph: ViewGraph
+
+    package var propertiesNeedingUpdate: ViewRendererHostProperties = []
+
+    package var renderingPhase: ViewRenderingPhase = .none
+
+    package var externalUpdateCount: Int = .zero
+
+    package var currentTimestamp: Time = .zero
+
+    package init(environment: EnvironmentValues = EnvironmentValues()) {
+        Update.begin()
+        viewGraph = ViewGraph(rootViewType: EmptyView.self, requestedOutputs: [])
+        viewGraph.setEnvironment(environment)
+        viewGraph.setRootView(EmptyView())
+        initializeViewGraph()
+        Update.end()
+    }
+
+    package func requestUpdate(after delay: Double) {}
+
+    package func updateRootView() {}
+
+    package func updateEnvironment() {}
+
+    package func updateSize() {}
+
+    package func updateSafeArea() {}
+
+    package func updateContainerSize() {}
+
+    package func forEachIdentifiedView(body: (_IdentifiedViewProxy) -> Void) {}
+}
