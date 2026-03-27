dd48a73e1ef9cb1628360ad4816519cac09f978b
Kyle <kyle201817146@gmail.com>
2026-03-27 23:48:33 +0800

    Add CAPresentationModifier.h

---
+++ Sources/OpenSwiftUICore/Util/CoreGraphicsShims.swift
@@ -0,0 +1,15 @@
+//
+//  CoreGraphicsShims.swift
+//  OpenSwiftUICore
+
+#if !canImport(CoreGraphics)
+
+// MARK: - CAPresentationModifierGroup
+
+open class CAPresentationModifierGroup: NSObject {}
+
+// MARK: - CAPresentationModifier
+
+open class CAPresentationModifier: NSObject {}
+
+#endif
