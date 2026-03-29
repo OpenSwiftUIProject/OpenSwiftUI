e5b749572bd7907792a903c0dbcd75480e3f1c3b
Kyle <kyle201817146@gmail.com>
2026-03-29 17:26:38 +0800

    Update DisplayListPlatformEffect

--- Sources/OpenSwiftUICore/Render/DisplayList/DisplayListPlatformEffect.swift
+++ Sources/OpenSwiftUICore/Render/DisplayList/DisplayListPlatformEffect.swift
@@ -2,21 +2,40 @@
 //  DisplayListPlatformEffect.swift
 //  OpenSwiftUICore
 //
-//  Audited for 6.0.87
+//  Audited for 6.5.4
 //  Status: Complete
+
+// MARK: - DisplayList.PlatformEffect
 
 extension DisplayList {
     package enum PlatformEffect {
         case identity
-        package var features: DisplayList.Features { [] }
-        package func encode(to encoder: any Encoder) throws {}
-        package init(from decoder: any Decoder) throws { self = .identity }
-        package func print(into sexp: inout SExpPrinter) {}
+
+        package var features: DisplayList.Features {
+            []
+        }
+
+        package func encode(to encoder: any Encoder) throws {
+            _openSwiftUIEmptyStub()
+        }
+
+        package init(from decoder: any Decoder) throws {
+            self = .identity
+        }
+
+        package func print(into sexp: inout SExpPrinter) {
+            _openSwiftUIEmptyStub()
+        }
     }
 }
 extension DisplayList.PlatformEffect: ProtobufMessage {
-    package func encode(to encoder: inout ProtobufEncoder) throws {}
-    package init(from decoder: inout ProtobufDecoder) throws { self = .identity }
+    package func encode(to encoder: inout ProtobufEncoder) throws {
+        _openSwiftUIEmptyStub()
+    }
+
+    package init(from decoder: inout ProtobufDecoder) throws {
+        self = .identity
+    }
 }
 
 extension DisplayList.ViewUpdater {
@@ -26,6 +45,11 @@ extension DisplayList.ViewUpdater {
 }
 extension DisplayList.ViewUpdater.Platform {
     package typealias PlatformViewInfo = DisplayList.ViewUpdater.PlatformViewInfo
+    package struct PlatformState {
+    }
+}
+
+extension DisplayList.ViewUpdater.Model {
     package struct PlatformState {
     }
 }
