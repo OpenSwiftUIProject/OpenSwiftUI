67c35ac7685626c58237a34839d759f024523ea2
Kyle <kyle201817146@gmail.com>
2026-05-13 00:41:03 +0800

    Update AsyncRenderExample

--- Example/Shared/ContentView.swift
+++ Example/Shared/ContentView.swift
@@ -10,6 +10,24 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        TextExample()
+        AsyncRenderExample()
+    }
+}
+
+struct AsyncRenderExample: View {
+    @State private var items = [6]
+
+    var body: some View {
+        VStack(spacing: 10) {
+            ForEach(items, id: \.self) { item in
+                Color.blue.opacity(Double(item) / 6.0)
+                    .frame(height: 50)
+                    .transition(.slide)
+            }
+        }
+        .animation(.easeInOut(duration: 2), value: items)
+        .onAppear {
+            items.removeAll { $0 == 6 }
+        }
     }
 }
