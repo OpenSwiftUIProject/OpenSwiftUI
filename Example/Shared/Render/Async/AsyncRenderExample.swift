18541a35d27a1aeba945a7826d559016383bad68
Kyle <kyle201817146@gmail.com>
2026-05-14 03:38:54 +0800

    Update ContentView

---
+++ Example/Shared/Render/Async/AsyncRenderExample.swift
@@ -0,0 +1,21 @@
+//
+//  GeometryEffectExample.swift
+//  Shared
+
+struct AsyncRenderExample: View {
+    @State private var items = [6]
+
+    var body: some View {
+        VStack(spacing: 10) {
+            ForEach(items, id: \.self) { item in
+                Color.blue.opacity(Double(item) / 6.0)
+                    .frame(height: 50)
+                    .transition(.slide)
+            }
+        }
+        .animation(.easeInOut(duration: 2), value: items)
+        .onAppear {
+            items.removeAll { $0 == 6 }
+        }
+    }
+}
