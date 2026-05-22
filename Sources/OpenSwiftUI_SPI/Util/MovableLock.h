f40de9eac0a4b19a1cd3ab7a8c825ae8f80b6c0b
Kyle <kyle201817146@gmail.com>
2026-05-23 00:49:57 +0800

    Remove redundant MovableLock export annotations

--- Sources/OpenSwiftUI_SPI/Util/MovableLock.h
+++ Sources/OpenSwiftUI_SPI/Util/MovableLock.h
@@ -30,31 +30,22 @@ typedef struct MovableLock_s {
 
 typedef MovableLock_t *MovableLock __attribute((swift_newtype(struct)));
 
-OPENSWIFTUI_EXPORT
 MovableLock _MovableLockCreate(void) OPENSWIFTUI_SWIFT_NAME(MovableLock.create());
 
-OPENSWIFTUI_EXPORT
 void _MovableLockDestroy(MovableLock lock) OPENSWIFTUI_SWIFT_NAME(MovableLock.destroy(self:));
 
-OPENSWIFTUI_EXPORT
 bool _MovableLockIsOwner(MovableLock lock) OPENSWIFTUI_SWIFT_NAME(getter:MovableLock.isOwner(self:));
 
-OPENSWIFTUI_EXPORT
 bool _MovableLockIsOutermostOwner(MovableLock lock) OPENSWIFTUI_SWIFT_NAME(getter:MovableLock.isOutermostOwner(self:));
 
-OPENSWIFTUI_EXPORT
 void _MovableLockLock(MovableLock lock) OPENSWIFTUI_SWIFT_NAME(MovableLock.lock(self:));
 
-OPENSWIFTUI_EXPORT
 void _MovableLockUnlock(MovableLock lock) OPENSWIFTUI_SWIFT_NAME(MovableLock.unlock(self:));
 
-OPENSWIFTUI_EXPORT
 void _MovableLockSyncMain(MovableLock lock, const void *context, void (*function)(const void *context)) OPENSWIFTUI_SWIFT_NAME(MovableLock.syncMain(self:_:function:));
 
-OPENSWIFTUI_EXPORT
 void _MovableLockWait(MovableLock lock) OPENSWIFTUI_SWIFT_NAME(MovableLock.wait(self:));
 
-OPENSWIFTUI_EXPORT
 void _MovableLockBroadcast(MovableLock lock) OPENSWIFTUI_SWIFT_NAME(MovableLock.broadcast(self:));
 
 OPENSWIFTUI_ASSUME_NONNULL_END
