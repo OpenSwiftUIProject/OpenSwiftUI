85f0c6f29658372d3d5b35d6ff5f6cdffceb2057
Kyle <kyle201817146@gmail.com>
2026-05-27 22:25:02 +0800

    Update ViewRendererVendor

--- Example/OpenSwiftUIUITests/Export.swift
+++ Example/OpenSwiftUIUITests/Export.swift
@@ -11,6 +11,23 @@ let shouldRecord: Bool? = nil
 #else
 @_exported import SwiftUI
 let shouldRecord: Bool? = true
+
+public struct ViewRendererVendor: RawRepresentable, Hashable, CaseIterable {
+    public let rawValue: String
+
+    public init(rawValue: String) {
+        self.rawValue = rawValue
+    }
+
+    /// OpenSwiftUI's view renderer.
+    public static let osui = ViewRendererVendor(rawValue: "org.OpenSwiftUIProject.OpenSwiftUI")
+
+    /// Apple's SwiftUI view renderer.
+    public static let sui = ViewRendererVendor(rawValue: "com.apple.SwiftUI")
+
+    public static var allCases: [ViewRendererVendor] { [.osui, .sui] }
+}
+public let viewRendererVendor = ViewRendererVendor.sui
 #endif
 let diffTool: SnapshotTestingConfiguration.DiffTool = .odiff
 
