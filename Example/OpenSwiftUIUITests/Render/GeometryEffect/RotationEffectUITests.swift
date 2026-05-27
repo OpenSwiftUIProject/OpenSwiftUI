1cb28096e01cb7857d93b1512edf158df88d6a60
Kyle <kyle201817146@gmail.com>
2026-05-27 23:12:00 +0800

    Run UI tests with both renderers (#877)

--- Example/OpenSwiftUIUITests/Render/GeometryEffect/RotationEffectUITests.swift
+++ Example/OpenSwiftUIUITests/Render/GeometryEffect/RotationEffectUITests.swift
@@ -6,7 +6,7 @@ import Testing
 import SnapshotTesting
 
 @MainActor
-@Suite(.snapshots(record: .never, diffTool: diffTool))
+@Suite(.snapshots(record: .never, diffTool: diffTool), .disabled(if: viewRendererVendor == .osui, "rotate is not supported"))
 struct RotationEffectUITests {
     @Test
     func rotationEffect() {
