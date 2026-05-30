1ed560bf6b705a63c542b827691c5e4d6ccf5b53
Kyle <kyle201817146@gmail.com>
2026-05-30 22:03:04 +0800

    Add AttributedStringConvertible

--- Sources/OpenSwiftUICore/View/Text/Text/Text+DiscreteFormatStyle.swift
+++ Sources/OpenSwiftUICore/View/Text/Text/Text+DiscreteFormatStyle.swift
@@ -5,3 +5,21 @@
 //  Audited for 6.5.4
 //  Status: Empty
 //  ID: C8A98712CE9284278805F6E671356D1B (SwiftUICore)
+
+package import Foundation
+
+package protocol AttributedStringConvertible {
+    var attributedString: AttributedString { get }
+}
+
+extension AttributedString: AttributedStringConvertible {
+    package var attributedString: AttributedString {
+        self
+    }
+}
+
+extension String: AttributedStringConvertible {
+    package var attributedString: AttributedString {
+        AttributedString(self)
+    }
+}
