22f30c9d3d6d8ca67dc7ddebc2ddb170146b28f0
Kyle <kyle201817146@gmail.com>
2026-06-13 16:05:56 +0800

    Update OverlayModifier

--- Sources/OpenSwiftUICore/Modifier/ViewModifier/OverlayModifier.swift
+++ Sources/OpenSwiftUICore/Modifier/ViewModifier/OverlayModifier.swift
@@ -2,12 +2,13 @@
 //  OverlayModifier.swift
 //  OpenSwiftUICore
 //
-//  Status: WIP
+//  Audited for 6.5.4
+//  Status: Complte
 
-import Foundation
+public import Foundation
 package import OpenAttributeGraphShims
 
-// MARK: - makeSecondaryLayerView [6.4.41]
+// MARK: - makeSecondaryLayerView
 
 package func makeSecondaryLayerView<SecondaryLayer>(
     secondaryLayer: Attribute<SecondaryLayer>,
@@ -49,10 +50,11 @@ package func makeSecondaryLayerView<SecondaryLayer>(
     return result
 }
 
-// MARK: - OverlayModifier [6.4.41]
+// MARK: - OverlayModifier
 
 /// A modifier that layers a secondary view in front of the primary content it
 /// modifies, while maintaining the layout characteristics of the primary view.
+@available(OpenSwiftUI_v1_0, *)
 @frozen
 public struct _OverlayModifier<Overlay>: ViewModifier, MultiViewModifier, PrimitiveViewModifier where Overlay: View {
     public var overlay: Overlay
@@ -85,9 +87,85 @@ public struct _OverlayModifier<Overlay>: ViewModifier, MultiViewModifier, Primit
 @available(*, unavailable)
 extension _OverlayModifier: Sendable {}
 
-// TODO
+@available(OpenSwiftUI_v1_0, *)
+extension _OverlayModifier: Equatable where Overlay: Equatable {
+    nonisolated public static func == (a: _OverlayModifier<Overlay>, b: _OverlayModifier<Overlay>) -> Bool {
+        a.overlay == b.overlay && a.alignment == b.alignment
+    }
+}
 
-// MARK: - View + Overlay [6.4.41] [WIP]
+// MARK: - OverlayStyleModifier
+
+@available(OpenSwiftUI_v3_0, *)
+@frozen
+public struct _OverlayStyleModifier<Style>: ViewModifier, MultiViewModifier, PrimitiveViewModifier where Style: ShapeStyle {
+    public var style: Style
+
+    public var ignoresSafeAreaEdges: Edge.Set
+
+    @inlinable
+    public init(style: Style, ignoresSafeAreaEdges: Edge.Set) {
+        self.style = style
+        self.ignoresSafeAreaEdges = ignoresSafeAreaEdges
+    }
+
+    nonisolated public static func _makeView(
+        modifier: _GraphValue<Self>,
+        inputs: _ViewInputs,
+        body: @escaping (_Graph, _ViewInputs) -> _ViewOutputs
+    ) -> _ViewOutputs {
+        _BackgroundStyleModifier.makeShapeView(
+            modifier: modifier.unsafeBitCast(to: _BackgroundStyleModifier<Style>.self),
+            inputs: inputs,
+            shapeIsBackground: false,
+            body: body
+        )
+    }
+
+    public typealias Body = Never
+}
+
+@available(*, unavailable)
+extension _OverlayStyleModifier: Sendable {}
+
+// MARK: - OverlayShapeModifier
+
+@available(OpenSwiftUI_v3_0, *)
+@frozen
+public struct _OverlayShapeModifier<Style, Bounds>: ViewModifier, MultiViewModifier, PrimitiveViewModifier where Style: ShapeStyle, Bounds: Shape {
+    public var style: Style
+
+    public var shape: Bounds
+
+    public var fillStyle: FillStyle
+
+    @inlinable
+    public init(style: Style, shape: Bounds, fillStyle: FillStyle) {
+        self.style = style
+        self.shape = shape
+        self.fillStyle = fillStyle
+    }
+
+    nonisolated public static func _makeView(
+        modifier: _GraphValue<Self>,
+        inputs: _ViewInputs,
+        body: @escaping (_Graph, _ViewInputs) -> _ViewOutputs
+    ) -> _ViewOutputs {
+        _BackgroundShapeModifier.makeShapeView(
+            modifier: modifier.unsafeBitCast(to: _BackgroundShapeModifier<Style, Bounds>.self),
+            inputs: inputs,
+            shapeIsBackground: false,
+            body: body
+        )
+    }
+
+    public typealias Body = Never
+}
+
+@available(*, unavailable)
+extension _OverlayShapeModifier: Sendable {}
+
+// MARK: - View + Overlay
 
 @available(OpenSwiftUI_v1_0, *)
 extension View {
@@ -115,6 +193,43 @@ extension View {
     @_disfavoredOverload
     nonisolated public func overlay<Overlay>(_ overlay: Overlay, alignment: Alignment = .center) -> some View where Overlay: View {
         modifier(_OverlayModifier(overlay: overlay, alignment: alignment))
+    }
+
+    /// Adds a border to this view with the specified style and width.
+    ///
+    /// Use this modifier to draw a border of a specified width around the
+    /// view's frame. By default, the border appears inside the bounds of this
+    /// view. For example, you can add a four-point wide border covers the text:
+    ///
+    ///     Text("Purple border inside the view bounds.")
+    ///         .border(Color.purple, width: 4)
+    ///
+    /// ![A screenshot showing the text Purple border inside the view bounds.
+    /// The text is surrounded by a purple border that outlines the text,
+    /// but isn't quite big enough and encroaches on the text.](View-border-1)
+    ///
+    /// To place a border around the outside of this view, apply padding of the
+    /// same width before adding the border:
+    ///
+    ///     Text("Purple border outside the view bounds.")
+    ///         .padding(4)
+    ///         .border(Color.purple, width: 4)
+    ///
+    /// ![A screenshot showing the text Purple border outside the view bounds.
+    /// The text is surrounded by a purple border that outlines the text
+    /// without touching the text.](View-border-2)
+    ///
+    /// - Parameters:
+    ///   - content: A value that conforms to the ``ShapeStyle`` protocol,
+    ///     like a ``Color`` or ``HierarchicalShapeStyle``, that OpenSwiftUI
+    ///     uses to fill the border.
+    ///   - width: The thickness of the border. The default is 1 pixel.
+    ///
+    /// - Returns: A view that adds a border with the specified style and width
+    ///   to this view.
+    @inlinable
+    nonisolated public func border<S>(_ content: S, width: CGFloat = 1) -> some View where S: ShapeStyle {
+        overlay(Rectangle().strokeBorder(content, lineWidth: width))
     }
 }
 
@@ -239,9 +354,111 @@ extension View {
     nonisolated public func overlay<V>(alignment: Alignment = .center, @ViewBuilder content: () -> V) -> some View where V: View {
         modifier(_OverlayModifier(overlay: content(), alignment: alignment))
     }
+
+    /// Layers the specified style in front of this view.
+    ///
+    /// Use this modifier to layer a type that conforms to the ``ShapeStyle``
+    /// protocol, like a ``Color``, ``Material``, or ``HierarchicalShapeStyle``,
+    /// in front of a view. For example, you can overlay the
+    /// ``ShapeStyle/ultraThinMaterial`` over a ``Circle``:
+    ///
+    ///     struct CoveredCircle: View {
+    ///         var body: some View {
+    ///             Circle()
+    ///                 .frame(width: 300, height: 200)
+    ///                 .overlay(.ultraThinMaterial)
+    ///         }
+    ///     }
+    ///
+    /// OpenSwiftUI anchors the style to the view's bounds. For the example above,
+    /// the overlay fills the entirety of the circle's frame (which happens
+    /// to be wider than the circle is tall):
+    ///
+    /// ![A screenshot of a circle showing through a rectangle that imposes
+    /// a blurring effect.](View-overlay-5)
+    ///
+    /// OpenSwiftUI also limits the style's extent to the view's
+    /// container-relative shape. You can see this effect if you constrain the
+    /// `CoveredCircle` view with a ``View/containerShape(_:)`` modifier:
+    ///
+    ///     CoveredCircle()
+    ///         .containerShape(RoundedRectangle(cornerRadius: 30))
+    ///
+    /// The overlay takes on the specified container shape:
+    ///
+    /// ![A screenshot of a circle showing through a rounded rectangle that
+    /// imposes a blurring effect.](View-overlay-6)
+    ///
+    /// By default, the overlay ignores safe area insets on all edges, but you
+    /// can provide a specific set of edges to ignore, or an empty set to
+    /// respect safe area insets on all edges:
+    ///
+    ///     Rectangle()
+    ///         .overlay(
+    ///             .secondary,
+    ///             ignoresSafeAreaEdges: []) // Ignore no safe area insets.
+    ///
+    /// If you want to specify a ``View`` or a stack of views as the overlay
+    /// rather than a style, use ``View/overlay(alignment:content:)`` instead.
+    /// If you want to specify a ``Shape``, use
+    /// ``View/overlay(_:in:fillStyle:)``.
+    ///
+    /// - Parameters:
+    ///   - style: An instance of a type that conforms to ``ShapeStyle`` that
+    ///     OpenSwiftUI layers in front of the modified view.
+    ///   - edges: The set of edges for which to ignore safe area insets
+    ///     when adding the overlay. The default value is ``Edge/Set/all``.
+    ///     Specify an empty set to respect safe area insets on all edges.
+    ///
+    /// - Returns: A view with the specified style drawn in front of it.
+    @inlinable
+    nonisolated public func overlay<S>(_ style: S, ignoresSafeAreaEdges edges: Edge.Set = .all) -> some View where S: ShapeStyle {
+        modifier(_OverlayStyleModifier(style: style, ignoresSafeAreaEdges: edges))
+    }
+
+    /// Layers a shape that you specify in front of this view.
+    ///
+    /// Use this modifier to layer a type that conforms to the ``Shape``
+    /// protocol --- like a ``Rectangle``, ``Circle``, or ``Capsule`` --- in
+    /// front of a view. Specify a ``ShapeStyle`` that's used to fill the shape.
+    /// For example, you can overlay the outline of one rectangle in front of
+    /// another:
+    ///
+    ///     Rectangle()
+    ///         .frame(width: 200, height: 100)
+    ///         .overlay(.teal, in: Rectangle().inset(by: 10).stroke(lineWidth: 5))
+    ///
+    /// The example above uses the ``InsettableShape/inset(by:)`` method to
+    /// slightly reduce the size of the overlaid rectangle, and the
+    /// ``Shape/stroke(lineWidth:)`` method to fill only the shape's outline.
+    /// This creates an inset border:
+    ///
+    /// ![A screenshot of a rectangle with a teal border that's
+    /// inset from the edge.](View-overlay-7)
+    ///
+    /// This modifier is a convenience method for layering a shape over a view.
+    /// To handle the more general case of overlaying a ``View`` --- or a stack
+    /// of views --- with control over the position, use
+    /// ``View/overlay(alignment:content:)`` instead. To cover a view with a
+    /// ``ShapeStyle``, use ``View/overlay(_:ignoresSafeAreaEdges:)``.
+    ///
+    /// - Parameters:
+    ///   - style: A ``ShapeStyle`` that OpenSwiftUI uses to fill the shape
+    ///     that you specify.
+    ///   - shape: An instance of a type that conforms to ``Shape`` that
+    ///     OpenSwiftUI draws in front of the view.
+    ///   - fillStyle: The ``FillStyle`` to use when drawing the shape.
+    ///     The default style uses the nonzero winding number rule and
+    ///     antialiasing.
+    ///
+    /// - Returns: A view with the specified shape drawn in front of it.
+    @inlinable
+    nonisolated public func overlay<S, T>(_ style: S, in shape: T, fillStyle: FillStyle = FillStyle()) -> some View where S: ShapeStyle, T: Shape {
+        modifier(_OverlayShapeModifier(style: style, shape: shape, fillStyle: fillStyle))
+    }
 }
 
-// MARK: - SecondaryLayerGeometryQuery [6.4.41]
+// MARK: - SecondaryLayerGeometryQuery
 
 package struct SecondaryLayerGeometryQuery: Rule, AsyncAttribute {
     @OptionalAttribute
