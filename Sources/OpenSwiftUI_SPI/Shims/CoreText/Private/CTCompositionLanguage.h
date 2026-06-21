41acbab03ed4c2f360067f83d47c24db73898619
Kyle <kyle201817146@gmail.com>
2026-06-21 18:13:53 +0800

    Update ArchivedViewCore

--- Sources/OpenSwiftUI_SPI/Shims/CoreText/Private/CTCompositionLanguage.h
+++ Sources/OpenSwiftUI_SPI/Shims/CoreText/Private/CTCompositionLanguage.h
@@ -7,13 +7,10 @@
 #include "OpenSwiftUIBase.h"
 
 #if __has_include(<CoreText/CoreText.h>)
-
 #import <CoreText/CoreText.h>
-#import <CoreFoundation/CoreFoundation.h>
+#endif
 
-CF_ASSUME_NONNULL_BEGIN
-
-typedef CF_ENUM(uint8_t, CTCompositionLanguage) {
+typedef OPENSWIFTUI_ENUM(uint8_t, CTCompositionLanguage) {
     kCTCompositionLanguageUnset,
     kCTCompositionLanguageNone,
     kCTCompositionLanguageJapanese,
@@ -21,6 +18,22 @@ typedef CF_ENUM(uint8_t, CTCompositionLanguage) {
     kCTCompositionLanguageTraditionalChinese,
 };
 
-CF_ASSUME_NONNULL_END
+OPENSWIFTUI_EXTERN_C_BEGIN
 
-#endif /* CoreText.h */
+#if __has_include(<CoreText/CoreText.h>)
+
+CTCompositionLanguage CTParagraphStyleGetCompositionLanguageForLanguage(CFStringRef language);
+
+static inline CTCompositionLanguage OpenSwiftUI_CTParagraphStyleGetCompositionLanguageForLanguage(CFStringRef language) {
+    return CTParagraphStyleGetCompositionLanguageForLanguage(language);
+}
+
+#else
+
+static inline CTCompositionLanguage OpenSwiftUI_CTParagraphStyleGetCompositionLanguageForLanguage(CFStringRef language) {
+    return kCTCompositionLanguageUnset;
+}
+
+#endif
+
+OPENSWIFTUI_EXTERN_C_END
