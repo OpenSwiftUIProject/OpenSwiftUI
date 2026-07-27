299df77df0f5332c7bfa591391f8afc718f6ee23
Kyle <kyle201817146@gmail.com>
2026-07-28 03:07:17 +0800

    Implement custom Font APIs

--- Sources/OpenSwiftUICore/View/Text/Font/CustomFont.swift
+++ Sources/OpenSwiftUICore/View/Text/Font/CustomFont.swift
@@ -2,5 +2,100 @@
 //  CustomFont.swift
 //  OpenSwiftUICore
 //
-//  Status: TODO
+//  Audited for 6.5.4
+//  Status: Complete
 //  ID: A1A6E08ED7787270EADAD2AE750791A9 (SwiftUICore)
+
+public import Foundation
+#if canImport(CoreText)
+public import CoreText
+#endif
+
+@available(OpenSwiftUI_v1_0, *)
+extension Font {
+    public static func custom(_ name: String, size: CGFloat) -> Font {
+        Font(
+            provider: NamedProvider(
+                name: name,
+                size: size,
+                textStyle: .body
+            )
+        )
+    }
+
+    @available(OpenSwiftUI_v2_0, *)
+    public static func custom(
+        _ name: String,
+        size: CGFloat,
+        relativeTo textStyle: Font.TextStyle
+    ) -> Font {
+        Font(
+            provider: NamedProvider(
+                name: name,
+                size: size,
+                textStyle: textStyle
+            )
+        )
+    }
+
+    @available(OpenSwiftUI_v2_0, *)
+    @available(*, deprecated, renamed: "custom(_:size:textStyle:)")
+    public static func _custom(
+        _ name: String,
+        size: CGFloat,
+        textStyle: Font.TextStyle
+    ) -> Font {
+        custom(name, size: size, relativeTo: textStyle)
+    }
+
+    @available(OpenSwiftUI_v2_0, *)
+    public static func custom(_ name: String, fixedSize: CGFloat) -> Font {
+        Font(
+            provider: NamedProvider(
+                name: name,
+                size: fixedSize,
+                textStyle: nil
+            )
+        )
+    }
+
+    @available(OpenSwiftUI_v2_0, *)
+    @available(*, deprecated, renamed: "custom(_:fixedSize:)")
+    public static func _custom(_ name: String, verbatimSize: CGFloat) -> Font {
+        custom(name, fixedSize: verbatimSize)
+    }
+
+    public init(_ font: CTFont) {
+        self.init(provider: PlatformFontProvider(font: font))
+    }
+}
+
+extension Font {
+    private struct NamedProvider: FontProvider {
+        var name: String
+        var size: CGFloat
+        var textStyle: Font.TextStyle?
+
+        func resolve(in context: Font.Context) -> CTFontDescriptor {
+            context.fontDefinition.base
+                .resolveCustomFont(
+                    name: name,
+                    size: size,
+                    textStyle: textStyle,
+                    in: context
+                )
+        }
+    }
+
+    private struct PlatformFontProvider: FontProvider {
+        var font: CTFont
+
+        func resolve(in context: Font.Context) -> CTFontDescriptor {
+            context.fontDefinition.base.resolveFont(font)
+        }
+
+        func resolveTraits(in context: Font.Context) -> Font.ResolvedTraits {
+            context.fontDefinition.base.resolveFontInfo(font)
+        }
+    }
+}
--- Tests/OpenSwiftUICoreTests/View/Text/Font/FontTests.swift
+++ Tests/OpenSwiftUICoreTests/View/Text/Font/FontTests.swift
@@ -21,6 +21,81 @@ struct FontTests {
         let boldWeight = CTFontDescriptorGetWeight(boldDescriptor)
         #expect(boldWeight.isApproximatelyEqual(to: 0.3, absoluteTolerance: 0.01))
     }
+
+    @Test
+    func customFontIdentity() {
+        let body = Font.custom("Helvetica", size: 17.0)
+
+        #expect(body == Font.custom("Helvetica", size: 17.0))
+        #expect(body == Font.custom("Helvetica", size: 17.0, relativeTo: .body))
+        #expect(body != Font.custom("Helvetica", size: 17.0, relativeTo: .headline))
+        #expect(body != Font.custom("Helvetica", fixedSize: 17.0))
+        #expect(body != Font.custom("Helvetica Neue", size: 17.0))
+    }
+
+    @MainActor
+    @Test
+    func customFontSizing() throws {
+        try Semantics.v2.test(as: \.sdk) {
+            let size = 17.0
+            let dynamicTypeSize = DynamicTypeSize.accessibility5
+
+            let scalableDescriptor = Font.custom(
+                "Helvetica",
+                size: size
+            ).resolve(in: dynamicTypeSize)
+            let fixedDescriptor = Font.custom(
+                "Helvetica",
+                fixedSize: size
+            ).resolve(in: dynamicTypeSize)
+
+            let scalableSize = try #require(
+                CTFontDescriptorCopyAttribute(
+                    scalableDescriptor,
+                    kCTFontSizeAttribute
+                ) as? CGFloat
+            )
+            let fixedSize = try #require(
+                CTFontDescriptorCopyAttribute(
+                    fixedDescriptor,
+                    kCTFontSizeAttribute
+                ) as? CGFloat
+            )
+            let expectedScalableSize = (
+                Font.scaleFactor(
+                    textStyle: .body,
+                    in: dynamicTypeSize
+                ) * size
+            ).rounded()
+
+            #expect(
+                scalableSize.isApproximatelyEqual(
+                    to: expectedScalableSize,
+                    absoluteTolerance: 0.01
+                )
+            )
+            #expect(
+                fixedSize.isApproximatelyEqual(
+                    to: size,
+                    absoluteTolerance: 0.01
+                )
+            )
+        }
+    }
+
+    @Test
+    func platformFont() {
+        let platformFont = CTFontCreateWithName(
+            "Helvetica" as CFString,
+            23.0,
+            nil
+        )
+        let font = Font(platformFont)
+        let descriptor = font.resolve(in: .large)
+
+        #expect(CFEqual(descriptor, CTFontCopyFontDescriptor(platformFont)))
+        #expect(Set([font, Font(platformFont)]).count == 1)
+    }
 }
 
 #endif
