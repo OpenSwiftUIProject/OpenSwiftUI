9374f98a01c6d265d6039917d88dbcd96c5ac814
Kyle <kyle201817146@gmail.com>
2026-07-29 01:21:07 +0800

    Enable Tuist caching and build insights in CI

--- Example/Tuist.swift
+++ Example/Tuist.swift
@@ -1,8 +1,13 @@
 import ProjectDescription
 
 let tuist = Tuist(
+    fullHandle: "OpenSwiftUIProject/openswiftui",
+    xcodeCache: .xcodeCache(
+        upload: Environment.isCI
+    ),
     project: .tuist(
         generationOptions: .options(
+            enableCaching: Environment.isCI,
             manifestEnvironment: [
                 "DARWINPRIVATEFRAMEWORKS_*",
                 "OPENATTRIBUTEGRAPH_*",
