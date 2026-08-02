8f0dd57e00aad1c04e9bbb12716f057c98504e0a
Kyle <kyle201817146@gmail.com>
2026-08-03 02:34:44 +0800

    Add text orphan and push-out UI test

---
+++ Example/Shared/View/Text/TextOrphanAndPushOutExample.swift
@@ -0,0 +1,34 @@
+//
+//  TextOrphanAndPushOutExample.swift
+//  Shared
+
+#if OPENSWIFTUI
+@_spi(Private) import OpenSwiftUI
+#else
+import SwiftUI_SPI
+#endif
+
+struct TextOrphanAndPushOutExample: View {
+    private let text = "OpenSwiftUI 开发者"
+    private let width = 150.0
+
+    @ViewBuilder
+    private func textView(avoidsOrphans: Bool? = nil) -> some View {
+        if let avoidsOrphans {
+            Text(text)
+                .frame(width: width, alignment: .leading)
+                .avoidsOrphans(avoidsOrphans)
+        } else {
+            Text(text)
+                .frame(width: width, alignment: .leading)
+        }
+    }
+
+    var body: some View {
+        VStack(alignment: .leading, spacing: 16) {
+            textView()
+            textView(avoidsOrphans: true)
+            textView(avoidsOrphans: false)
+        }
+    }
+}
