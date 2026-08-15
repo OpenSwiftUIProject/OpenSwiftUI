e683684f2a3cc84d3ec0aa397140ccfe548e64dc
Kyle <kyle201817146@gmail.com>
2026-08-15 21:22:33 +0800

    Add PlatformItemListFlag

---
+++ Sources/OpenSwiftUI/Integration/PlatformItemList/PlatformItemListFlag.swift
@@ -0,0 +1,105 @@
+//
+//  PlatformItemListFlag.swift
+//  OpenSwiftUI
+//
+//  Audited for 6.5.4
+//  Status: Complete
+
+@_spi(ForOpenSwiftUIOnly)
+import OpenSwiftUICore
+
+// MARK: - PlatformItemListFlagsSet
+
+struct PlatformItemListFlagsSet: OptionSet, Hashable {
+    var rawValue: UInt32
+
+    init(rawValue: UInt32) {
+        self.rawValue = rawValue
+    }
+
+    static var selection: PlatformItemListFlagsSet { .init(rawValue: 1 << 0) }
+
+    static var _1: PlatformItemListFlagsSet { .init(rawValue: 1 << 1) }
+
+    static var text: PlatformItemListFlagsSet { .init(rawValue: 1 << 2) }
+
+    static var layout: PlatformItemListFlagsSet { .init(rawValue: 1 << 3) }
+
+    static var _4: PlatformItemListFlagsSet { .init(rawValue: 1 << 4) }
+
+    static var _5: PlatformItemListFlagsSet { .init(rawValue: 1 << 5) }
+
+    static var _6: PlatformItemListFlagsSet { .init(rawValue: 1 << 6) }
+
+    static var action: PlatformItemListFlagsSet { [.selection, .text, .layout] }
+
+    static var label: PlatformItemListFlagsSet { [._1, .text, ._4] }
+
+    static var toolbar: PlatformItemListFlagsSet { .label }
+
+    static var searchToken: PlatformItemListFlagsSet { [.selection, ._1, .text, .layout] }
+
+    static var widgetMetadata: PlatformItemListFlagsSet { [._1, .text, ._4, ._5, ._6] }
+
+    static var all: PlatformItemListFlagsSet { .init(rawValue: .max) }
+
+    enum EditOperation {
+        case replace
+        case formUnion
+    }
+}
+
+// MARK: - PlatformItemListFlags
+
+protocol PlatformItemListFlags {
+    static var flags: PlatformItemListFlagsSet { get }
+}
+
+struct SelectionPlatformItemListFlags: PlatformItemListFlags {
+    static var flags: PlatformItemListFlagsSet { .selection }
+}
+
+struct AllPlatformItemListFlags: PlatformItemListFlags {
+    static var flags: PlatformItemListFlagsSet { .all }
+}
+
+struct TextPlatformItemListFlags: PlatformItemListFlags {
+    static var flags: PlatformItemListFlagsSet { .text }
+}
+
+struct LayoutPlatformItemListFlags: PlatformItemListFlags {
+    static var flags: PlatformItemListFlagsSet { .layout }
+}
+
+struct ActionPlatformItemListFlags: PlatformItemListFlags {
+    static var flags: PlatformItemListFlagsSet { .action }
+}
+
+struct LabelPlatformItemListFlags: PlatformItemListFlags {
+    static var flags: PlatformItemListFlagsSet { .label }
+}
+
+struct ToolbarPlatformItemListFlags: PlatformItemListFlags {
+    static var flags: PlatformItemListFlagsSet { .toolbar }
+}
+
+struct SearchTokenPlatformItemListFlags: PlatformItemListFlags {
+    static var flags: PlatformItemListFlagsSet { .searchToken }
+}
+
+struct WidgetMetadataPlatformItemListFlags: PlatformItemListFlags {
+    static var flags: PlatformItemListFlagsSet { .widgetMetadata }
+}
+
+// MARK: - PlatformItemListFlagsInput
+
+struct PlatformItemListFlagsInput: ViewInput {
+    static var defaultValue: PlatformItemListFlagsSet { .all }
+}
+
+extension _ViewInputs {
+    var platformItemListFlags: PlatformItemListFlagsSet {
+        get { self[PlatformItemListFlagsInput.self] }
+        set { self[PlatformItemListFlagsInput.self] = newValue }
+    }
+}
