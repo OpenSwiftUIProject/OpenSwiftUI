6785a0ba221ea8df2a09b237f8bed6c8e7737205
Kyle <kyle201817146@gmail.com>
2026-08-19 16:10:20 +0800

    Add UIKitGestureUtilities

---
+++ Sources/OpenSwiftUI/Event/Platform/UIKit/UIKitGestureUtilities.swift
@@ -0,0 +1,29 @@
+//
+//  UIKitGestureUtilities.swift
+//  OpenSwiftUI
+//
+//  Audited for 6.5.4
+//  Status: Complete
+
+#if os(iOS) || os(visionOS)
+import UIKit
+@_spi(ForOpenSwiftUIOnly)
+import OpenSwiftUICore
+
+extension UIGestureRecognizer.State {
+    func nextState(for phase: GesturePhase<Void>) -> UIGestureRecognizer.State {
+        switch phase {
+        case .possible:
+            self
+        case .active:
+            self == .possible ? .began : .changed
+        case .ended:
+            .ended
+        case .failed:
+            self == .possible || self == .failed ? .failed : .cancelled
+        @unknown default:
+            _openSwiftUIUnreachableCode()
+        }
+    }
+}
+#endif
