d176928692a8ac9018d1667c012b7f30fa026996
Hunter Breathat <nexushunter@pm.me>
2026-09-05 20:35:49 -0400

    Label by text and image

--- Sources/OpenSwiftUI/View/Label/Label.swift
+++ Sources/OpenSwiftUI/View/Label/Label.swift
@@ -1,4 +1,10 @@
-nonisolated struct Label<Title,Icon>  where Title: View, Icon: View {
+import Foundation 
+
+nonisolated struct Label<Title,Icon>: View where Title: View, Icon: View {
+    private init(title: Title, icon: Icon) {
+        self.title = title;
+        self.icon = icon
+    }
     init(@ViewBuilder title: () -> Title, @ViewBuilder icon: () -> Icon) {
         self.title = title();
         self.icon = icon();
@@ -12,5 +18,35 @@ nonisolated struct Label<Title,Icon>  where Title: View, Icon: View {
             title
             icon
         }
+    }
+}
+
+extension Label where Title == Text, Icon == Image {
+    nonisolated
+    init(_ title: LocalizedStringResource, image name: String) {
+        self.init(title: Text(title), icon: Image(name))
+    }
+
+    nonisolated
+    init(_ title: LocalizedStringResource, image resource: ImageResource) {
+        self.init(title: Text(title), icon: Image(resource))
+    }
+     nonisolated
+    init(_ title: LocalizedStringKey, image name: String) {
+        self.init(title: Text(title), icon: Image(name))
+    }
+
+    nonisolated
+    init(_ title: LocalizedStringKey, image resource: ImageResource) {
+        self.init(title: Text(title), icon: Image(resource))
+    }
+     nonisolated
+    init<S>(_ title: S, image name: String) where S: StringProtocol {
+        self.init(title: Text(title), icon: Image(name))
+    }
+
+    nonisolated
+    init<S>(_ titleResource: S, image resource: ImageResource) where S: StringProtocol {
+        self.init(title: Text(titleResource), icon: Image(resource))
     }
 }
