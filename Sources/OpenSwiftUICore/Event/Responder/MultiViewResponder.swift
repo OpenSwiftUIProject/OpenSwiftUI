ecc870fb76a83a032756fd121aaa3a2da3455f74
Kyle <kyle201817146@gmail.com>
2026-09-08 22:40:27 +0800

    feat(event): complete MultiViewResponder implementation

--- Sources/OpenSwiftUICore/Event/Responder/MultiViewResponder.swift
+++ Sources/OpenSwiftUICore/Event/Responder/MultiViewResponder.swift
@@ -2,18 +2,19 @@
 //  MultiViewResponder.swift
 //  OpenSwiftUICore
 //
-//  Status: Blocked by children.setter
+//  Audited for 6.5.4
+//  Status: Complete
 //  ID: 4A74C6B0E69BD6BC864CC77E33CF2D28 (SwiftUICore)
 
 public import Foundation
 
-// MARK: - MultiViewResponder [6.5.4] [WIP]
+// MARK: - MultiViewResponder
 
 @_spi(ForOpenSwiftUIOnly)
 open class MultiViewResponder: ViewResponder {
-    private var _children: [ViewResponder]
-    private var cache: ContainsPointsCache
-    private var observers: ContentPathObservers
+    private var _children: [ViewResponder] = []
+    private var cache: ContainsPointsCache = .init()
+    private var observers: ContentPathObservers = .init()
 
     override public init() {
         _children = []
@@ -22,7 +23,63 @@ open class MultiViewResponder: ViewResponder {
         super.init()
     }
 
-    // MARK: - MultiViewResponder: ResponderNode
+    override final public var children: [ViewResponder] {
+        get { _children }
+        set {
+            var count = _children.count
+            var index = 0
+            var changed = false
+            for child in newValue {
+                var foundIndex: Int?
+                if child.parent === self {
+                    for candidateIndex in index..<count {
+                        if _children[candidateIndex] === child {
+                            foundIndex = candidateIndex
+                            break
+                        }
+                    }
+                }
+                if let foundIndex {
+                    if foundIndex != index {
+                        _children.swapAt(index, foundIndex)
+                        changed = true
+                    }
+                } else {
+                    child.parent = self
+                    _children.append(child)
+                    if index < count {
+                        _children.swapAt(index, count)
+                    }
+                    count += 1
+                    changed = true
+                }
+                index += 1
+            }
+            if index < count {
+                for removalIndex in index..<count {
+                    let child = _children[removalIndex]
+                    if child.parent === self {
+                        child.parent = nil
+                    }
+                }
+                _children.removeSubrange(index..<count)
+                changed = true
+            }
+            if changed {
+                childrenDidChange()
+            }
+        }
+    }
+
+    final package func updateChildren(_ data: (value: [ViewResponder], changed: Bool)) {
+        if data.changed {
+            children = data.value
+        }
+    }
+
+    open func childrenDidChange() {
+        observers.notifyDidChange(for: self)
+    }
 
     override open func bindEvent(_ event: any EventType) -> ResponderNode? {
         for child in children {
@@ -34,28 +91,11 @@ open class MultiViewResponder: ViewResponder {
         return nil
     }
 
-    @discardableResult
-    override final public func visit(applying visitor: (ResponderNode) -> ResponderVisitorResult) -> ResponderVisitorResult {
-        let result = visitor(self)
-        guard result == .next else {
-            return result
-        }
-        for child in children {
-            let childResult = child.visit(applying: visitor)
-            guard childResult != .cancel else {
-                return .cancel
-            }
-        }
-        return .next
-    }
-
     override open func resetGesture() {
         for child in children {
             child.resetGesture()
         }
     }
-
-    // MARK: - MultiViewResponder: ViewResponder
 
     override open func containsGlobalPoints(
         _ points: [PlatformPoint],
@@ -101,13 +141,21 @@ open class MultiViewResponder: ViewResponder {
         observers.addObserver(observer)
     }
 
-    override final public var children: [ViewResponder] {
-        get { _children }
-        set { _openSwiftUIUnimplementedFailure() }
-    }
-
-    open func childrenDidChange() {
-        observers.notifyDidChange(for: self)
+    @discardableResult
+    override final public func visit(
+        applying visitor: (ResponderNode) -> ResponderVisitorResult
+    ) -> ResponderVisitorResult {
+        let result = visitor(self)
+        guard result == .next else {
+            return result
+        }
+        for child in children {
+            let childResult = child.visit(applying: visitor)
+            guard childResult != .cancel else {
+                return .cancel
+            }
+        }
+        return .next
     }
 }
 
