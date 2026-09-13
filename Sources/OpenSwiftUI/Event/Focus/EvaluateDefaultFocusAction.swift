62d0bcfd5cb6c6c9598606fe336d2ea58327d812
Kyle <kyle201817146@gmail.com>
2026-09-13 16:45:58 +0800

    feat: Add focus related placeholder type

---
+++ Sources/OpenSwiftUI/Event/Focus/EvaluateDefaultFocusAction.swift
@@ -0,0 +1,8 @@
+//
+//  EvaluateDefaultFocusAction.swift
+//  OpenSwiftUI
+//
+//  Audited for 6.5.4
+//  Status: WIP
+
+struct EvaluateDefaultFocusAction {}
---
+++ Sources/OpenSwiftUI/Event/Focus/FocusItem.swift
@@ -0,0 +1,9 @@
+//
+//  FocusItem.swift
+//  OpenSwiftUI
+//
+//  Audited for 6.5.4
+//  Status: WIP
+//  ID: 25A477FB55B8969CF1C407C6BC07980E (SwiftUI)
+
+struct FocusItem {}
