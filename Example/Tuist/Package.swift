777e000db7bc0d8c316226eb83053fa54c252e23
Kyle <kyle201817146@gmail.com>
2026-09-25 13:13:56 +0800

    build: update Hammer to 0.19.2

--- Example/Tuist/Package.swift
+++ Example/Tuist/Package.swift
@@ -35,7 +35,7 @@ var dependencies: [PackageDescription.Package.Dependency] = [
     .package(url: "https://github.com/OpenSwiftUIProject/equatable.git", branch: "main"),
     .package(url: "https://github.com/OpenSwiftUIProject/SymbolLocator.git", from: "0.2.0"),
     .package(url: "https://github.com/OpenSwiftUIProject/swift-snapshot-testing.git", exact: "1.19.3"),
-    .package(url: "https://github.com/OpenSwiftUIProject/Hammer.git", exact: "0.19.1"),
+    .package(url: "https://github.com/OpenSwiftUIProject/Hammer.git", exact: "0.19.2"),
 ]
 
 if openCombineCondition {
